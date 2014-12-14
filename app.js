2add2560bd0d3199ee34bf8905e986977d2abe50
topherwhite <topherwhite@gmail.com>
2014-12-14 11:33:27 +0100

    finally settled on multer for multipart fileuploads... to replace body-parser

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var express = require("express");
 var path = require("path");
 var favicon = require("serve-favicon");
 var logger = require("morgan");
-var bodyParser = require("body-parser");
+var multer = require("multer");
 var passport = require("passport");
 var middleware = {};
 var app = express();
@@ -26,8 +26,7 @@ app.set("title", "Rainforest Connection API");
 app.set("port", process.env.PORT || 8080);
 app.use(favicon(__dirname + "/public/img/logo/favicon.ico"));
 app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer({ dest: "/Users/Shared/" }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(passport.initialize());
 
@@ -61,17 +60,41 @@ app.get("/health_check",function(req,res){ res.status(200).json({rfcx:"awesome"}
 
 // Catch & Report Various HTTP Errors (needs some work)
 
+// app.use(function(req, res, next) {
+//   var err = new Error("Not Found");
+//   err.status = 404;
+//   next(err);
+// });
+
+// app.use(function(err, req, res, next) {
+//   if (process.env.NODE_ENV === "development") { console.log(err); }
+//   res.status(err.status || 500).json({
+//     message: err.message,
+//     error: (process.env.NODE_ENV === "development") ? err : {}
+//   });
+// });
+
 app.use(function(req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500).json({
+            message: err.message,
+            error: err
+        });
+    });
+}
+
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500).json({
-    message: err.message,
-    error: (process.env.NODE_ENV === "development") ? err : {}
-  });
+    res.status(err.status || 500).json({
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
