202065982c929d1781d4fa86a009e0b94d1bd045
topherwhite <topherwhite@gmail.com>
2014-12-14 02:28:34 +0100

    start of post for guardians

--- routes/v1/guardians.js
+++ routes/v1/guardians.js
@@ -6,13 +6,15 @@ for (m in middleware_v1) { router.use(middleware_v1[m]); }
 
 
 router.route("/")
-  .get(function(req, res) {
+  .get(function(req,res) {
     res.json({name:"list guardians"});
+  })
+  .post(function(req,res){
   })
 ;
 
 router.route("/:guardian_id")
-  .get(function(req, res) {
+  .get(function(req,res) {
     res.json({name:"one guardian: "+req.params.guardian_id});
   })
 ;
