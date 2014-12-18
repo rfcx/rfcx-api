6b551dd88d5b512b543072375115aff8feccbb6a
topherwhite <topherwhite@gmail.com>
2014-12-18 18:48:19 +0100

    checkin audio saved to s3 and ingestion queued in sqs. still needs sequelize integration

--- routes/v1/checkins.js
+++ routes/v1/checkins.js
@@ -6,23 +6,16 @@ var aws = require("../../config/aws.js").aws(process.env);
 
 router.route("/")
   .post(function(req, res) {
-    if (req.files.audio.length > 0) {
-      aws.s3("rfcx-ark").putFile(
-      req.files.audio[0].path,
-      "/test/"+req.files.audio[0].originalname, 
-      function(err, s3Res){
-        var jsonObj = { name: "thisName" };      
-        if (!!err) {
-          aws.sqs("rfcx-ingestion").sendMessage(jsonObj)
-            .then(function(messageArray) {
-              res.json(messageArray);
-            }).catch(function(err){
-              res.json(jsonObj);
-            });
-        } else {
-          res.json(err);
-        }
-
+    if (!!req.files.audio) {
+      var fileInfo = {
+        guardian_id: "abcdef",
+        checkin_id: "ghijkl",
+        created_at: new Date()
+      };
+      saveCheckInAudio(req, res, fileInfo, function(req, res, fileInfo){
+        addAudioToIngestionQueue(req, res, fileInfo, function(req, res, fileInfo){
+          res.json(fileInfo);
+        });
       });
     }
   })
@@ -35,3 +28,41 @@ router.route("/:checkin_id")
 ;
 
 module.exports = router;
+
+// Special Callbacks
+
+function saveCheckInAudio(req, res, fileInfo, callback) {
+  fileInfo.s3Path = 
+    "/"+process.env.NODE_ENV
+    +"/guardians/"+fileInfo.guardian_id
+    +"/"+fileInfo.created_at.toISOString().substr(0,10).replace(/-/g,"/")
+    +"/"+fileInfo.guardian_id
+    +"-"+fileInfo.created_at.toISOString().substr(0,19).replace(/:/g,"-")
+    +req.files.audio.originalname.substr(req.files.audio.originalname.indexOf("."));
+  aws.s3("rfcx-ark").putFile(
+  req.files.audio.path, fileInfo.s3Path, 
+  function(err, s3Res){
+    var s3Info = {  };
+    s3Res.resume();
+    if (!!err) {
+      res.status(500).json({msg:"error saving audio"});
+    } else {
+      callback(req, res, fileInfo);
+    }
+  });
+}
+
+function addAudioToIngestionQueue(req, res, fileInfo, callback) {
+  fileInfo.created_at = fileInfo.created_at.toISOString();
+  console.log(fileInfo);
+  aws.sqs("rfcx-ingestion").sendMessage(fileInfo)
+    .then(function(sqsResponse) {    
+      console.log(sqsResponse);
+      callback(req, res, fileInfo);
+//      res.json(messageArray);
+    }).catch(function(err){
+      res.status(500).json({msg:"error adding audio to ingestion queue"});
+    });
+}
+
+
