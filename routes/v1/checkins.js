21dc3a6b7603f37df1c6323fe083ccc3d1d7dbbf
topherwhite <topherwhite@gmail.com>
2014-12-18 15:36:10 +0100

    initial test implementation for file upload, sqs entry, and s3 put with knox

--- routes/v1/checkins.js
+++ routes/v1/checkins.js
@@ -1,14 +1,30 @@
 var models  = require('../../models');
 var express = require('express');
 var router = express.Router();
-var sqs = require("aws-sqs-promises");
+var aws = require("../../config/aws.js").aws(process.env);
 
 
 router.route("/")
   .post(function(req, res) {
+    if (req.files.audio.length > 0) {
+      aws.s3("rfcx-ark").putFile(
+      req.files.audio[0].path,
+      "/test/"+req.files.audio[0].originalname, 
+      function(err, s3Res){
+        var jsonObj = { name: "thisName" };      
+        if (!!err) {
+          aws.sqs("rfcx-ingestion").sendMessage(jsonObj)
+            .then(function(messageArray) {
+              res.json(messageArray);
+            }).catch(function(err){
+              res.json(jsonObj);
+            });
+        } else {
+          res.json(err);
+        }
 
-    res.json({name:"name"});
-  
+      });
+    }
   })
 ;
 
