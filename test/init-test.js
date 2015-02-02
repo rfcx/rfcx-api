6fcef298569a929d9a01fbe28996c12116a912f9
Topher White <t@pher.me>
2015-02-02 23:49:22 +0100

    first steps toward making the test work without certain files

--- test/init-test.js
+++ test/init-test.js
@@ -1,5 +1,8 @@
-var env = require("../config/env_vars.js").env;
-for (i in env) { process.env[i] = env[i]; }
+var fs = require("fs");
+if (fs.existsSync("../config/env_vars.js")) {
+  var env = require("../config/env_vars.js").env;
+  for (i in env) { process.env[i] = env[i]; }
+}
 if (process.env.NODE_ENV === "production"){
   throw Error("unit tests cannot be run in production!")
 }
