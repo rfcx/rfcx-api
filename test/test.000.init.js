0df10cdfbaa8b528a9665dcb7baf4184986e9f98
Topher White <t@pher.me>
2015-04-12 01:43:06 -0300

    config.json existence check for logging

--- test/test.000.init.js
+++ test/test.000.init.js
@@ -1,10 +1,16 @@
 var fs = require("fs");
 
-if (fs.existsSync("../config/env_vars.js")) {
-  var env = require("../config/env_vars.js").env;
+if (fs.existsSync(process.cwd()+"/config/env_vars.js")) {
+  var env = require(process.cwd()+"/config/env_vars.js").env;
   for (i in env) { process.env[i] = env[i]; }
 }
 
 if (process.env.NODE_ENV === "production"){
   throw Error("unit tests cannot be run in production!")
+}
+
+if (fs.existsSync(process.cwd()+"/config/config.json")) {
+  console.log("config.json exists");
+} else {
+  console.log("config.json does not exist");
 }
