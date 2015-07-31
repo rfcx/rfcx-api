7fa1cebce123146c84a29abd7ddcbde0bb9ef638
Topher White <t@pher.me>
2015-08-01 01:43:47 +0200

    middleware to set apiUrl

--- middleware/v1.js
+++ middleware/v1.js
@@ -3,5 +3,11 @@ exports.middleware = {
   emptyMiddleware: function(req, res, next) {
     next();
   },
+    
+  setApiUrl: function(req, res, next) {
+    var protocol = (req.headers["x-forwarded-proto"] != null) ? req.headers["x-forwarded-proto"] : req.protocol;
+    process.env.apiUrl = protocol+"://"+req.headers.host;
+    next();
+  },
 
 }
