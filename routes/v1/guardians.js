3c97ff05535dbca1d54998d42055c913a59950de
Topher White <t@pher.me>
2015-08-02 23:08:32 +0200

    some basic guardian endpoints

--- routes/v1/guardians.js
+++ routes/v1/guardians.js
@@ -1,19 +1,45 @@
 var models  = require("../../models");
 var express = require("express");
 var router = express.Router();
+var views = require("../../views/v1/models/_all.js").views;
 
 
 router.route("/")
   .get(function(req,res) {
-    res.json({name:"list guardians"});
-  })
-  .post(function(req,res){
+
+    models.Guardian
+      .findAll({ 
+//        where: { guardian_id: dbGuardian.id }, 
+        include: [ { all: true } ], 
+        order: [ ["last_check_in", "DESC"] ],
+        limit: req.rfcx.count
+      }).then(function(dbGuardian){
+        
+        res.status(200).json(views.guardian(req,res,dbGuardian));
+
+      }).catch(function(err){
+        console.log("failed to return guardians | "+err);
+        if (!!err) { res.status(500).json({msg:"failed to return guardians"}); }
+      });
+  
   })
 ;
 
 router.route("/:guardian_id")
   .get(function(req,res) {
-    res.json({name:"one guardian: "+req.params.guardian_id});
+
+    models.Guardian
+      .findOne({ 
+        where: { guid: req.params.guardian_id }
+      }).then(function(dbGuardian){
+
+        res.status(200).json(views.guardian(req,res,dbGuardian));
+
+      }).catch(function(err){
+        console.log("failed to return guardian | "+err);
+        if (!!err) { res.status(500).json({msg:"failed to return guardian"}); }
+      });
+
   })
 ;
 
