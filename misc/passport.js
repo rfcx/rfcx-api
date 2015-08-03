6d089863f985c1910d56252bb59e3eba10bb485d
Topher White <t@pher.me>
2015-08-03 13:36:34 +0200

    modularizing stuff

--- misc/passport.js
+++ misc/passport.js
@@ -1,18 +1,16 @@
 var models  = require("../models");
 var hash = require("../misc/hash.js").hash;
-var TokenStrategy   = require("passport-accesstoken").Strategy;
+var TokenStrategy = require("passport-accesstoken").Strategy;
 
-exports.passport = {
+exports.strategy = function(){
 
-  tokenStrategy:
-    new TokenStrategy({
+    return new TokenStrategy({
         tokenHeader: "x-auth-token",
         tokenField: "auth_token",
         tokenParams: "auth_token",
         tokenQuery: "auth_token",
         passReqToCallback: true
-      },
-      function (req, token, done) {
+      }, function(req,token,done){
 
         // parses auth_user from req.rfcx...
         // the way this is being done should probably be consolidated or re-considered
@@ -28,30 +26,7 @@ exports.passport = {
 
         if (authUser.type === "guardian") {
 
-          models.Guardian
-            .findOne({ 
-              where: { guid: authUser.guid }
-            }).then(function(dbGuardian){
-              var dbRow = dbGuardian;
-              if  (   (dbGuardian != null)
-                  &&  (dbGuardian.auth_token_hash == hash.hashedCredentials(dbGuardian.auth_token_salt,token))
-                  ) {
-                    var userObj = {
-                          type: "guardian",
-                          id: dbGuardian.id,
-                          guid: dbGuardian.guid,
-                          name: dbGuardian.shortname
-                        };
-                    console.log("authenticated as guardian "+userObj.guid);
-                    return done(null,userObj);
-              } else {
-                console.log("failed to match token with salted hash");
-                return done(null, false, {message:"invalid user/token combination"});
-              }
-            }).catch(function(err){
-              console.log("failed to find guardian | "+err);
-              return done(err);
-            });
+          return authenticateAs.guardian(req,token,done,authUser);
 
         } else {
 
@@ -60,6 +35,40 @@ exports.passport = {
         }
 
       }
-    )
+  );
 
 };
+
+var authenticateAs = {
+
+  guardian: function(req,token,done,authUser){
+
+    models.Guardian
+      .findOne({ 
+        where: { guid: authUser.guid }
+      }).then(function(dbGuardian){
+        var dbRow = dbGuardian;
+        if  (   (dbGuardian != null)
+            &&  (dbGuardian.auth_token_hash == hash.hashedCredentials(dbGuardian.auth_token_salt,token))
+            ) {
+              var userObj = {
+                    type: "guardian",
+                    id: dbGuardian.id,
+                    guid: dbGuardian.guid,
+                    name: dbGuardian.shortname
+                  };
+              console.log("authenticated as guardian "+userObj.guid);
+              return done(null,userObj);
+        } else {
+          console.log("failed to match token with salted hash");
+          return done(null, false, {message:"invalid user/token combination"});
+        }
+      }).catch(function(err){
+        console.log("failed to find guardian | "+err);
+        return done(err);
+      });
+
+  }
+
+};
+
