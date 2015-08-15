54eb923586aeeadfd41aa451d92cb76a90d7826f
Topher White <t@pher.me>
2015-08-15 13:11:56 +0200

    organizing the models directory

--- models/index.js
+++ models/index.js
@@ -8,14 +8,18 @@ var config    = require(process.cwd() + '/config/config.json')[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== "index.js") && (file !== "_associations.js");
-  })
-  .forEach(function(file) {
-    var model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
+// get file listing in 'models' directory, filtered by those we know to ignore...
+fs.readdirSync(__dirname).filter(function(file) {
+    return (file.indexOf(".") !== 0) && (file !== "index.js") && !fs.statSync(__dirname+"/"+file).isDirectory();
+  }).forEach(function(file) { importSequelizeModelFile(file); });
+
+// get file listings from inner directories in models
+fs.readdirSync(__dirname).filter(function(file) {
+    return (file.indexOf(".") !== 0) && fs.statSync(__dirname+"/"+file).isDirectory();
+  }).forEach(function(file) { 
+    fs.readdirSync(__dirname+"/"+file).filter(function(fileInDir) {
+      return (fileInDir.indexOf(".") !== 0);
+    }).forEach(function(fileInDir) { importSequelizeModelFile(file+"/"+fileInDir); });
   });
 
 Object.keys(db).forEach(function(modelName) {
@@ -28,3 +32,8 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
+function importSequelizeModelFile(file) {
+  var model = sequelize["import"](path.join(__dirname, file));
+  db[model.name] = model;
+}
