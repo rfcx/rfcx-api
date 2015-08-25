11a0bb281d10a7d04565b1b5304be738fa01dd4c
Topher White <t@pher.me>
2015-08-24 17:48:53 -0700

    modularizing token generation a little bit

--- misc/token.js
+++ misc/token.js
@@ -12,20 +12,22 @@ exports.token = {
 	 * generate an access token, and register in database
 	 *
    * @param {Object}  options:
+   *                  {String} token
    *                  {Integer} token_length
    *                  {String} reference_tag
+   *                  {Integer} owner_primary_key
    *                  {String} token_type
    *                  {Integer} minutes_until_expiration
    *                  {String} only_allow_access_to
    *                  {String} created_by
-	 * @return {Object} access_token
+	 * @return {Object} output_token
+   *                  {String} token
    *                  {String} reference_tag
    *                  {String} token_type
    *                  {Date} token_expires_at
    *                  {String} only_allow_access_to
    *                  {String} created_by
    *                  {String} token_guid
-   *                  {String} token
 	 * @api private
 	 */
   createToken: function(what_kind_of_token, options) {
@@ -33,6 +35,7 @@ exports.token = {
     var // validate inputs and set certain defaults
         token_length = ((options.token_length == null) ? 40 : options.token_length),
         reference_tag = ((options.reference_tag == null) ? null : options.reference_tag),
+        owner_primary_key = ((options.owner_primary_key == null) ? null : options.owner_primary_key),
         token_type = ((options.token_type == null) ? null : options.token_type),
         only_allow_access_to = ((options.only_allow_access_to == null) ? null : options.only_allow_access_to),
         created_by = ((options.created_by == null) ? null : options.created_by),
@@ -40,13 +43,13 @@ exports.token = {
         minutes_until_expiration = ((options.minutes_until_expiration == null) ? 15 : parseInt(options.minutes_until_expiration)),
         expires_at = new Date((new Date()).valueOf()+(1000*60*minutes_until_expiration)),
 
-        token = hash.randomToken(token_length),
+        token = ((options.token == null) ? hash.randomToken(token_length) : options.token),
         salt = hash.randomHash(320),
         tokenHash = hash.hashedCredentials(salt,token),
 
-
-        access_token = {
+        output_token = {
             reference_tag: reference_tag,
+            owner_primary_key: owner_primary_key,
             token_type: token_type,
             token_guid: null, 
             token: token, 
@@ -54,36 +57,63 @@ exports.token = {
             only_allow_access_to: only_allow_access_to,
             created_by: created_by
           };
-    
-    return new Promise(function(resolve,reject){
 
-      if (what_kind_of_token === "anonymous") {
-
-        models.AnonymousToken
-          .create({
+      // build generic token (type-specific attributes to be added later)
+      var dbTokenAttributes = {
             type: token_type,
             auth_token_salt: salt,
             auth_token_hash: tokenHash,
             auth_token_expires_at: expires_at,
-            only_allow_access_to: only_allow_access_to,
-            created_by: created_by
-          }).then(function(dbToken){
+            only_allow_access_to: only_allow_access_to
+      };
+
+      if (what_kind_of_token === "anonymous") {
+        dbTokenAttributes.created_by = created_by;
+      } else if (what_kind_of_token === "user") {
+        dbTokenAttributes.user_id = owner_primary_key;
+      }
+
+      return saveToken(what_kind_of_token, dbTokenAttributes, output_token);
+
+  },
+
+
+  saveToken: function(what_kind_of_token, dbTokenAttributes, returnTokenObj) {
+
+    return new Promise(function(resolve,reject){
+
+      if (what_kind_of_token === "anonymous") {
+        // create token in db
+        models.AnonymousToken
+          .create(dbTokenAttributes).then(function(dbToken){
             try {
-              access_token.token_guid = dbToken.guid;
-              resolve(access_token);
-            } catch (e) {
-              reject(e);
-            }
+              returnTokenObj.token_guid = dbToken.guid;
+              resolve(returnTokenObj);
+            } catch (e) { reject(e); }
           }).catch(function(err){
             console.log("failed to create anonymous token | "+err);
             reject(new Error(err));
           });
 
-      }
+      } else if (what_kind_of_token === "user") {
+        // create token in db
+        models.UserToken
+          .create(dbTokenAttributes).then(function(dbToken){
+            try {
+              resolve(returnTokenObj);
+            } catch (e) { reject(e); }
+          }).catch(function(err){
+            console.log("failed to create anonymous token | "+err);
+            reject(new Error(err));
+          });
 
+      } else {
+        // no type specified, so reject
+        reject(new Error());
+      }
     });
 
-  },
+  }
   
 };
 
