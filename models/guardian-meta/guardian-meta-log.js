ad5da62dcaf082432ac495e078a409287c591700
Topher <topherwhite@gmail.com>
2015-10-12 17:21:52 -0700

    added new meta models for screenhots and logs

---
+++ models/guardian-meta/guardian-meta-log.js
@@ -0,0 +1,54 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var GuardianMetaLog = sequelize.define("GuardianMetaLog", {
+    guid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    captured_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      validate: {
+        isDate: true
+      }
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+      }
+    },
+    size: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        isInt: true,
+        min: 0
+      }
+    },
+    sha1_checksum: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+        GuardianMetaLog.belongsTo(models.Guardian, {as: 'Guardian'});
+      },
+      indexes: [
+        {
+          unique: true,
+          fields: ["guid"]
+        }
+      ]
+    }
+  });
+
+  return GuardianMetaLog;
+};
