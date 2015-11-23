dfd215d65e08197795ec8e9151fa940765155149
Topher <topherwhite@gmail.com>
2015-11-23 11:11:35 -0800

    update guardian events

--- routes/v1/events.js
+++ routes/v1/events.js
@@ -32,6 +32,45 @@ router.route("/:event_id")
   })
 ;
 
+router.route("/:event_id")
+  .post(passport.authenticate("token",{session:false}), function(req,res) {
+
+    models.GuardianEvent
+      .findAll({ 
+        where: { guid: req.params.event_id }, 
+        include: [ { all: true } ],
+        limit: 1
+      }).then(function(dbEvent){
+
+        if (dbEvent.length < 1) {
+          httpError(res, 404, "database");
+        } else {
+
+          var reviewerInput = {
+            classification_reviewer: (req.body.classification_reviewer != null) ? req.body.classification_reviewer.toLowerCase() : null,
+            begins_at_reviewer: (req.body.begins_at_reviewer != null) ? new Date(req.body.begins_at_reviewer) : null,
+            duration_reviewer: (req.body.duration_reviewer != null) ? parseInt(req.body.duration_reviewer) : null
+          };
+
+          if (reviewerInput.classification_reviewer != null) { dbEvent[0].classification_reviewer = reviewerInput.classification_reviewer; }
+          if (reviewerInput.begins_at_reviewer != null) { dbEvent[0].begins_at_reviewer = reviewerInput.begins_at_reviewer; }
+          if (reviewerInput.duration_reviewer != null) { dbEvent[0].duration_reviewer = reviewerInput.duration_reviewer; }
+          
+          dbEvent[0].save();
+
+          views.models.guardianEvents(req,res,dbEvent)
+            .then(function(json){ res.status(200).json(json); });
+
+        }
+
+    }).catch(function(err){
+      console.log(err);
+      if (!!err) { httpError(res, 500, "database"); }
+    });
+
+  })
+;
+
 router.route("/:event_id/fingerprint")
   .get(passport.authenticate("token",{session:false}), function(req,res) {
 
