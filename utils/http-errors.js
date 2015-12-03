1db11282d1793dadcf33fa0b3f70b6a7ab73f4f4
Topher <topherwhite@gmail.com>
2015-12-03 15:19:21 -0800

    a bit better logging for the errors

--- utils/http-errors.js
+++ utils/http-errors.js
@@ -8,20 +8,22 @@ var options = {
 
   "500": {
     "default": "Internal Server Error",
-    "database": "Server Error While Retrieving Data"
+    "database": "Server Error While Retrieving Data",
+    "parse": "Failed to parse input"
   },
 
 };
 
 var httpError = function(res, code, context) {
-    
-  res.status(parseInt(code))
-    .json({
+  
+  var json = {
       message: ((context != null) && (options[""+code][""+context] != null)) ? options[""+code][""+context] : options[""+code]["default"],
       error: {
         status: parseInt(code)
       }
-    });
+    };
+  console.log("Error: "+json.message);
+  res.status(parseInt(code)).json(json);
 }
 
 module.exports = httpError;
