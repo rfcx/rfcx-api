2dc672d83253ff29901d67b9f3ebb339287fb5b9
Topher <topherwhite@gmail.com>
2015-12-28 11:26:36 -0800

    supporting more text on access json list

--- routes/v1/player.js
+++ routes/v1/player.js
@@ -18,13 +18,16 @@ router.route("/login")
     if (userInput.pswd == process.env.PLAYER_PASSWORD) {
 
       token.createAnonymousToken({
-        reference_tag: "player-anonymous",
-        token_type: "player-anonymous",
-        created_by: "player-anonymous",
+        reference_tag: "player-web",
+        token_type: "player-web",
+        created_by: "player-web",
         minutes_until_expiration: 360,
         allow_garbage_collection: false,
         only_allow_access_to: [ 
+          "^/v1/player/web",
           "^/v1/guardians/[012345678abcdef]{12}/audio.json$",
+          "^/v1/sites/mbang/images.json",
+          "^/v1/sites/tembe/images.json",
           "^/v1/playlists/[012345678abcdef]{8}-[012345678abcdef]{4}-[012345678abcdef]{4}-[012345678abcdef]{4}-[012345678abcdef]{12}/audio.json$"
           ]
       }).then(function(tokenInfo){
@@ -34,47 +37,7 @@ router.route("/login")
             guid: tokenInfo.token_guid,
             token: tokenInfo.token,
             expires_at: tokenInfo.token_expires_at.toISOString()
-          },
-          streams: [
-          ],
-          playlists: [
-            {
-              audio_url: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
-              image_url: "/v1/sites/mbang/images.json",
-              name: "Sunrise",
-              group: "Congo Basin"
-            },
-            {
-              audio_url: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
-              image_url: "/v1/sites/mbang/images.json",
-              name: "Sunset",
-              group: "Congo Basin"
-            },
-            {
-              audio_url: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
-              image_url: "/v1/sites/mbang/images.json",
-              name: "Midnight",
-              group: "Congo Basin"
-            },
-            {
-              audio_url: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
-              image_url: "/v1/sites/mbang/images.json",
-              name: "Sunrise",
-              group: "Amazon, Tembé Territory"
-            },
-            {
-              audio_url: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
-              image_url: "/v1/sites/mbang/images.json",
-              name: "Sunset",
-              group: "Amazon, Tembé Territory"
-            },
-            {
-              audio_url: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
-              image_url: "/v1/sites/mbang/images.json",
-              name: "Midnight",
-              group: "Amazon, Tembé Territory"
-            },
-          ]
+          }
         });
        
       }).catch(function(err){
@@ -87,6 +50,118 @@ router.route("/login")
         message: "invalid password", error: { status: 401 }
       });
     }
+
+  })
+;
+
+
+router.route("/web")
+  .get(passport.authenticate("token",{session:false}), function(req,res) {
+
+        res.status(200).json({
+
+          streams: [
+
+            {
+              name: "Amazon (LIVE)",
+              location: "",
+              urls: {
+                audio: "/v1/guardians/0bdbb4a5d567/audio.json",
+                slideshow: "/v1/sites/mbang/images.json"
+              }
+            }
+
+          ],
+          
+          playlists: [
+
+            {
+              name: "Congo Basin",
+              location: "Mbang, Cameroon",
+              playlists: [
+
+                {
+                  name: "Sunrise",
+                  urls: {
+                    audio: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
+                    slideshow: "/v1/sites/mbang/images.json"
+                  }
+                },
+
+                {
+                  name: "Sunset",
+                  urls: {
+                    audio: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
+                    slideshow: "/v1/sites/mbang/images.json"
+                  }
+                },
+
+                {
+                  name: "Midnight",
+                  urls: {
+                    audio: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
+                    slideshow: "/v1/sites/mbang/images.json"
+                  }
+                }
+
+              ]
+            },
+
+
+            {
+              name: "Amazon (Brazil)",
+              location: "Tembé Territory, Brazil",
+              playlists: [
+
+                {
+                  name: "Sunrise",
+                  urls: {
+                    audio: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
+                    slideshow: "/v1/sites/tembe/images.json"
+                  }
+                },
+
+                {
+                  name: "Sunset",
+                  urls: {
+                    audio: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
+                    slideshow: "/v1/sites/tembe/images.json"
+                  }
+                },
+
+                {
+                  name: "Midnight",
+                  urls: {
+                    audio: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
+                    slideshow: "/v1/sites/tembe/images.json"
+                  }
+                },
+
+                {
+                  name: "Tembé Ceremony",
+                  urls: {
+                    audio: "/v1/playlists/453e6376-d0b9-4ec9-8ebc-55bf336711a3.json",
+                    slideshow: "/v1/sites/tembe/images.json"
+                  }
+                }
+
+              ]
+            },
+
+
+            {
+              name: "Amazon (Ecuador)",
+              location: "Cerro Blanco, Ecuador",
+              playlists: [
+              ]
+            }
+
+
+
+
+          ]
+        });
+
 
   })
 ;
