7f91a48182605cb29425a6add7a96efd496c45d6
Topher <topherwhite@gmail.com>
2016-05-02 19:52:38 -0700

    added guardian registration endpoint

--- routes/v1/guardians.js
+++ routes/v1/guardians.js
@@ -1,6 +1,7 @@
 var models  = require("../../models");
 var express = require("express");
 var router = express.Router();
+var hash = require("../../utils/misc/hash.js").hash;
 var views = require("../../views/v1");
 var httpError = require("../../utils/http-errors.js");
 var passport = require("passport");
@@ -55,5 +56,44 @@ router.route("/:guardian_id")
 
   })
 ;
+
+router.route("/register")
+  .post(passport.authenticate("token",{session:false}), function(req,res) {
+
+    var guardianInput = {
+      guid: req.body.guid.toLowerCase(),
+      token: req.body.token.toLowerCase()
+    };
+
+    models.Guardian 
+      .findOrCreate({
+        where: { guid: guardianInput.guid }
+      }).spread(function(dbGuardian, wasCreated){
+
+        if (!wasCreated) {
+          res.status(409).json({ 
+            message: "A guardian with that guid already exists", error: { status: 409 }
+          });
+        } else {
+
+          var token_salt = hash.randomHash(320);
+          dbGuardian.auth_token_salt = token_salt;
+          dbGuardian.auth_token_hash = hash.hashedCredentials(token_salt,guardianInput.token);
+          dbGuardian.auth_token_updated_at = new Date();
+          dbGuardian.save();
+
+          res.status(200).json(
+            views.models.guardian(req,res,dbGuardian)
+            );
+
+        }
+      }).catch(function(err){
+        console.log(err);
+        res.status(500).json({
+          message: err.message, error: { status: 500 }
+        });
+      });
+  
+  });
 
 module.exports = router;
