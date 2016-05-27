46d5bef125b5b9dc01d31369c735b1e9fc859aba
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2016-05-27 22:50:25 +0300

    create hooks for getting/setting user token headers

---
+++ hooks/hooks.js
@@ -0,0 +1,18 @@
+var hooks = require('hooks');
+
+var stash = {};
+
+// hook to retrieve session on a login
+hooks.after('Authentication > Retrieve Token > Example 1', function (transaction) {
+  stash['guid'] = JSON.parse(transaction.real.body).token.guid;
+  stash['token'] = JSON.parse(transaction.real.body).token.token;
+});
+
+// hook to set the session cookie in all following requests
+hooks.beforeEach(function (transaction) {
+  if(stash['token'] != undefined && stash['guid'] != undefined){
+    transaction.request['headers']['x-auth-user'] = "token/" + stash['guid'];
+    transaction.request['headers']['x-auth-token'] = stash['token'];
+  }
+});
+
