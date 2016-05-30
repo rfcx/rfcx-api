24118615a2a7d14eab2ff6151d5a31f8758ea7e8
Topher <topherwhite@gmail.com>
2016-05-30 12:15:36 -0700

    migration to remove columns from audio table

--- models/guardian-audio/guardian-audio.js
+++ models/guardian-audio/guardian-audio.js
@@ -34,38 +34,7 @@ module.exports = function(sequelize, DataTypes) {
         min: 0
       }
     },
-    duration: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      validate: {
-        isInt: true,
-        min: 0
-      }
-    },
     capture_sample_count: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      validate: {
-        isInt: true,
-        min: 0
-      }
-    },
-    capture_format: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      unique: false,
-      validate: {
-      }
-    },
-    capture_bitrate: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      validate: {
-        isInt: true,
-        min: 0
-      }
-    },
-    capture_sample_rate: {
       type: DataTypes.INTEGER,
       allowNull: true,
       validate: {
