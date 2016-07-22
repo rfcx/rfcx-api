fa0c0c724991fc0e7c587ea14f36b5ac5cfa678a
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2016-07-22 11:42:56 +0300

    create get endpoint for analysis models

--- routes/v1/analysis/analysis.js
+++ routes/v1/analysis/analysis.js
@@ -4,13 +4,15 @@ var express = require("express");
 var router = express.Router();
 var views = require("../../../views/v1");
 var httpError = require("../../../utils/http-errors.js");
+var ApiConverter = require("../../../utils/api-converter");
+var requireUser = require("../../../middleware/authorization/authorization").requireTokenType("user");
+var passport = require("passport");
 
 router.route("/methods")
   .get(function(req,res) {
 
     models.AudioAnalysisMethod
-      .findAll({ 
-   //     where: {  }, 
+      .findAll({
         include: [{ all: true }]
       }).then(function(dbAnalysisMethods){
 
@@ -25,10 +27,37 @@ router.route("/methods")
         if (!!err) { res.status(500).json({msg:"failed to return analysis methods"}); }
       });
 
-  })
-;
+  });
 
+router.route('/models')
+  .get(passport.authenticate("token", {session: false}), requireUser, function(req, res) {
 
+    var converter = new ApiConverter("audioAnalysisModel", req);
+
+    models.AudioAnalysisModel
+      .findAll({
+        include: [{ all: true }]
+      })
+      .then(function(dbAnalysisModels){
+
+        if (dbAnalysisModels.length < 1) {
+          httpError(res, 404, "database");
+        } else {
+          var api = { type: 'audioAnalysisModels'};
+          api.data = dbAnalysisModels.map(function (dbAnalysisModel) {
+            return converter.mapSequelizeToApi(dbAnalysisModel.dataValues);
+          });
+          res.status(200).json(api);
+        }
+
+        return null;
+      })
+      .catch(function(err) {
+        console.log("failed to return analysis models | "+err);
+        if (!!err) { res.status(500).json({msg:"failed to return analysis models"}); }
+      })
+
+  });
 
 module.exports = router;
 
