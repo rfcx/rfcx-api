dfd9905672adfed17084aa8452a5b2c5630c9d11
Topher <topherwhite@gmail.com>
2016-07-22 03:19:44 -0500

    adding stashed checkins to databases

---
+++ migrations/20160722025233-add-stashed-checkin-count-to-checkins.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+
+    migration.addColumn(
+      'GuardianCheckIns',
+      'guardian_stashed_checkins',
+      {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+          isInt: true,
+          min: 0
+        }
+      }
+    );
+
+    done();
+  },
+
+  down: function(migration, DataTypes, done) {
+
+    migration.removeColumn('GuardianCheckIns', 'guardian_stashed_checkins');
+
+    done();
+  }
+};
