069fb799528051779cc32fd2436e18bb8b2d9e4b
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2016-07-28 16:50:20 +0300

    add audio data to model performance endpoint

--- routes/v1/analysis/analysis.js
+++ routes/v1/analysis/analysis.js
@@ -26,7 +26,6 @@ function getModel(req) {
   }
 }
 
-
 router.route("/methods")
   .get(function(req,res) {
 
@@ -96,9 +95,9 @@ router.route('/models/precision')
     sqlAllEvents = sqlUtils.condAdd(sqlAllEvents, opts.modelId, ' and tagged_by_model=:modelId');
     sqlAllEvents = sqlUtils.condAdd(sqlAllEvents, true, ' group by audio_id');
 
-    var sqlTrueEvents = 'SELECT m.audio_id, count(*) as count FROM ' +
+    var sqlTrueEvents = 'SELECT m.audio_id, count(*) as count, t.guid, t.measured_at as measuredAt, t.capture_sample_count, f.sample_rate FROM ' +
                           '(SELECT audio_id, begins_at_offset, ROUND(AVG(confidence)) as confidence FROM GuardianAudioTags where confidence=1 and type="label" and value=:tagValue group by audio_id, begins_at_offset) u ' +
-                          'INNER JOIN GuardianAudioTags m ON u.audio_id=m.audio_id and u.begins_at_offset=m.begins_at_offset and u.confidence=m.confidence where m.type="classification" and m.value=:tagValue ';
+                          'INNER JOIN GuardianAudioTags m ON u.audio_id=m.audio_id and u.begins_at_offset=m.begins_at_offset and u.confidence=m.confidence INNER JOIN GuardianAudio t ON t.id=m.audio_id INNER JOIN GuardianAudioFormats f ON f.id=t.format_id where m.type="classification" and m.value=:tagValue ';
 
     sqlTrueEvents = sqlUtils.condAdd(sqlTrueEvents, opts.modelId, ' and m.tagged_by_model=:modelId');
     sqlTrueEvents = sqlUtils.condAdd(sqlTrueEvents, true, ' group by m.audio_id');
@@ -149,12 +148,12 @@ router.route('/models/recall')
 
     var sqlAllEvents = 'SELECT audio_id, count(*) as count FROM (SELECT audio_id, begins_at_offset FROM GuardianAudioTags where type="label" and value="chainsaw" group by audio_id, begins_at_offset HAVING ROUND(AVG(confidence))=1) s group by audio_id';
 
-    var sqlTrueEvents = 'SELECT m.audio_id, count(*) as count FROM ' +
+    var sqlTrueEvents = 'SELECT m.audio_id, count(*) as count, t.guid, t.measured_at as measuredAt, t.capture_sample_count, f.sample_rate FROM ' +
       '(SELECT audio_id, begins_at_offset, ROUND(AVG(confidence)) as confidence FROM GuardianAudioTags where confidence=1 and type="label" and value=:tagValue group by audio_id, begins_at_offset) u ' +
-      'INNER JOIN GuardianAudioTags m ON u.audio_id=m.audio_id and u.begins_at_offset=m.begins_at_offset and u.confidence=m.confidence where m.type="classification" and m.value=:tagValue ';
+      'INNER JOIN GuardianAudioTags m ON u.audio_id=m.audio_id and u.begins_at_offset=m.begins_at_offset and u.confidence=m.confidence INNER JOIN GuardianAudio t ON t.id=m.audio_id INNER JOIN GuardianAudioFormats f ON f.id=t.format_id where m.type="classification" and m.value=:tagValue ';
 
-    sqlTrueEvents = sqlUtils.condAdd(sqlTrueEvents, opts.modelId, ' and m.tagged_by_model=:modelId');
-    sqlTrueEvents = sqlUtils.condAdd(sqlTrueEvents, true, ' group by m.audio_id');
+    sqlTrueEvents = sqlUtils.condAdd(sqlTrueEvents, opts.modelId, ' and m.tagged_by_model=:modelId ');
+    sqlTrueEvents = sqlUtils.condAdd(sqlTrueEvents, true, ' group by m.audio_id ');
 
     getModel(req)
       .then(function(model) {
@@ -169,6 +168,14 @@ router.route('/models/recall')
         return models.sequelize.query(sqlTrueEvents,
           { replacements: opts, type: models.sequelize.QueryTypes.SELECT }
         ).then(function(dataTrueEvents) {
+
+            dataTrueEvents.forEach(function(item) {
+              if (item.capture_sample_count && item.sample_rate) {
+                item.duration = Math.round(1000 * item.capture_sample_count / item.sample_rate);
+                delete item.capture_sample_count;
+                delete item.sample_rate;
+              }
+            });
 
             var api = converter.mapSequelizeToApi({
               all: dataAllEvents,
