9c86b284dc14307a0f486e840107e67b8bcce6d0
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2016-08-18 16:15:01 +0300

    create db migration for adding a playback_count column (re #72)

---
+++ migrations/20160818155930-add-playback-count-to-guardianaudiotags.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+
+    migration.addColumn(
+      'GuardianAudioTags',
+      'playback_count',
+      {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+          isInt: true,
+          min: 1
+        }
+      }
+    );
+
+    done();
+  },
+
+  down: function(migration, DataTypes, done) {
+
+    migration.removeColumn('GuardianAudioTags', 'playback_count');
+
+    done();
+  }
+};
