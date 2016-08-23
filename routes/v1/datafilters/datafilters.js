caba839ac9ec3696e7fe487f69774036e7e9f4fd
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2016-08-23 17:28:37 +0300

    extend datafilters endpoint with audio guid filter (re #13)

--- routes/v1/datafilters/datafilters.js
+++ routes/v1/datafilters/datafilters.js
@@ -36,6 +36,7 @@ function filter(filterOpts) {
   sql = condAdd(sql, filterOpts.tagValues, ' and t.value in (:tagValues)');
   sql = condAdd(sql, filterOpts.lowConfidence, ' and t.confidence <= 0.5');
   sql = condAdd(sql, filterOpts.highConfidence, ' and t.confidence > 0.5');
+  sql = condAdd(sql, filterOpts.audioGuids, ' and a.guid in (:audioGuids)');
   sql = condAdd(sql, filterOpts.hasLabels, ' group by a.guid having count(DISTINCT t.tagged_by_user) >= 1');
   sql = condAdd(sql, !filterOpts.hasLabels, ' group by a.guid having count(DISTINCT t.tagged_by_user) < 3 order by count(DISTINCT t.tagged_by_user) DESC, RAND()');
   sql = condAdd(sql, filterOpts.limit, ' LIMIT :limit');
@@ -60,6 +61,7 @@ function getLabelsData(filterOpts) {
   sql = condAdd(sql, filterOpts.tagValues, ' and value in (:tagValues)');
   sql = condAdd(sql, filterOpts.start, ' and a.measured_at >= :start');
   sql = condAdd(sql, filterOpts.end, ' and a.measured_at < :end');
+  sql = condAdd(sql, filterOpts.audioGuids, ' and a.guid in (:audioGuids)');
   sql = condAdd(sql, true, ' group by t.audio_id, begins_at_offset having count(DISTINCT t.tagged_by_user) >= 1 order by t.begins_at_offset ASC');
 
   return models.sequelize.query(sql,
@@ -114,6 +116,10 @@ router.route("/labelling/:tagValues?")
 
     if (req.query.lowConfidence) {
       filterOpts.lowConfidence = Boolean(req.query.lowConfidence);
+    }
+
+    if (req.query.audioGuids) {
+      filterOpts.audioGuids = req.query.audioGuids.split(',');
     }
 
     // if tag was specified, then flip coin
