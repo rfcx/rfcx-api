7807c9e4d5d6c67396636e9fc0048cbb7c4e31db
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2016-11-08 12:58:52 +0300

    make training set name unique attribute

--- models/audio-analysis/audio-analysis-training-set.js
+++ models/audio-analysis/audio-analysis-training-set.js
@@ -10,7 +10,7 @@ module.exports = function(sequelize, DataTypes) {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: false,
+      unique: true,
       validate: {
       }
     }
