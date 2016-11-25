815276e454b36aaf4dee86bed4956f4654f7b73c
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2016-11-25 12:25:08 +0300

    refactor audio/:audio_id/tags endpoint; remove previously created tags by model

--- routes/v1/audio/audio-tags.js
+++ routes/v1/audio/audio-tags.js
@@ -5,99 +5,124 @@ var router = express.Router();
 var querystring = require("querystring");
 var fs = require("fs");
 var passport = require("passport");
+var httpError = require("../../../utils/http-errors.js");
 passport.use(require("../../../middleware/passport-token").TokenStrategy);
 
 router.route("/:audio_id/tags")
   .post(passport.authenticate("token",{session:false}), function(req, res) {
 
-    // models.Guardian
-    //   .findOne( { where: { guid: req.params.guardian_id } })
-    //   .then(function(dbGuardian){
+    var analysisResults;
 
-    //     models.GuardianCheckIn
-    //       .findOne( { where: { guid: req.params.checkin_id } })
-    //       .then(function(dbCheckIn){
+    try {
+      analysisResults = JSON.parse(req.body.json);
+    }
+    catch (e) {
+      return httpError(res, 400, null, 'Failed to parse json data');
+    }
 
-            models.GuardianAudio
-              .findOne( { where: { guid: req.params.audio_id } })
-              .then(function(dbAudio){
-                  
-                var analysisResults = JSON.parse(req.body.json);
+    if (!analysisResults.results || !analysisResults.results.length) {
+      return httpError(res, 400, null, 'Request payload doesn\'t contain tags');
+    }
 
-                models.AudioAnalysisModel
-                  .findOne( { where: { guid: analysisResults.model } })
-                  .then(function(dbModel){
+    return models.GuardianAudio
+      .findOne( { where: { guid: req.params.audio_id } })
+      .bind({})
+      .then(function(dbAudio) {
+        if (!dbAudio) {
+          throw new Error('Audio with given guid not found');
+        }
+        this.dbAudio = dbAudio;
+        return models.AudioAnalysisModel.findOne({where: {guid: analysisResults.model}});
+      })
+      .then(function(dbModel){
+        if (!dbModel) {
+          throw new Error('Model with given guid not found');
+        }
+        this.dbModel = dbModel;
 
-                    if (analysisResults.results.length > 0) {
+        var removePromises = [];
 
-                      var preInsertGuardianAudioTags = [];
+        // if model has already classified this file, then remove all previous tags
+        for (var wndwInd in analysisResults.results) {
+          if (analysisResults.results.hasOwnProperty(wndwInd)) {
+            var currentWindow = analysisResults.results[wndwInd];
 
-                      for (wndwInd in analysisResults.results) {
-                        var currentWindow = analysisResults.results[wndwInd];
+            for (var tagName in currentWindow.classifications) {
+              if (currentWindow.classifications.hasOwnProperty(tagName)) {
+                if (tagName.toLowerCase() !== "ambient") {
 
-                        var beginsAt = new Date((dbAudio.measured_at.valueOf()+parseInt(currentWindow.window[0])));
-                        var endsAt = new Date((dbAudio.measured_at.valueOf()+parseInt(currentWindow.window[1])));
-
-
-                        for (tagName in currentWindow.classifications) {
-
-//                          if (currentWindow.classifications[tagName] > 0.5) {
-                          if (tagName.toLowerCase() != "ambient") {
-
-                            preInsertGuardianAudioTags.push({
-                              type: "classification",
-                              value: tagName,
-                              confidence: currentWindow.classifications[tagName],
-                              begins_at: beginsAt,
-                              ends_at: endsAt,
-                              begins_at_offset: currentWindow.window[0],
-                              ends_at_offset: currentWindow.window[1],
-                              audio_id: dbAudio.id,
-                              tagged_by_model: dbModel.id
-                            });
-
-                          }
-
-                        }
-
+                  var promise = models.GuardianAudioTag
+                    .destroy({
+                      where: {
+                        audio_id: this.dbAudio.id,
+                        type: 'classification',
+                        value: tagName,
+                        tagged_by_model: dbModel.id
                       }
+                    });
+                  removePromises.push(promise);
+                }
+              }
+            }
 
-                    models.GuardianAudioTag
-                      .bulkCreate(preInsertGuardianAudioTags)
-                      .then(function(){
-                        res.status(200).json([]);
-                      }).catch(function(err){
-                        console.log("failed to save analysis windows | "+err);
-                        if (!!err) { res.status(500).json({msg:"failed to save analysis windows"}); }
-                      });
-                        
-                    } else {
-                      res.status(200).json([]);
-                    }
+          }
+        }
 
-                }).catch(function(err){
-                  console.log("failed to find model reference | "+err);
-                  if (!!err) { res.status(404).json({ message: "failed to find model reference", error: { status: 500 } }); }
-                });
+        return Promise.all(removePromises);
+      })
+      .then(function() {
 
-              }).catch(function(err){
-                console.log("failed to find audio reference | "+err);
-                if (!!err) { res.status(404).json({ message: "failed to find audio reference", error: { status: 404 } }); }
-              });
+        var preInsertGuardianAudioTags = [];
 
-      //     }).catch(function(err){
-      //       console.log("failed to find checkIn reference | "+err);
-      //       if (!!err) { res.status(404).json({ message: "failed to find checkIn reference", error: { status: 404 } }); }
-      //     });
+        for (var wndwInd in analysisResults.results) {
 
-      // }).catch(function(err){
-      //   console.log("failed to find guardian reference | "+err);
-      //   if (!!err) { res.status(404).json({ message: "failed to find guardian reference", error: { status: 404 } }); }
-      // });
+          if (analysisResults.results.hasOwnProperty(wndwInd)) {
+
+            var currentWindow = analysisResults.results[wndwInd];
+
+            var beginsAt = new Date((this.dbAudio.measured_at.valueOf() + parseInt(currentWindow.window[0])));
+            var endsAt   = new Date((this.dbAudio.measured_at.valueOf() + parseInt(currentWindow.window[1])));
+
+            for (var tagName in currentWindow.classifications) {
+
+              if (currentWindow.classifications.hasOwnProperty(tagName)) {
+
+                if (tagName.toLowerCase() !== "ambient") {
+
+                  preInsertGuardianAudioTags.push({
+                    type: "classification",
+                    value: tagName,
+                    confidence: currentWindow.classifications[tagName],
+                    begins_at: beginsAt,
+                    ends_at: endsAt,
+                    begins_at_offset: currentWindow.window[0],
+                    ends_at_offset: currentWindow.window[1],
+                    audio_id: this.dbAudio.id,
+                    tagged_by_model: this.dbModel.id
+                  });
+
+                }
+
+              }
+
+            }
+
+          }
+
+        }
+
+        return models.GuardianAudioTag.bulkCreate(preInsertGuardianAudioTags)
+      })
+      .then(function() {
+        res.status(200).json([]);
+      })
+      .catch(function(err){
+        console.log("failed to save tags | ", err);
+        if (!!err) { res.status(404).json({ message: err.message || "Failed to save tags", error: { status: 500 } }); }
+      });
 
   })
 ;
-
 
 module.exports = router;
 
