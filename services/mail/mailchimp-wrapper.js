f628f9d14649603ca318dee4f65ff7f2a40ea603
daebwae <daebwae@gmail.com>
2017-03-24 17:00:15 +0100

    Integrate Mandrill

--- services/mail/mailchimp-wrapper.js
+++ services/mail/mailchimp-wrapper.js
@@ -1,5 +1,7 @@
 const rp = require("request-promise");
 const ValidationError = require('../../utils/converter/validation-error');
+const mandrill = require('mandrill-api/mandrill');
+const Promise = require('bluebird');
 
 module.exports = {
   subsribeToList: function (listId, email) {
@@ -22,5 +24,37 @@ module.exports = {
         }
         throw new ValidationError(body.error.detail);
     });
+  },
+  sendMail: function(email_address, name, subject, message){
+
+    return new Promise(function(resolve, reject) {
+      const mandrill_client = new mandrill.Mandrill(process.env.MANDRILL_KEY);
+      var msg = {
+        "text": message,
+        "subject": subject,
+        "from_email": "contact@rfcx.org",
+        "from_name": "Rainforest Connection",
+        "to": [{
+          "email": email_address,
+          "name": name == "" ? null : name,
+          "type": "to"
+        }],
+
+        "auto_html": true
+      };
+
+      mandrill_client.messages.send({"message": msg, "async": true}, function() {
+        resolve({success: true});
+      }, function(e){
+        reject(new Error(`Error sending mail: ${e.name} - ${e.message}`));
+      });
+
+    });
+
+
+
+
+
+
   }
 };
