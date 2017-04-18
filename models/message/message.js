2347a888828a02407aa32081bd0c162e5e0069d2
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2017-04-18 17:35:44 +0300

    create new tables: Messages and MessageType

---
+++ models/message/message.js
@@ -0,0 +1,53 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var Message = sequelize.define("Message", {
+    guid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      unique: true
+    },
+    time: {
+      type: DataTypes.DATE(3),
+      defaultValue: DataTypes.NOW,
+      validate: {
+        isDate: true
+      }
+    },
+    latitude: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+      validate: {
+        isFloat: true,
+        min: -90,
+        max: 90
+      }
+    },
+    longitude: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+      validate: {
+        isFloat: true,
+        min: -180,
+        max: 180
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Message.belongsTo(models.MessageType, { as: 'Type', foreignKey: "type" });
+        Message.belongsTo(models.User, { as: "UserFrom", foreignKey: "from_user" });
+        Message.belongsTo(models.User, { as: "UserTo", foreignKey: "to_user" });
+      },
+      indexes: [
+        {
+          unique: true,
+          fields: ["guid"]
+        }
+      ]
+    },
+    tableName: "Messages"
+  });
+
+  return Message;
+};
