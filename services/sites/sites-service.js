80e8edd31176bcbe91ac9ae4b024d0061895de88
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2017-06-05 17:39:24 +0300

    create service for sites

---
+++ services/sites/sites-service.js
@@ -0,0 +1,19 @@
+var models = require("../../models");
+var sequelize = require("sequelize");
+
+function getSiteByGuid(guid) {
+  return models.GuardianSite
+    .findOne({
+      where: { guid: guid }
+    })
+    .then((site) => {
+      if (!site) {
+        throw new sequelize.EmptyResultError('Site with given guid not found.');
+      }
+      return site;
+    });
+}
+
+module.exports = {
+  getSiteByGuid: getSiteByGuid,
+}
