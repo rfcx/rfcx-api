3d4d3b3abdf43c535113c87060fbd58143c0a2a7
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2017-07-03 14:42:16 +0300

    RA-4 #comment Use winston combined with morgan for nodejs logging

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ if (process.env.NODE_ENV === "production") {
 var express = require("express"),
     path = require("path"),
     favicon = require("serve-favicon"),
-    logger = require("morgan"),
+    morgan = require("morgan"),
+    logger = require('./utils/logger');
     multer = require("multer"),
     passport = require("passport"),
     cors = require("cors"),
@@ -24,7 +25,7 @@ app.set("title", "rfcx-api");
 app.set("port", process.env.PORT || 8080);
 app.use(favicon(__dirname + "/public/img/logo/favicon.ico"));
 app.use(cors()); // TO-DO: Currently enables CORS for all requests. We may have a reason to limit this in the future...
-app.use(logger("dev"));
+app.use(morgan('combined', { "stream": logger.stream }));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(multer(require("./config/multer").config(process.env)));
