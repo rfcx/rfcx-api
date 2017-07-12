ce98d58ae4e24ed8fb226191305dedb3e531cdc4
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2017-07-12 15:11:42 +0300

    add error handler for checkins route

--- routes/v1/guardians/guardians-checkins.js
+++ routes/v1/guardians/guardians-checkins.js
@@ -160,6 +160,10 @@ router.route("/:guardian_id/checkins")
       .then(function() {
         loggers.debugLogger.log('Guardian checkins endpoint: return json', { req: req, json: returnJson });
         return res.status(200).json(returnJson);
+      })
+      .catch(function(err) {
+        loggers.errorLogger.log('Failed to save checkin', { req: req, err: err });
+        httpError(res, 500, err, 'failed to save checkin');
       });
   })
 ;
