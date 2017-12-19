2e1e18d598e2a839f98dfcf0c78aa01184644b30
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2017-12-19 12:40:31 +0300

    add distance attribute to Report model

--- models/report.js
+++ models/report.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Creates a repository of reports for incidents 
+// Creates a repository of reports for incidents
 // like 'heard a chainsaw' or 'poacher sighting'
 module.exports = function(sequelize, DataTypes) {
   var Report = sequelize.define('Report', {
@@ -13,7 +13,16 @@ module.exports = function(sequelize, DataTypes) {
     end_time: DataTypes.DATE,
     long: DataTypes.REAL,
     lat: DataTypes.REAL,
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    distance: { // 0 is immediate area (nearby), 50 is not far away (but not visible), 100 very far (faintly heard)
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: true,
+        min: 0,
+        max: 100
+      },
+      allowNull: true
+    }
   }, {
     classMethods: {
       associate: function(models) {
