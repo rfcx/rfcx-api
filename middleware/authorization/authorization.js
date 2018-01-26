f2ed8d15af84ea26ed4ee34d069e35494f64426f
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2018-01-26 16:45:13 +0300

    initial jwt middleware implementation

--- middleware/authorization/authorization.js
+++ middleware/authorization/authorization.js
@@ -1,20 +1,36 @@
 var httpError = require('../../utils/http-errors');
+var passport = require("passport");
+let checkJwt = require('../../utils/jwt');
+passport.use(require("../passport-token").TokenStrategy);
 
 // Factory method to create a token type authorization middleware
 function requireTokenType(type) {
-    // curry
-    return function (req, res, next) {
-        if(req.rfcx.auth_token_info.type != type) {
-            httpError(res, 403, "token");
-            req.end();
-        } else {
-            next();
-        }
-    };
+  // curry
+  return function (req, res, next) {
+    if(req.rfcx.auth_token_info.type != type) {
+      httpError(res, 403, "token");
+      req.end();
+    } else {
+      next();
+    }
+  };
 }
 
-
+function ensureAuthenticated(req, res, next) {
+  passport.authenticate('token', {session: false}, function(err, user) {
+    if (err) {
+      return next(err);
+    }
+    if (user) {
+      return next();
+    }
+    else {
+      checkJwt(req, res, next);
+    }
+  })(req, res, next);
+}
 
 module.exports = {
-    requireTokenType: requireTokenType
+  requireTokenType,
+  ensureAuthenticated,
 };
