3f4a93d45c6cb82bf6e1f9fa1cb68c61ad055c10
Topher <topherwhite@gmail.com>
2018-02-20 14:40:21 -0800

    breaking down mqtt functionality

--- app-mqtt.js
+++ app-mqtt.js
@@ -10,33 +10,44 @@ if (process.env.NODE_ENV === "production") {
   process.env.NEW_RELIC_HOME = __dirname+"/config"; require("newrelic");
 }
 
-var express = require("express"),
-    path = require("path"),
-    favicon = require("serve-favicon");
- // var expressWinston = require('express-winston');
-var loggers = require('./utils/logger');
-//var multer = require("multer"),
-//  passport = require("passport"),
-//  cors = require("cors"),
-  // bodyParser = require("body-parser"),
-// var addRequestId = require('express-request-id');
-// var addInstanceId = require('./middleware/misc/aws').addInstanceId;
-var toobusy = require('toobusy-js');
-var app = express();
+var express = require("express"), path = require("path"), favicon = require("serve-favicon"), loggers = require('./utils/logger'), toobusy = require('toobusy-js'), mqtt = require("mqtt");
+var app = { http: express(), mqtt: null };
 
-app.set("title", "rfcx-api-mqtt");
-app.set("port", process.env.PORT || 8080);
-//app.use(addInstanceId);
-app.use(favicon(__dirname + "/public/img/logo/favicon.ico"));
-app.use(function(req, res, next) { if (toobusy()) { loggers.errorLogger.log('Server is too busy to handle request', { req: req, info: { url: req.url, body: req.body } }); } next(); });
-app.use(express.static(path.join(__dirname, "public")));
+
+
+app.mqtt = mqtt.connect( "tcp://mqtt.rfcx.org", { clientId: "rfcx-api-mqtt", protocolId: "MQIsdp",  protocolVersion: 3,  connectTimeout: 1000, debug: true });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.http.set("title", "rfcx-api-mqtt");
+app.http.set("port", process.env.PORT || 8080);
+app.http.use(favicon(__dirname + "/public/img/logo/favicon.ico"));
+app.http.use(function(req, res, next) { if (toobusy()) { loggers.errorLogger.log('Server is too busy to handle request', { req: req, info: { url: req.url, body: req.body } }); } next(); });
+app.http.use(express.static(path.join(__dirname, "public")));
 
 // Health Check HTTP Endpoint
 var healthCheck = require("./utils/internal-rfcx/health-check.js").healthCheck;
-app.get("/health_check", function(req,res){ healthCheck.httpResponse(req,res); });
+app.http.get("/health_check", function(req,res){ healthCheck.httpResponse(req,res); });
 
 // Default HTTP Endpoint
-app.get('/',function(req,res){
+app.http.get('/',function(req,res){
   res.status(200).json({
     name: 'Rainforest Connection (RFCx)',
     message: 'Access to this API requires authentication (mqtt). Please send requests for access by email to contact@rfcx.org',
@@ -45,11 +56,30 @@ app.get('/',function(req,res){
 });
 
 // Catch & Report Various HTTP Errors (needs some work)
-app.use(function(req, res, next) { var err = new Error('Not Found'); err.status = 404; next(err); });
-app.use(function(err, req, res, next) {
+app.http.use(function(req, res, next) { var err = new Error('Not Found'); err.status = 404; next(err); });
+app.http.use(function(err, req, res, next) {
   var status = err.status || 500;
   loggers.errorLogger.log('Express.js error handler', { req: req, url: req.url, status: status, err: err });
   res.status(status).json({ message: err.message, error: err });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
