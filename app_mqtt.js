9816fe4145a59dab85787e44d172a077c189596f
Topher <topherwhite@gmail.com>
2018-02-20 12:53:38 -0800

    package.json changes

--- app_mqtt.js
+++ app_mqtt.js
@@ -169,7 +169,7 @@ app.get("/health_check", function(req,res){ healthCheck.httpResponse(req,res); }
 app.get('/',function(req,res){
   res.status(200).json({
     name: 'Rainforest Connection (RFCx)',
-    message: 'Access to this API requires authentication. Please send requests for access by email to contact@rfcx.org',
+    message: 'Access to this API requires authentication (mqtt). Please send requests for access by email to contact@rfcx.org',
     info: 'https://rfcx.org/'
   });
 });
