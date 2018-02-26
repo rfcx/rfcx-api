516dc430005c5ee3806f64d2bdf0bc9b94d1fe3a
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2018-02-26 12:07:24 +0300

    create analysis entries with state perc_queued after audio checkin

--- services/analysis/analysis-service.js
+++ services/analysis/analysis-service.js
@@ -1,6 +1,5 @@
 
 function findEntityByParams(audioGuid, AIGuid) {
-
   return models.AudioAnalysisEntity
     .findOne({
       include: [
@@ -16,9 +15,26 @@ function findEntityByParams(audioGuid, AIGuid) {
         }
       ]
     });
+}
 
+function findStateByName(name) {
+  return models.AudioAnalysisState
+    .findOne({
+      where: { name }
+    });
+}
+
+function createEntity(guardian_audio_id, audio_analysis_model_id, state) {
+  return models.AudioAnalysisEntry
+    .create({
+      guardian_audio_id,
+      audio_analysis_model_id,
+      state
+    });
 }
 
 module.exports = {
-
+  findEntityByParams,
+  findStateByName,
+  createEntity,
 };
