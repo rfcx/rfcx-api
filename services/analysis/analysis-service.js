f3d05e8c55d752977b1afa768f2a7f17e0b04d70
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2018-02-26 18:06:59 +0300

    simplify entry search filter

--- services/analysis/analysis-service.js
+++ services/analysis/analysis-service.js
@@ -2,22 +2,15 @@ const sequelize = require("sequelize");
 const ValidationError = require("../../utils/converter/validation-error");
 const models  = require("../../models");
 const Promise = require("bluebird");
+const logDebug = require('../../utils/logger').debugLogger.log;
 
 function findEntityByParams(guardian_audio_id, audio_analysis_model_id) {
-  return models.AudioAnalysisEntity
+  return models.AudioAnalysisEntry
     .findOne({
-      include: [
-        {
-          model: models.GuardianAudio,
-          as: 'Audio',
-          where: { 'id': guardian_audio_id }
-        },
-        {
-          modesl: models.AudioAnalysisModel,
-          as: 'AI',
-          where: { 'id': audio_analysis_model_id }
-        }
-      ]
+      where: {
+        guardian_audio_id,
+        audio_analysis_model_id,
+      }
     });
 }
 
