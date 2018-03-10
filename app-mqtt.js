1f5deb522898878c45d9db8bfd128faeeb5860f9
Topher <topherwhite@gmail.com>
2018-03-09 20:18:53 -0800

    http endpoints with auth

--- app-mqtt.js
+++ app-mqtt.js
@@ -10,8 +10,9 @@ if (process.env.NODE_ENV === "production") {
   process.env.NEW_RELIC_HOME = __dirname+"/config"; require("newrelic");
 }
 
-var express = require("express"), path = require("path"), favicon = require("serve-favicon"), loggers = require('./utils/logger'), toobusy = require('toobusy-js'), mqtt = require("mqtt");
-var app = { http: express(), mqtt: null };
+var app = { http: null, mqtt: null };
+
+var mqtt = require("mqtt");
 
 app.mqtt = mqtt.connect({ 
     clientId: "rfcx-api-mqtt-"+process.env.NODE_ENV, 
@@ -29,18 +30,39 @@ app.mqtt = mqtt.connect({
   });
 
 
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var expressWinston = require('express-winston');
+var loggers = require("./utils/logger");
+var passport = require("passport");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var addRequestId = require('express-request-id');
+//var addInstanceId = require('./middleware/misc/aws').addInstanceId;
+var toobusy = require("toobusy-js");
 
-
-
-
-
-
-
+app.http = express();
 app.http.set("title", "rfcx-api-mqtt");
 app.http.set("port", process.env.PORT || 8080);
 app.http.use(favicon(__dirname + "/public/img/logo/favicon.ico"));
+app.http.use(cors());
 app.http.use(function(req, res, next) { if (toobusy()) { loggers.errorLogger.log('Server is too busy to handle request', { req: req, info: { url: req.url, body: req.body } }); } next(); });
-app.http.use(express.static(path.join(__dirname, "public")));
+app.http.use(bodyParser.urlencoded({extended:false}));
+app.http.use(bodyParser.json({ limit: '1mb' }));
+app.http.use(multer(require("./config/multer").config(process.env)));
+app.http.use(passport.initialize());
+
+// app.http.use(express.static(path.join(__dirname, "public")));
+
+// Middleware
+app.http.use("/v1/", require("./middleware/v1.js").middleware.setApiParams);
+app.http.use("/v1/", require("./middleware/v1.js").middleware.insecureRequestRedirect);
+
+// Guardian Update Endpoints
+app.http.use("/v1/guardians", require("./utils/rfcx-mqtt/http/guardians-register"));
+//app.http.use("/v1/guardians", require("./utils/rfcx-mqtt/http/guardians-software"));
 
 // Health Check HTTP Endpoint
 var healthCheck = require("./utils/rfcx-mqtt/health-check-mqtt.js").healthCheck;
---
+++ utils/rfcx-mqtt/http/middleware-v1.js
@@ -0,0 +1,75 @@
+var path = require("path");
+var url = require("url");
+
+exports.middleware = {
+
+  setApiParams: function(req, res, next) {
+
+    var requestStartTime = (new Date()).valueOf();
+
+    var apiUrlProtocol = ((req.headers["x-forwarded-proto"] != null) ? req.headers["x-forwarded-proto"] : req.protocol);
+    var apiUrlDomain = apiUrlProtocol+"://"+req.headers.host;
+
+    var paramLimit = (req.query.limit == null) ? 20 : parseInt(req.query.limit);
+    if (paramLimit > 1000) { paramLimit = 1000; } else if (paramLimit < 1) { paramLimit = 1; }
+
+    var paramOffset = (req.query.offset == null) ? 0 : parseInt(req.query.offset);
+
+    var paramAfter = (req.query.starting_after == null) ? null : (isNaN(Number(req.query.starting_after))) ? (new Date(""+req.query.starting_after)) : (new Date(parseInt(req.query.starting_after)));
+    var paramBefore = (req.query.ending_before == null) ? null : (isNaN(Number(req.query.ending_before))) ? (new Date(""+req.query.ending_before)) : (new Date(parseInt(req.query.ending_before)));
+    var fallbackDurationInMinutes = 60;
+    if ((paramAfter != null) && (paramBefore == null)) { paramBefore = new Date(paramAfter.valueOf()+fallbackDurationInMinutes*60*1000); }
+    if ((paramBefore != null) && (paramAfter == null)) { paramAfter = new Date(paramBefore.valueOf()-fallbackDurationInMinutes*60*1000); }
+
+    var paramOrder = (req.query.order == null || typeof(req.query.order) !== 'string') ? null : ((req.query.order.toLowerCase() == "ascending") ? "ASC" : "DESC");
+
+    var contentType = path.extname(req.path).trim().substr(1);
+    if (contentType.trim().length == 0) { contentType = "json"; }
+    var urlPath = "/v1"+url.parse(req.url).pathname;
+    req.url = req.url.replace("."+contentType,"");
+
+    var authUser = { header: null, query: null, body: null };
+    if (req.headers["x-auth-user"] != null) { authUser.header = req.headers["x-auth-user"]; }
+    if (req.query["auth_user"] != null) { authUser.query = req.query["auth_user"]; }
+    if (req.body["auth_user"] != null) { authUser.body = req.body["auth_user"]; }
+
+    var apiVersion = { header: null, query: null, body: null };
+    if (req.headers["x-rfcx-version"] != null) { apiVersion.header = req.headers["x-rfcx-version"]; }
+    if (req.query["rfcx_version"] != null) { apiVersion.query = req.query["rfcx_version"]; }
+    if (req.body["rfcx_version"] != null) { apiVersion.body = req.body["rfcx_version"]; }
+
+    req.rfcx = {
+      request_start_time: requestStartTime,
+      api_url_protocol: apiUrlProtocol,
+      api_url_domain: apiUrlDomain,
+      api_version: apiVersion,
+      url_path: urlPath,
+      limit: paramLimit,
+      offset: paramOffset,
+      starting_after: paramAfter,
+      ending_before: paramBefore,
+      order: paramOrder,
+      content_type: contentType.toLowerCase(),
+      auth_user: authUser
+    };
+
+    next();
+  },
+
+  insecureRequestRedirect: function(req, res, next) {
+
+    var allowedOverInsecureConnection = [ "rf.cx", "api-insecure.rfcx.org", "checkin.rfcx.org" ];
+
+    if (    ((process.env.NODE_ENV === "production") || (process.env.NODE_ENV === "staging"))
+        &&  (req.rfcx.api_url_protocol === "http")
+        &&  (allowedOverInsecureConnection.indexOf(req.headers.host) < 0)
+        ) {
+          res.redirect("https://"+req.headers.host+req.rfcx.url_path);
+    } else {
+
+      next();
+    }
+
+  }
+
+}
