8c1922d80c8cdeaf2711d43c8b9c77afae1430bd
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2018-04-19 13:11:27 +0300

    create migration to add comment attr into events table

---
+++ migrations/20180419125334-add-comment-attr-to-events.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+    return queryInterface.addColumn(
+      'GuardianAudioEvents',
+      'comment',
+      {
+        type: Sequelize.TEXT('long'),
+        allowNull: true,
+        defaultValue: null
+      }
+    );
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+
+    return queryInterface.removeColumn('GuardianAudioEvents', 'comment');
+
+  }
+};
