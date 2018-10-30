c221df288705daeec57ea3001df318163f0117dd
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2018-10-30 16:25:33 +0300

    set private key prefix and postfix in the code

--- utils/firebase/firebase.js
+++ utils/firebase/firebase.js
@@ -3,13 +3,13 @@ const admin = require("firebase-admin");
 const rangerAppCert = admin.credential.cert({
   projectId: 'rfcx-ranger',
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL_RANGER_APP,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY_RANGER_APP
+  privateKey: `-----BEGIN PRIVATE KEY-----\n${process.env.FIREBASE_PRIVATE_KEY_RANGER_APP}\n-----END PRIVATE KEY-----\n`
 });
 
 const playerAppCert = admin.credential.cert({
   projectId: 'rfcx-stream',
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL_PLAYER_APP,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY_PLAYER_APP
+  privateKey: `-----BEGIN PRIVATE KEY-----\n${process.env.FIREBASE_PRIVATE_KEY_PLAYER_APP}\n-----END PRIVATE KEY-----\n`
 });
 
 const rangerApp = admin.initializeApp({ credential: rangerAppCert }, 'rangerApp');
