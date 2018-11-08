3273634a30dc2b4699f7e719565dc5f3fc5816a6
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2018-11-08 18:25:12 +0300

    update report model and create migration

--- models/report.js
+++ models/report.js
@@ -1,19 +1,38 @@
 'use strict';
 
-// Creates a repository of reports for incidents
-// like 'heard a chainsaw' or 'poacher sighting'
 module.exports = function(sequelize, DataTypes) {
   var Report = sequelize.define('Report', {
     guid: {
       type:DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
       unique: true
     },
-    start_time: DataTypes.DATE,
-    end_time: DataTypes.DATE,
-    long: DataTypes.REAL,
-    lat: DataTypes.REAL,
-    type: DataTypes.STRING,
+    reported_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        isDate: true
+      }
+    },
+    lat: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        isFloat: true,
+        min: -90,
+        max: 90
+      }
+    },
+    long: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        isFloat: true,
+        min: -180,
+        max: 180
+      }
+    },
     distance: { // 0 is immediate area (nearby), 50 is not far away (but not visible), 100 very far (faintly heard)
       type: DataTypes.INTEGER,
       validate: {
@@ -21,12 +40,33 @@ module.exports = function(sequelize, DataTypes) {
         min: 0,
         max: 100
       },
-      allowNull: true
+      allowNull: true,
+    },
+    age_estimate: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: true,
+        min: {
+          args: 0,
+          msg: 'age_estimate should be equal or greater than 0'
+        },
+        max: {
+          args: 30,
+          msg: 'age_estimate should be equal or less than 30'
+        }
+      },
+    },
+    audio: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false,
+      validate: { }
     }
   }, {
     classMethods: {
       associate: function(models) {
-        Report.belongsTo(models.User, {foreignKey: 'reporter'});
+        Report.belongsTo(models.GuardianAudioEventValue, { as: 'Value', foreignKey: "value" });
+        Report.belongsTo(models.User, { foreignKey: 'reporter' });
       }
     }
   });
