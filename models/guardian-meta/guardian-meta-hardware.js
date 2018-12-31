34d28142ced0f395bde86d502b570eaf4fb7170f
Topher <topherwhite@gmail.com>
2018-12-31 10:58:18 -0800

    added guardian hardware model definition

---
+++ models/guardian-meta/guardian-meta-hardware.js
@@ -0,0 +1,84 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var GuardianMetaHardware = sequelize.define('GuardianMetaHardware', {
+
+    manufacturer: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+      }
+    },
+
+    model: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+      }
+    },
+
+    brand: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+      }
+    },
+
+    product: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+      }
+    },
+
+    android_version: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+      }
+    },  
+
+    phone_sim_carrier: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+      }
+    },  
+
+    phone_sim_serial: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+      }
+    },  
+
+    phone_imsi: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+      }
+    },
+
+    phone_imei: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+      }
+    },
+
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+      }
+    }
+
+  }, {
+    classMethods: {
+      associate: function(models) {
+        GuardianMetaHardware.belongsTo(models.Guardian, {as: 'Guardian'});
+      }
+    },
+    tableName: "GuardianMetaHardware"
+  });
+  return GuardianMetaHardware;
+};
