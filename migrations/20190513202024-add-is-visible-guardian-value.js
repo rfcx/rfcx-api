32421b3baf467b77d8a0ce326869d2685681f193
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2019-05-15 10:46:35 +0300

    add is_visible column to Guardians table

---
+++ migrations/20190513202024-add-is-visible-guardian-value.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+    return queryInterface.addColumn(
+      'Guardians',
+      'is_visible',
+      {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: true,
+        validate: {}
+      }
+    );
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+
+    return queryInterface.removeColumn('Guardians', 'is_visible');
+
+  }
+
+};
