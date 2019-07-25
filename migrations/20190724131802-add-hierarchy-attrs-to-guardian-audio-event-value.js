ac4243c97a55a5c14d7229b43bef07e024f37066
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2019-07-25 15:28:59 +0300

    implement labels hierarchy

---
+++ migrations/20190724131802-add-hierarchy-attrs-to-guardian-audio-event-value.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize, done) {
+
+    var sqlAddColumnModel = "ALTER TABLE `GuardianAudioEventValues`" +
+      " ADD COLUMN `high_level_key` INTEGER DEFAULT NULL" +
+      ", ADD FOREIGN KEY (`high_level_key`) REFERENCES `GuardianAudioEventValueHighLevelKeys`(`id`)" +
+      " ON UPDATE CASCADE ON DELETE RESTRICT";
+
+    queryInterface.sequelize.query(sqlAddColumnModel, {
+      type: queryInterface.sequelize.QueryTypes.RAW
+    });
+
+    queryInterface.addColumn(
+      'GuardianAudioEventValues',
+      'low_level_key',
+      {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: false,
+      }
+    );
+
+    done();
+
+  },
+
+  down: function (queryInterface, Sequelize, done) {
+
+    queryInterface.removeColumn('GuardianAudioEventValues', 'high_level_key');
+    queryInterface.removeColumn('GuardianAudioEventValues', 'low_level_key');
+
+    done();
+
+  }
+
+};
--- models/guardian-audio/guardian-audio-event-value.js
+++ models/guardian-audio/guardian-audio-event-value.js
@@ -6,11 +6,17 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
-    }
+    },
+    low_level_key: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false,
+    },
   }, {
     classMethods: {
       associate: function(models) {
         GuardianAudioEventValue.belongsToMany(models.GuardianGroup, { through: models.GuardianGroupGuardianAudioEventValueRelation });
+        GuardianAudioEventValue.belongsTo(models.GuardianAudioEventValueHighLevelKey, { as: 'HighLevelKey', foreignKey: "high_level_key" });
       },
       indexes: [
       ]
