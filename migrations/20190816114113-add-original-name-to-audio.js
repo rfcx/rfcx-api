15c66eed328ccc11cf92e12c7f49c99b7722f700
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2019-08-16 12:39:19 +0300

    save original_filename to guardian audio table

---
+++ migrations/20190816114113-add-original-name-to-audio.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+    return queryInterface.addColumn(
+      'GuardianAudio',
+      'original_filename',
+      {
+        type: Sequelize.STRING,
+        allowNull: true,
+        validate: {}
+      }
+    );
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+
+    return queryInterface.removeColumn('GuardianAudio', 'original_filename');
+
+  }
+
+};
+
