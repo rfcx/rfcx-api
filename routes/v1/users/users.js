0174c5f3daae33a2dd9b1f156011c3341f5a42cc
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2019-09-05 15:20:46 +0300

    add endpoints for export users from DB for updating

--- routes/v1/users/users.js
+++ routes/v1/users/users.js
@@ -702,7 +702,15 @@ router.route("/auth0/update-user/public")
           if (body.user_metadata.family_name) {
             opts.user_metadata.family_name = transformedParams.family_name;
           }
-          opts.user_metadata.name = `${transformedParams.given_name} ${transformedParams.family_name}`;
+          if (opts.user_metadata.given_name && opts.user_metadata.family_name) {
+            opts.user_metadata.name = `${opts.user_metadata.given_name} ${opts.user_metadata.family_name}`;
+         }
+          else if (opts.user_metadata.given_name && !opts.user_metadata.family_name) {
+            opts.user_metadata.name = `${opts.user_metadata.given_name}`;
+          }
+          else if (!opts.user_metadata.given_name && opts.user_metadata.family_name) {
+            opts.user_metadata.name = `${opts.user_metadata.family_name}`;
+          }
           return auth0Service.updateAuth0User(this.token, opts);
         }
         return body;
@@ -732,6 +740,90 @@ router.route("/auth0/update-user/public")
       })
       .catch(ValidationError, e => httpError(req, res, 400, null, e.message))
       .catch((err) => {
+        res.status(500).json({ err });
+      });
+
+  });
+
+router.route("/auth0/users/export-link")
+  .get(passport.authenticate(['jwt', 'jwt-custom'], {session: false}), hasRole(['usersAdmin']), function (req, res) {
+
+    let transformedParams = {};
+    let params = new Converter(req.query, transformedParams);
+
+    params.convert('connection_id').optional().toString();
+    params.convert('limit').optional().toString();
+    params.convert('fields').optional().toArray();
+
+    params.validate()
+      .then(() => {
+        return auth0Service.getToken();
+      })
+      .bind({})
+      .then((token) => {
+        this.token = token;
+        console.log('\ntransformedParams\n', transformedParams);
+        console.log('\ntoken\n', this.token);
+        // use for getting all users by connections with database
+        return auth0Service.getAllUsersForExports(token, transformedParams);
+      })
+      .then((data) => {
+        // use for uploading csv or json file with sorting users by fields
+        console.log('\njob_ID\n', data);
+        return auth0Service.getAjob( this.token, data);
+      })
+      .then((data) => {
+        console.log('data for uploading users', data);
+        res.status(200).json(data);
+      })
+      .catch(ValidationError, e => httpError(req, res, 400, null, e.message))
+      .catch((err) => {
+        console.log('\nerror\n', err);
+        res.status(500).json({ err });
+      });
+
+  });
+
+router.route("/auth0/update-users")
+  .post(passport.authenticate(['jwt', 'jwt-custom'], {session: false}), hasRole(['usersAdmin']), function (req, res) {
+
+    let transformedParams = {};
+    let params = new Converter(req.body, transformedParams);
+
+    params.convert('users').toArray();
+
+    params.validate()
+      .then(() => {
+        return auth0Service.getToken();
+      })
+      .then((token) => {
+        console.log('\ntransformedParams\n', transformedParams);
+        let proms = [];
+        if (transformedParams.users) {
+          transformedParams.users.forEach((user) => {
+            if (user.given_name && user.family_name) {
+              return true;
+            }
+            else {
+              let opts = {
+                user_id: user.user_id,
+                given_name: user.given_name,
+                family_name: user.family_name,
+                name: user.name
+              }
+              proms.push(auth0Service.updateAuth0User(token, opts));
+            }
+          })
+        }
+        return Promise.all(proms);
+      })
+      .then((data) => {
+        console.log('data', data);
+        res.status(200).json(data);
+      })
+      .catch(ValidationError, e => httpError(req, res, 400, null, e.message))
+      .catch((err) => {
+        console.log('\nerror\n', err);
         res.status(500).json({ err });
       });
 
