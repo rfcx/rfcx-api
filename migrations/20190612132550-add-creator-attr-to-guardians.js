efa0d89fa5b3b969e11dc8c9d89f4e80e9f7008d
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2019-10-02 12:30:47 +0300

    update guardian creator migration

--- migrations/20190612132550-add-creator-attr-to-guardians.js
+++ migrations/20190612132550-add-creator-attr-to-guardians.js
@@ -3,10 +3,10 @@
 module.exports = {
   up: function (migration, DataTypes, done) {
 
-    var sqlAddColumnModel = "ALTER TABLE `Guardians`" +
-      " ADD COLUMN `creator` INTEGER DEFAULT NULL" +
-      ", ADD FOREIGN KEY (`creator`) REFERENCES `Users`(`id`)" +
-      " ON UPDATE CASCADE ON DELETE RESTRICT";
+    var sqlAddColumnModel =
+      "ALTER TABLE `Guardians` ADD COLUMN `creator` INTEGER DEFAULT NULL, " +
+      "ADD CONSTRAINT creator FOREIGN KEY (`creator`) REFERENCES `Users`(`id`) " +
+      "ON UPDATE CASCADE ON DELETE RESTRICT";
 
     migration.sequelize.query(sqlAddColumnModel, {
       type: migration.sequelize.QueryTypes.RAW
@@ -17,7 +17,12 @@ module.exports = {
 
   down: function (migration, DataTypes, done) {
 
-    migration.removeColumn('Guardians', 'creator');
+    var sqlAddColumnModel =
+      "ALTER TABLE `Guardians` DROP FOREIGN KEY creator, DROP COLUMN creator";
+
+    migration.sequelize.query(sqlAddColumnModel, {
+      type: migration.sequelize.QueryTypes.RAW
+    });
 
     done();
 
