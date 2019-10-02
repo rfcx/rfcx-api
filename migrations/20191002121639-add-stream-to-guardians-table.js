cd8d4abf77442195aa2d5467fec2c61af4a1bf9f
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2019-10-02 12:18:20 +0300

    add stream column to guardians table

---
+++ migrations/20191002121639-add-stream-to-guardians-table.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: function (migration, DataTypes, done) {
+
+    var sqlAddColumnModel =
+      "ALTER TABLE `Guardians` ADD COLUMN `stream` INTEGER DEFAULT NULL, " +
+      "ADD CONSTRAINT stream FOREIGN KEY (`stream`) REFERENCES `Streams`(`id`) " +
+      "ON UPDATE CASCADE ON DELETE RESTRICT";
+
+    migration.sequelize.query(sqlAddColumnModel, {
+      type: migration.sequelize.QueryTypes.RAW
+    });
+
+    done();
+  },
+
+  down: function (migration, DataTypes, done) {
+
+    var sqlAddColumnModel =
+      "ALTER TABLE `Guardians` DROP FOREIGN KEY stream, DROP COLUMN stream";
+
+    migration.sequelize.query(sqlAddColumnModel, {
+      type: migration.sequelize.QueryTypes.RAW
+    });
+
+    done();
+
+  }
+};
