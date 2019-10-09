b9d01451c0c78a2fb4ebcca79382070f667222c9
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2019-10-09 14:59:12 +0300

    add endpoints for compress URLs and read short links

--- routes/v1/shortlinks/shortlinks.js
+++ routes/v1/shortlinks/shortlinks.js
@@ -3,7 +3,12 @@ var models  = require("../../../models");
 var express = require("express");
 var router = express.Router();
 var httpError = require("../../../utils/http-errors.js");
-
+var Converter = require("../../../utils/converter/converter");
+var hasRole = require('../../../middleware/authorization/authorization').hasRole;
+const generator = require('generate-password');
+const redis = require('../../../utils/redis');
+const ValidationError = require("../../../utils/converter/validation-error");
+const EmptyResultError = require('../../../utils/converter/empty-result-error');
 
 router.route("/:shortlink_id")
   .get(function(req,res) {
@@ -21,14 +26,14 @@ router.route("/:shortlink_id")
     } else {
 
       models.ShortLink
-        .findOne({ 
+        .findOne({
           where: { guid: linkId },
           include: [ { all: true } ]
         }).then(function(dbShortLink){
-          
+
           dbShortLink.access_count = 1 + dbShortLink.access_count;
           dbShortLink.save();
-          
+
           console.log("redirecting client to: '"+dbShortLink.url+"'");
           res.redirect(301, dbShortLink.url );
 
@@ -43,13 +48,90 @@ router.route("/:shortlink_id")
   })
 ;
 
+router.route("/s/:hash")
+  .get(function(req,res) {
+
+    let hash = req.params.hash;
+    redis.getAsync(hash)
+      .then((url) => {
+        if (!url) {
+          return httpError(req, res, 404, null, 'Short link not found.');
+        }
+        res.redirect(301, url);
+      })
+      .catch(e => { httpError(req, res, 500, e, "Error while getting the short link."); console.log(e) });
+
+})
+
 router.route("/")
   .get(function(req,res) {
     res.redirect(301, "https://rfcx.org/");
   })
 ;
 
+router.route("/")
+  .post(passport.authenticate(['token', 'jwt', 'jwt-custom'], {session: false}), hasRole(['rfcxUser']), function (req, res) {
 
+    let transformedParams = {};
+    let params = new Converter(req.body, transformedParams);
+
+    params.convert('url').toString();
+    params.convert('type').optional().toString().default('temp'); // temp or const
+    params.convert('expires').optional().toInt().default(86400000); // 24 hours in ms
+
+    params.validate()
+      .then(() => {
+        if (transformedParams.type === 'const') {
+          return models.ShortLink.find({
+            where: { url: transformedParams.url },
+            include: [ { all: true } ]
+          })
+        }
+        else {
+          return null;
+        }
+      })
+      .then((dbShortLink) => {
+        if (dbShortLink) {
+          return dbShortLink.guid;
+        }
+        let hash = generator.generate({
+          length: 7,
+          numbers: true,
+          symbols: false,
+          uppercase: true,
+          excludeSimilarCharacters: false,
+        });
+        hash.toUpperCase();
+        if (transformedParams.type === 'const') {
+          return models.ShortLink.create({
+            guid: hash,
+            url: transformedParams.url,
+            access_count: 0
+          })
+          .then((dbShortLink) => {
+            return dbShortLink.guid;
+          })
+        }
+        else {
+          return redis.getAsync(hash)
+            .then((url) => {
+              if (!url) {
+                redis.set(hash, transformedParams.url, 'PX', transformedParams.expires);
+                return hash;
+              }
+              throw new Error('Error while getting the short link.');
+            })
+        }
+      })
+      .then(function(hash) {
+        let url = `${process.env.REST_PROTOCOL}://${process.env.REST_HOST_SHORT}${(transformedParams.type !== 'const')?
+         '/s' : ''}/${hash}`;
+        res.status(200).send(url);
+      })
+      .catch(ValidationError, e => httpError(req, res, 400, null, e.message))
+      .catch(e => { httpError(req, res, 500, e, 'Error while getting the short link.'); console.log(e) });
+  });
 
 module.exports = router;
 
