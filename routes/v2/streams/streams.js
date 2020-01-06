e394497e0d72df4302da4d16ff31e04351d5a4ea
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2020-01-06 10:09:38 +0300

    move master-segment endpoint lower

--- routes/v2/streams/streams.js
+++ routes/v2/streams/streams.js
@@ -143,6 +143,88 @@ router.route("/")
       .catch(e => { httpError(req, res, 500, e, 'Error while creating a stream.'); console.log(e) });
   });
 
+// Stream update
+
+router.route("/:guid")
+  .post(passport.authenticate(["token", 'jwt', 'jwt-custom'], { session:false }), hasRole(['rfcxUser']), function(req,res) {
+
+    let transformedParams = {};
+    let params = new Converter(req.body, transformedParams);
+
+    params.convert('name').optional().toString();
+    params.convert('description').optional().toString();
+    params.convert('starts').optional().toInt().minimum(0).maximum(32503669200000);
+    params.convert('ends').optional().toInt().minimum(0).maximum(32503669200000);
+    params.convert('location').optional().toString();
+    params.convert('site').optional().toString();
+    params.convert('visibility').optional().toString();
+
+    let stream;
+    let user;
+
+    params.validate()
+      .then(() => {
+        return streamsService.getStreamByGuid(req.params.guid)
+      })
+      .then((dbStream) => {
+        stream = dbStream;
+        return usersService.getUserByGuid(req.rfcx.auth_token_info.guid);
+      })
+      .then((dbUser) => {
+        user = usersService.formatUser(dbUser);
+        if (transformedParams.visibility) {
+          if (!allowedVisibilities.includes(transformedParams.visibility)) {
+            throw new ValidationError(`Invalid visibility. Possible values are: "private", "public", "site".`);
+          }
+          if (transformedParams.visibility !== 'private' && !transformedParams.site && !stream.Site) {
+            throw new ForbiddenError(`You are not allowed to make stream without adding a site.`);
+          }
+          return models.StreamVisibility.findOne({ where: { value: transformedParams.visibility } })
+            .then((dbStreamVisibility) => {
+              transformedParams.visibility = dbStreamVisibility.id;
+            });
+        }
+        return Promise.resolve();
+      })
+      .then(() => {
+        if (transformedParams.site) {
+          return sitesService.getSiteByGuid(transformedParams.site)
+            .then((dbSite) => {
+              if (!user.accessibleSites || !user.accessibleSites.length ||
+                  !user.accessibleSites.includes(dbSite.guid)) {
+                throw new ForbiddenError(`You are not allowed to add a stream with the site ${dbSite.guid}`);
+              }
+              transformedParams.site = dbSite.id;
+            })
+        }
+        return Promise.resolve();
+      })
+      .then(() => {
+        if (transformedParams.location) {
+          return models.Location.findOne({ where: { name: transformedParams.location } })
+            .then((dbLocation) => {
+              transformedParams.location = dbLocation.id;
+            });
+        }
+        return Promise.resolve();
+      })
+      .then(() => {
+        return streamsService.updateStream(stream, transformedParams);
+      })
+      .then((dbStream) => {
+        return streamsService.formatStream(dbStream);
+      })
+      .then(function(json) {
+        res.status(200).send(json);
+      })
+      .catch(ValidationError, e => { httpError(req, res, 400, null, e.message) })
+      .catch(ForbiddenError, e => { httpError(req, res, 403, null, e.message) })
+      .catch(EmptyResultError, e => { httpError(req, res, 404, null, e.message) })
+      .catch(sequelize.EmptyResultError, e => { httpError(req, res, 404, null, e.message) })
+      .catch(e => { httpError(req, res, 500, e, 'Error while updating the stream.'); console.log(e) });
+
+  });
+
 router.route("/:guid/master-segments")
   .post(passport.authenticate(["token", 'jwt', 'jwt-custom'], { session:false }), hasRole(['rfcxUser', 'systemUser']), function(req,res) {
 
@@ -246,88 +328,6 @@ router.route("/:guid/master-segments")
       .catch(EmptyResultError, e => { httpError(req, res, 404, null, e.message) })
       .catch(sequelize.EmptyResultError, e => { httpError(req, res, 404, null, e.message) })
       .catch(e => { httpError(req, res, 500, e, 'Error while creating master stream.'); console.log(e) });
-
-  });
-
-// Stream update
-
-router.route("/:guid")
-  .post(passport.authenticate(["token", 'jwt', 'jwt-custom'], { session:false }), hasRole(['rfcxUser']), function(req,res) {
-
-    let transformedParams = {};
-    let params = new Converter(req.body, transformedParams);
-
-    params.convert('name').optional().toString();
-    params.convert('description').optional().toString();
-    params.convert('starts').optional().toInt().minimum(0).maximum(32503669200000);
-    params.convert('ends').optional().toInt().minimum(0).maximum(32503669200000);
-    params.convert('location').optional().toString();
-    params.convert('site').optional().toString();
-    params.convert('visibility').optional().toString();
-
-    let stream;
-    let user;
-
-    params.validate()
-      .then(() => {
-        return streamsService.getStreamByGuid(req.params.guid)
-      })
-      .then((dbStream) => {
-        stream = dbStream;
-        return usersService.getUserByGuid(req.rfcx.auth_token_info.guid);
-      })
-      .then((dbUser) => {
-        user = usersService.formatUser(dbUser);
-        if (transformedParams.visibility) {
-          if (!allowedVisibilities.includes(transformedParams.visibility)) {
-            throw new ValidationError(`Invalid visibility. Possible values are: "private", "public", "site".`);
-          }
-          if (transformedParams.visibility !== 'private' && !transformedParams.site && !stream.Site) {
-            throw new ForbiddenError(`You are not allowed to make stream without adding a site.`);
-          }
-          return models.StreamVisibility.findOne({ where: { value: transformedParams.visibility } })
-            .then((dbStreamVisibility) => {
-              transformedParams.visibility = dbStreamVisibility.id;
-            });
-        }
-        return Promise.resolve();
-      })
-      .then(() => {
-        if (transformedParams.site) {
-          return sitesService.getSiteByGuid(transformedParams.site)
-            .then((dbSite) => {
-              if (!user.accessibleSites || !user.accessibleSites.length ||
-                  !user.accessibleSites.includes(dbSite.guid)) {
-                throw new ForbiddenError(`You are not allowed to add a stream with the site ${dbSite.guid}`);
-              }
-              transformedParams.site = dbSite.id;
-            })
-        }
-        return Promise.resolve();
-      })
-      .then(() => {
-        if (transformedParams.location) {
-          return models.Location.findOne({ where: { name: transformedParams.location } })
-            .then((dbLocation) => {
-              transformedParams.location = dbLocation.id;
-            });
-        }
-        return Promise.resolve();
-      })
-      .then(() => {
-        return streamsService.updateStream(stream, transformedParams);
-      })
-      .then((dbStream) => {
-        return streamsService.formatStream(dbStream);
-      })
-      .then(function(json) {
-        res.status(200).send(json);
-      })
-      .catch(ValidationError, e => { httpError(req, res, 400, null, e.message) })
-      .catch(ForbiddenError, e => { httpError(req, res, 403, null, e.message) })
-      .catch(EmptyResultError, e => { httpError(req, res, 404, null, e.message) })
-      .catch(sequelize.EmptyResultError, e => { httpError(req, res, 404, null, e.message) })
-      .catch(e => { httpError(req, res, 500, e, 'Error while updating the stream.'); console.log(e) });
 
   });
 
