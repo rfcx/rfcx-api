8c40744e669cb45325ace8c68f8a051872e14838
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2020-01-22 15:57:35 +0300

    add endpoint for search images and descriptions for guardian audio event value

--- routes/v1/events/events.js
+++ routes/v1/events/events.js
@@ -19,6 +19,7 @@ var sequelize = require("sequelize");
 var sqlUtils = require("../../../utils/misc/sql");
 var loggers = require('../../../utils/logger');
 var ValidationError = require("../../../utils/converter/validation-error");
+var EmptyResultError = require("../../../utils/converter/empty-result-error");
 var hasRole = require('../../../middleware/authorization/authorization').hasRole;
 var firebaseService = require('../../../services/firebase/firebase-service');
 var guardianGroupService = require('../../../services/guardians/guardian-group-service');
@@ -236,6 +237,18 @@ router.route("/values")
       })
       .then((data) => { res.status(200).json(data); })
       .catch(e => { httpError(req, res, 500, e, "Could not return Guardian Audio Event Values."); console.log(e)});
+
+  });
+
+router.route("/values/:value")
+  .get(passport.authenticate(['token', 'jwt', 'jwt-custom'], {session: false}), hasRole(['rfcxUser']), function (req, res) {
+
+    return eventValueService.searchForHighLevelKeysImageAndDescription(req.params.value)
+      .then((data) => { res.status(200).json(data); })
+      .catch(sequelize.EmptyResultError, e => httpError(req, res, 404, null, e.message))
+      .catch(EmptyResultError, e => httpError(req, res, 404, null, e.message))
+      .catch(ValidationError, e => httpError(req, res, 400, null, e.message))
+      .catch(e => { httpError(req, res, 500, e, "Could not return image and description for Guardian Audio Event Value."); console.log(e)});
 
   });
 
