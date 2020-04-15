04f8db5755c7c7121f6e48ef827e193396c8d42e
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2020-04-15 17:20:58 +0300

    add script to run mongo locally

---
+++ bin/mongo/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+source $(PWD)/bin/mongo/.env
+
+docker run --rm --name rfcx-api-mongo -p 27017:27017 -v $DB_FILES_PATH:/data \
+  -e MONGO_INITDB_ROOT_USERNAME=$MONGO_USERNAME \
+  -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_PASSWORD \
+  -d mongo:4.2.5
