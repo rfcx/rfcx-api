6a6196526e3ae70d5f8fc065c2cbbd69c421404b
Antony Harfield <antonyharfield@gmail.com>
2020-05-21 20:53:49 +0700

    WB-1354 Log when firebase certs are not defined (silently hangs otherwise)

--- utils/firebase/firebase.js
+++ utils/firebase/firebase.js
@@ -1,5 +1,12 @@
 const admin = require("firebase-admin");
 
+if (process.env.FIREBASE_CLIENT_EMAIL_RANGER_APP === '' || process.env.FIREBASE_CLIENT_EMAIL_RANGER_APP === undefined || process.env.FIREBASE_PRIVATE_KEY_RANGER_APP === '' || process.env.FIREBASE_PRIVATE_KEY_RANGER_APP === undefined) {
+  console.log('Missing FIREBASE_CLIENT_EMAIL_RANGER_APP/FIREBASE_PRIVATE_KEY_RANGER_APP')
+}
+if (process.env.FIREBASE_CLIENT_EMAIL_PLAYER_APP === '' || process.env.FIREBASE_CLIENT_EMAIL_PLAYER_APP === undefined || process.env.FIREBASE_PRIVATE_KEY_PLAYER_APP === '' || process.env.FIREBASE_PRIVATE_KEY_PLAYER_APP === undefined) {
+  console.log('Missing FIREBASE_CLIENT_EMAIL_PLAYER_APP/FIREBASE_PRIVATE_KEY_PLAYER_APP')
+}
+
 const rangerAppCert = admin.credential.cert({
   projectId: 'rfcx-ranger',
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL_RANGER_APP,
