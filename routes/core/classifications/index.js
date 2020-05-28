55314647b567f0955f675e01219bf5e09f7c562a
Antony Harfield <antonyharfield@gmail.com>
2020-05-28 20:45:22 +0700

    Add documentation for current classification endpoints

--- routes/core/classifications/index.js
+++ routes/core/classifications/index.js
@@ -11,8 +11,49 @@ const classificationService = require('../../../services/classification/classifi
 const Promise = require("bluebird");
 const Converter = require("../../../utils/converter/converter");
 
+/**
+ * @swagger
+ *
+ * /classifications/search:
+ *   get:
+ *     summary: Get list of classifications matching search criteria
+ *     tags:
+ *       - classfications
+ *     parameters:
+ *       - name: q
+ *         description: Search query
+ *         in: query
+ *         type: string
+ *         example: gibbon
+ *       - name: levels
+ *         description: Limit the results to classifications from specific levels in the hierarchy (meaning, different classification types)
+ *         in: query
+ *         type: array
+ *         example: species
+ *       - name: limit
+ *         description: Maximum number of results to return
+ *         in: query
+ *         type: int
+ *         default: 100
+ *       - name: offset
+ *         description: Number of results to skip
+ *         in: query
+ *         type: int
+ *         default: 0
+ *     responses:
+ *       200:
+ *         description: List of classification objects
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Classification'
+ *       400:
+ *         description: Invalid query parameters
+ */
 router.route("/search")
-  .get(passport.authenticate(['jwt', 'jwt-custom'], {session: false}), hasRole(['rfcxUser']), function (req, res) {
+  .get(passport.authenticate(['jwt', 'jwt-custom'], { session: false }), hasRole(['rfcxUser']), function (req, res) {
 
     let transformedParams = {};
     let params = new Converter(req.query, transformedParams);
@@ -38,25 +79,49 @@ router.route("/search")
       .catch(ValidationError, e => httpError(req, res, 400, null, e.message))
       .catch(ForbiddenError, e => { httpError(req, res, 403, null, e.message) })
       .catch(e => { httpError(req, res, 500, e, "Error while searching for Classification."); console.log(e) });
-
   });
 
+/**
+ * @swagger
+ *
+ * /classifications/{value}/characteristics:
+ *   get:
+ *     summary: Get the characteristics belonging to a classification
+ *     tags:
+ *       - classfications
+ *     parameters:
+ *       - name: value
+ *         description: Classification identifier
+ *         in: path
+ *         type: string
+ *         example: attenuata
+ *     responses:
+ *       200:
+ *         description: List of classification objects
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Classification'
+ *       400:
+ *         description: Invalid query parameters
+ */
 router.route("/:value/characteristics")
-  .get(passport.authenticate(['jwt', 'jwt-custom'], {session: false}), hasRole(['rfcxUser']), function (req, res) {
+  .get(passport.authenticate(['jwt', 'jwt-custom'], { session: false }), hasRole(['rfcxUser']), function (req, res) {
 
     return classificationService.getCharacteristicsForClassification(req.params.value)
-    .then((dbClassifications) => {
-      return classificationService.formatClassifications(dbClassifications);
-    })
-    .then((data) => {
-      return res.status(200).json(data);
-    })
-    .catch(sequelize.EmptyResultError, e => httpError(req, res, 404, null, e.message))
-    .catch(EmptyResultError, e => httpError(req, res, 404, null, e.message))
-    .catch(ValidationError, e => httpError(req, res, 400, null, e.message))
-    .catch(ForbiddenError, e => { httpError(req, res, 403, null, e.message) })
-    .catch(e => { httpError(req, res, 500, e, "Error while searching for Characteristics."); console.log(e) });
-
+      .then((dbClassifications) => {
+        return classificationService.formatClassifications(dbClassifications);
+      })
+      .then((data) => {
+        return res.status(200).json(data);
+      })
+      .catch(sequelize.EmptyResultError, e => httpError(req, res, 404, null, e.message))
+      .catch(EmptyResultError, e => httpError(req, res, 404, null, e.message))
+      .catch(ValidationError, e => httpError(req, res, 400, null, e.message))
+      .catch(ForbiddenError, e => { httpError(req, res, 403, null, e.message) })
+      .catch(e => { httpError(req, res, 500, e, "Error while searching for Characteristics."); console.log(e) });
   });
 
 module.exports = router;
