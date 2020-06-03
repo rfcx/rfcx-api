5fbd888ba85b2e1a9371ec0dd803a64d5edf0cb9
Antony Harfield <antonyharfield@gmail.com>
2020-06-03 16:15:33 +0700

    Add FIREBASE_ENABLED env var (note: need to add to rfcx.sh before release)

--- utils/firebase/firebase.js
+++ utils/firebase/firebase.js
@@ -1,11 +1,9 @@
-const admin = require("firebase-admin");
+if (process.env.FIREBASE_ENABLED !== 'true') {
+  module.exports = {}
+  return
+}
 
-if (process.env.FIREBASE_CLIENT_EMAIL_RANGER_APP === '' || process.env.FIREBASE_CLIENT_EMAIL_RANGER_APP === undefined || process.env.FIREBASE_PRIVATE_KEY_RANGER_APP === '' || process.env.FIREBASE_PRIVATE_KEY_RANGER_APP === undefined) {
-  console.log('Missing FIREBASE_CLIENT_EMAIL_RANGER_APP/FIREBASE_PRIVATE_KEY_RANGER_APP')
-}
-if (process.env.FIREBASE_CLIENT_EMAIL_PLAYER_APP === '' || process.env.FIREBASE_CLIENT_EMAIL_PLAYER_APP === undefined || process.env.FIREBASE_PRIVATE_KEY_PLAYER_APP === '' || process.env.FIREBASE_PRIVATE_KEY_PLAYER_APP === undefined) {
-  console.log('Missing FIREBASE_CLIENT_EMAIL_PLAYER_APP/FIREBASE_PRIVATE_KEY_PLAYER_APP')
-}
+const admin = require("firebase-admin");
 
 const rangerAppCert = admin.credential.cert({
   projectId: 'rfcx-ranger',
