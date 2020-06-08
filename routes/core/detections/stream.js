d0731a8ebd299609efaa10d49beccf2ab6862702
Antony Harfield <antonyharfield@gmail.com>
2020-06-08 23:45:16 +0700

    WB-1423 Basic implementation supporting prediction service specific saving of multiple detections

--- routes/core/detections/stream.js
+++ routes/core/detections/stream.js
@@ -85,5 +85,81 @@ router.get("/:streamId/detections", authenticatedWithRoles('rfcxUser'), function
     .catch(httpErrorHandler(req, res, 'Failed getting detections'))
 })
 
+/**
+ * @swagger
+ *
+ * /streams/{id}/detections:
+ *   post:
+ *     summary: Create an annotation belonging to a stream
+ *     tags:
+ *       - detections
+ *     parameters:
+ *       - name: id
+ *         description: Stream identifier
+ *         in: path
+ *         required: true
+ *         type: string
+ *     requestBody:
+ *       description: A single Detection object or consequetive Detections for a specific classification and classifier
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             $ref: '#/components/requestBodies/Detection'
+ *         application/json:
+ *           schema:
+ *             oneOf:
+ *               - $ref: '#/components/requestBodies/Detection'
+ *               - $ref: '#/components/requestBodies/DetectionsShortForm'
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/DetectionLite'
+ *       400:
+ *         description: Invalid query parameters
+ *       404:
+ *         description: Stream not found
+ */
+router.post("/:streamId/detections", authenticatedWithRoles('rfcxUser'), function (req, res) {
+  const streamId = req.params.streamId
+  const convertedParams = {}
+  const params = new Converter(req.body, convertedParams)
+  params.convert('start').toMomentUtc()
+  params.convert('end').toMomentUtc()
+  params.convert('classification').toString()
+  params.convert('classifier').toInt()
+  params.convert('confidence').optional().toFloat()
+  params.convert('confidences').optional().toFloatArray()
+  params.convert('step').optional().toFloat()
+
+  return params.validate()
+    .then(() => checkAccess(streamId, req))
+    .then(() => classificationService.getId(convertedParams.classification))
+    .then(classificationId => {
+      let { start, end, classifier, confidence, confidences, step } = convertedParams
+
+      // Can specify either confidence (float) or confidences (array of floats)
+      if (confidence === undefined && confidences === undefined) {
+        throw new ValidationError('Either parameter "confidence" or "confidences" is required')
+      } else if (confidences !== undefined && step === undefined) {
+        throw new ValidationError('Parameter "step" is required with parameter "confidences"')
+      } else if (confidences === undefined) {
+        confidences = [confidence]
+      }
+
+      const detections = confidences.map((confidence, i) => {
+        // If there are mulitple confidences then they are spaced by "step" seconds
+        const offsetStart = start.clone().add(i * step, 's')
+        const offsetEnd = end.clone().add(i * step, 's')
+        return { streamId, classificationId, classifierId: classifier, start: offsetStart, end: offsetEnd, confidence }
+      })
+      return detectionsService.create(detections)
+    })
+    .then(detections => res.sendStatus(201))
+    .catch(httpErrorHandler(req, res, 'Failed creating annotation'))
+})
 
 module.exports = router
