76bbc9930ced0c65c885d7e96554d2fa96ac564d
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2020-06-24 14:09:08 +0300

    update seed file for classifiers

--- bin/timescaledb/seeds/40-classifiers.sql
+++ bin/timescaledb/seeds/40-classifiers.sql
@@ -1,1 +1,8 @@
-INSERT INTO public.classifiers (id, version) VALUES (1, 1);
+INSERT INTO public.classifiers(id, uuid, name, version, min_confidence, min_windows_count) VALUES (1, 'e890e406-4855-4401-9705-5e6158ba9040', 'everything', 1, 0.5, 1);
+INSERT INTO public.classifiers(id, uuid, name, version, min_confidence, min_windows_count) VALUES (2, 'a74715f1-73ba-4dc8-9a3f-1cfcd2846782', 'whale-orca', 1, 0.8, 1);
+INSERT INTO public.classifiers(id, uuid, name, version, min_confidence, min_windows_count) VALUES (3, 'cd9e47d0-ea47-ed6a-be06-3963ecea03bc', 'chainsaw-rfcx', 1, 0.99758, 4);
+INSERT INTO public.classifiers(id, uuid, name, version, min_confidence, min_windows_count) VALUES (4, 'fcb4473b-ca9f-3974-7e54-20bf3cdb84d3', 'spider-monkey', 3, 0.8, 2);
+INSERT INTO public.classifiers(id, uuid, name, version, min_confidence, min_windows_count) VALUES (5, '8a9710c2-1056-e169-df6e-68aba3889719', 'vehicle-rfcx', 1, 0.999251, 4);
+INSERT INTO public.classifiers(id, uuid, name, version, min_confidence, min_windows_count) VALUES (6, '549f141d-78de-875b-f83c-c7236b133600', 'disturbance-a', 1, 0.8, 1);
+INSERT INTO public.classifiers(id, uuid, name, version, min_confidence, min_windows_count) VALUES (7, '2b2c2f0f-eb34-bb69-027b-556ca8a42a63', 'kungfu-gunshot', 3, 0.8, 1);
+INSERT INTO public.classifiers(id, uuid, name, version, min_confidence, min_windows_count) VALUES (8, '2c72afc0-532e-f31e-50dd-d948054cd6ca', 'chainsaw', 1, 0.911, 4);
