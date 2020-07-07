11a36ff6cf653e3cddb9dc2561ac023b10c31d7a
Antony Harfield <antonyharfield@gmail.com>
2020-07-07 19:52:47 +0700

    WB-1548 Add neo4j feature toggle

--- utils/neo4j.js
+++ utils/neo4j.js
@@ -1,3 +1,13 @@
+// Returns a Neo4j driver when NEO4J_ENABLED is set, else returns a fake driver object
+if (process.env.NEO4J_ENABLED !== 'true') {
+  module.exports = {
+    session: () => {
+      throw { name: 'NotEnabledError', message : 'NEO4J_ENABLED environment variable is not set'}
+    } 
+  }
+  return
+}
+
 const neo4j = require('neo4j-driver').v1;
 const env   = process.env.NODE_ENV || 'development';
 
