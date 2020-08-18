d0998dbbd84f2d50c52df5d01ee5f2a9d31c5abb
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2020-08-18 18:20:28 +0300

    remove temp code

--- services/guardians/authentication.js
+++ services/guardians/authentication.js
@@ -2,15 +2,11 @@ const guardianService = require('./guardians-service')
 const hash = require("../../utils/misc/hash.js").hash;
 
 async function isTokenCorrect(guardianOrGuid, token) {
-  console.log('\n\nisTokenCorrect', guardianOrGuid, token, '\n\n');
   const guardian = await (typeof guardianOrGuid === 'string' ? guardianService.getGuardianByGuid(guardianOrGuid, true) : Promise.resolve(guardianOrGuid))
-  console.log('\n\nguardian', guardian, '\n\n')
   if (!guardian) {
     return false
   }
-  const hhh = hash.hashedCredentials(guardian.auth_token_salt, token)
-  console.log('\n\n hhh', hhh, '\n\n');
-  return guardian.auth_token_hash == hhh
+  return guardian.auth_token_hash == hash.hashedCredentials(guardian.auth_token_salt, token)
 }
 
 module.exports = {
