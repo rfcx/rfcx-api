461506748e4e1aec2d32eee997204bac314d5f4d
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2020-08-28 19:20:12 +0300

    update logging format for request

--- middleware/logging.js
+++ middleware/logging.js
@@ -7,21 +7,11 @@ module.exports = expressWinston.logger({
   expressFormat: true,
   level: 'info',
   requestWhitelist: ['guid', 'instance', 'url', 'headers', 'method', 'httpVersion', 'originalUrl', 'query', 'body', 'files'],
+  responseWhitelist: ['body'],
   format: winston.format.combine(
     winston.format.colorize(),
     winston.format.json()
   ),
-  requestFilter: function (req, propName) {
-    if (propName === 'headers') {
-      // remove user token from logging for security reasons
-      delete req.headers['x-auth-token']
-    }
-    if (propName === 'body') {
-      // delete password from login body
-      delete req.body.password
-    }
-    return req[propName]
-  },
   ignoreRoute: function (req) {
     if (req.url === '/health_check') return true
     return false
