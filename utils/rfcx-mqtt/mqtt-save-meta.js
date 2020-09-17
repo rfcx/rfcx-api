56a3ded3f53e3784c9b5d58e205563615869d711
topherwhite <t@pher.me>
2020-09-17 04:41:58 -0700

    some data checks on input state of charge

--- utils/rfcx-mqtt/mqtt-save-meta.js
+++ utils/rfcx-mqtt/mqtt-save-meta.js
@@ -275,6 +275,9 @@ exports.saveMeta = {
       sntnlPwrEntries[timeStamp].state_of_charge = null;
       if ((sysInpBatt === 'battery') && (metaSntnlPwr[duInd][4].length > 0)) {
         sntnlPwrEntries[timeStamp].state_of_charge = parseFloat(metaSntnlPwr[duInd][4]);
+        if ((sntnlPwrEntries[timeStamp].state_of_charge > 100) || (sntnlPwrEntries[timeStamp].state_of_charge < 0)) {
+          sntnlPwrEntries[timeStamp].state_of_charge = null;
+        }
       }
     }
 
