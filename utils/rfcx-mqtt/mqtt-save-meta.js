fd206168b0d60f229130082fc49d1eceffb8289f
topherwhite <t@pher.me>
2020-09-19 05:02:29 -0700

    revised meta filtering

--- utils/rfcx-mqtt/mqtt-save-meta.js
+++ utils/rfcx-mqtt/mqtt-save-meta.js
@@ -7,9 +7,9 @@ exports.saveMeta = {
 
     for (const cpuInd in metaCPU) {
 
-      // if (    (parseInt(metaCPU[cpuInd][1]) <= 100) && (parseInt(metaCPU[cpuInd][1]) >= 0) 
-      //     &&  (parseInt(metaCPU[cpuInd][2]) <= 5000) && (parseInt(metaCPU[cpuInd][2]) >= 0)
-      // ) {
+      if (    (parseInt(metaCPU[cpuInd][1]) <= 100) && (parseInt(metaCPU[cpuInd][1]) >= 0) 
+          &&  (parseInt(metaCPU[cpuInd][2]) <= 5000) && (parseInt(metaCPU[cpuInd][2]) >= 0)
+      ) {
         dbMetaCPU.push({
           guardian_id: guardianId,
           check_in_id: checkInId,
@@ -17,7 +17,7 @@ exports.saveMeta = {
           cpu_percent: parseInt(metaCPU[cpuInd][1]),
           cpu_clock: parseInt(metaCPU[cpuInd][2])
         })
-//      }
+     }
     }
 
     return models.GuardianMetaCPU.bulkCreate(dbMetaCPU)
@@ -279,7 +279,7 @@ exports.saveMeta = {
       
       if ((sysInpBatt === 'battery') && (metaSntnlPwr[duInd][4].length > 0)) {
         sntnlPwrEntries[timeStamp].state_of_charge = parseFloat(metaSntnlPwr[duInd][4]);
-        if ((sntnlPwrEntries[timeStamp].state_of_charge > 105) || (sntnlPwrEntries[timeStamp].state_of_charge < 0)) {
+        if ((sntnlPwrEntries[timeStamp].state_of_charge > 150) || (sntnlPwrEntries[timeStamp].state_of_charge < -50)) {
           sntnlPwrEntries[timeStamp].state_of_charge = null;
         }
       }
