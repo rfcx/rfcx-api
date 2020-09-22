61f2d831dabda937ad0a33517af67701ff611ce5
topherwhite <t@pher.me>
2020-09-22 06:01:52 -0700

    including broker hostname in registration

--- routes/v2/guardians/guardians.js
+++ routes/v2/guardians/guardians.js
@@ -106,6 +106,7 @@ router.route('/register')
         guid: dbGuardian.guid,
         stream: dbStream.guid,
         token: token,
+        broker_hostname: 'staging-api-mqtt.rfcx.org', // process.env.GUARDIAN_BROKER_HOSTNAME
         keystore_passphrase: process.env.GUARDIAN_KEYSTORE_PASSPHRASE
       })
     } catch (e) {
