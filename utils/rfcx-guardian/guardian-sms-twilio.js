ba6c07e7233a6e90432e0ca8cdaaca08b94dc2de
Antony Harfield <antonyharfield@gmail.com>
2021-01-04 22:18:13 +0700

    CS-297 Logging for twilio env vars

--- utils/rfcx-guardian/guardian-sms-twilio.js
+++ utils/rfcx-guardian/guardian-sms-twilio.js
@@ -1,44 +1,43 @@
-//var models = require('../../models')
-const twilioClient = require('twilio');
+const twilioClient = require('twilio')
+
+if (process.env.TWILIO_AUTH_TOKEN === undefined || process.env.TWILIO_ACCOUNT_SID === undefined) {
+  console.error('Unable to send SMS because Twilio environmental variables are not set')
+}
 
 exports.smsTwilio = {
 
-  validateIncomingMessage: function(req) {
-    
-    var isValid = false;
-
-    console.log( process.env.TWILIO_AUTH_TOKEN + " - " + req.headers["x-twilio-signature"] + " - " + req.headers["x-forwarded-proto"] + "://" + req.headers.host + req.originalUrl)
-
-    if ( (req.headers["x-twilio-signature"] != null) && (process.env.TWILIO_AUTH_TOKEN != null) ) {
-
-      isValid = twilioClient.validateRequest(
-                  process.env.TWILIO_AUTH_TOKEN, 
-                  req.headers["x-twilio-signature"], 
-                  req.headers["x-forwarded-proto"] + "://" + req.headers.host + req.originalUrl, 
-                  req.body
-                );
+  validateIncomingMessage: function (req) {
+    if (process.env.TWILIO_AUTH_TOKEN === undefined) {
+      console.error('Unable to validate SMS because Twilio environmental variables are not set')
+      return
     }
 
-    return isValid;
+    if (req.headers['x-twilio-signature'] === undefined) {
+      console.error('Unable to validate SMS because request is missing x-twilio-signature header')
+      return
+    }
+
+    return twilioClient.validateRequest(
+      process.env.TWILIO_AUTH_TOKEN,
+      req.headers['x-twilio-signature'],
+      req.headers['x-forwarded-proto'] + '://' + req.headers.host + req.originalUrl,
+      req.body
+    )
   },
 
   sendSms: function (msgBody, toAddress) {
-    
-    if ( (process.env.TWILIO_ACCOUNT_SID != null) && (process.env.TWILIO_AUTH_TOKEN != null) ) {
-
-      var smsOpts = {
-        body: msgBody,
-        from: process.env.GUARDIAN_API_SMS_ADDRESS,
-        to: toAddress
-      };
-
-      twilioClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
-        .messages.create(smsOpts).then(message => console.log("SMS sent to " + message.to + " via Twilio: " + message.body));
-      
-    } else {
-      console.error("Unable to send SMS because Twilio environmental variables are not set");
+    if (process.env.TWILIO_AUTH_TOKEN === undefined || process.env.TWILIO_ACCOUNT_SID === undefined) {
+      console.error('Unable to send SMS because Twilio environmental variables are not set')
+      return
     }
 
+    const smsOpts = {
+      body: msgBody,
+      from: process.env.GUARDIAN_API_SMS_ADDRESS,
+      to: toAddress
+    }
+    twilioClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+      .messages.create(smsOpts).then(message => console.log('SMS sent to ' + message.to + ' via Twilio: ' + message.body))
   }
 
 }
