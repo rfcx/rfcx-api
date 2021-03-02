e38b4372bea37b7f593a3919de979e9344ff8f80
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-03-02 13:10:44 +0700

    CE-161 Implement GET /detections

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 1.0.12 (2021-03-02)
+
+### Features
+* **core:** Implement GET /detections with required `start` and `end` query and optional `streams`, `classifications`, `min_confidence`, `limit`, and `offset` ([CE-161](https://jira.rfcx.org/browse/CE-161))
+
+
 ## 1.0.11 (2021-02-27)
 
 ### Features
--- routes/core/detections/index.js
+++ routes/core/detections/index.js
@@ -1,4 +1,10 @@
 const router = require('express').Router()
+const { httpErrorHandler } = require('../../../utils/http-error-handler')
+const detectionsService = require('../../../services/detections')
+const models = require('../../../modelsTimescale')
+const roleService = require('../../../services/roles')
+const Converter = require('../../../utils/converter/converter')
+const { ForbiddenError } = require('../../../utils/errors')
 
 /**
  * @swagger
@@ -9,6 +15,36 @@ const router = require('express').Router()
  *     description:
  *     tags:
  *       - detections
+ *     parameters:
+ *       - name: start
+ *         description: Limit to a start date on or after (iso8601 or epoch)
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: end
+ *         description: Limit to a start date before (iso8601 or epoch)
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: streams
+ *         description: List of stream ids to limit results
+ *         in: query
+ *         type: array|string
+ *       - name: classifications
+ *         description: List of clasification values
+ *         in: query
+ *         type: array|string
+ *       - name: limit
+ *         description: Maximum number of results to return
+ *         in: query
+ *         type: int
+ *         default: 100
+ *       - name: offset
+ *         description: Number of results to skip
+ *         in: query
+ *         type: int
+ *         default: 0
+ *
  *     responses:
  *       200:
  *         description: List of detection (lite) objects
@@ -20,7 +56,35 @@ const router = require('express').Router()
  *         description: Invalid query parameters
  */
 router.get('/', (req, res) => {
-  res.sendStatus(501)
+  const user = req.rfcx.auth_token_info
+  const convertedParams = {}
+  const params = new Converter(req.query, convertedParams)
+  params.convert('start').toMomentUtc()
+  params.convert('end').toMomentUtc()
+  params.convert('streams').optional().toArray()
+  params.convert('classifications').optional().toArray()
+  params.convert('min_confidence').optional().toFloat()
+  params.convert('limit').optional().toInt()
+  params.convert('offset').optional().toInt()
+
+  return params.validate()
+    .then(async () => {
+      const streamIds = convertedParams.streams
+      console.log('Stream ids', streamIds)
+      if (streamIds) {
+        for (const id of streamIds) {
+          const allowed = await roleService.hasPermission(roleService.READ, user, id, roleService.STREAM)
+          if (!allowed) {
+            throw new ForbiddenError(`You do not have permission to access this stream: ${id}`)
+          }
+        }
+      }
+      const minConfidence = convertedParams.min_confidence
+      const { start, end, classifications, limit, offset } = convertedParams
+      const result = await detectionsService.query(start, end, streamIds, classifications, minConfidence, limit, offset, user)
+      return res.json(result)
+    })
+    .catch(httpErrorHandler(req, res, 'Failed getting detections'))
 })
 
 module.exports = router
