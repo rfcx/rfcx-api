2f147cd62d7960b9c25c3439ca27653421777cb5
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-04-01 15:03:27 +0700

    CE-432 Add query parameter

--- routes/internal/annotations/index.js
+++ routes/internal/annotations/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { httpErrorHandler } = require('../../../utils/http-error-handler')
+const Converter = require('../../../utils/converter/converter')
 
 /**
  * @swagger
@@ -14,8 +15,23 @@ const { httpErrorHandler } = require('../../../utils/http-error-handler')
  *         description: Success
  */
 router.get('/', async (req, res) => {
+  const convertedParams = {}
+  const params = new Converter(req.query, convertedParams, true)
+  params.convert('start').toMomentUtc()
+  params.convert('end').toMomentUtc()
+  params.convert('streams').optional().toArray()
+  params.convert('projects').optional().toArray()
+  params.convert('classifications').optional().toArray()
+  params.convert('min_confidence').optional().toFloat()
+  params.convert('is_reviewed').optional().toBoolean()
+  params.convert('is_positive').optional().toBoolean()
+  params.convert('limit').optional().toInt().maximum(1000)
+  params.convert('offset').optional().toInt()
+
   try {
-    res.sendStatus(501)
+    await params.validate()
+    const { start, end, streams, projects, classifications, minConfidence, isReviewed, isPositive, limit, offset } = convertedParams
+    return res.send({ start, end, streams, projects, classifications, minConfidence, isReviewed, isPositive, limit, offset })
   } catch (e) {
     httpErrorHandler(req, res, 'Failed getting annotations')
   }
