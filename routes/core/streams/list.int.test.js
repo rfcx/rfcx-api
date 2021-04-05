f2fd2dc071174230dfb5be17bf1ce37bf961ba6d
Antony Harfield <antonyharfield@gmail.com>
2021-04-05 13:37:27 +0700

    CE-166 Add tests for public streams

--- routes/core/streams/list.int.test.js
+++ routes/core/streams/list.int.test.js
@@ -68,29 +68,62 @@ describe('GET /', () => {
     expect(response.body[1].id).toBe(stream1.id)
   })
 
-  test('readable by stream guest', async () => {
+  test('results do not include public streams from others', async () => {
+    const stream = { id: 'x456y', isPublic: true, createdById: seedValues.otherUserId, name: 'Jaguar Station' }
+    await models.Stream.create(stream)
+
+    const response = await request(app).get('/')
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body.length).toBe(0)
+  })
+
+  test('results include public streams from me', async () => {
+    const stream = { id: 'x456y', isPublic: true, createdById: seedValues.primaryUserId, name: 'Jaguar Station' }
+    await models.Stream.create(stream)
+
+    const response = await request(app).get('/')
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body.length).toBe(1)
+  })
+
+  test('results include only public streams using only_public', async () => {
+    await models.Stream.create({ id: 'public1', createdById: seedValues.otherUserId, isPublic: true, name: 'AB01', latitude: 10.1, longitude: 101.1 })
+    await models.Stream.create({ id: 'private2', createdById: seedValues.primaryUserId, name: 'AB02', latitude: 10.2, longitude: 101.2 })
+
+    const response = await request(app).get('/').query({ only_public: true })
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body.length).toBe(1)
+    expect(response.body[0].id).toBe('public1')
+  })
+
+  test('results include stream guest', async () => {
     const stream = { id: 'x456y', createdById: seedValues.otherUserId, name: 'Jaguar Station' }
     await models.Stream.create(stream)
     await models.UserStreamRole.create({ user_id: seedValues.primaryUserId, stream_id: stream.id, role_id: seedValues.roleGuest })
 
-    const response = await request(app).get(`/${stream.id}`)
+    const response = await request(app).get('/')
 
     expect(response.statusCode).toBe(200)
+    expect(response.body.length).toBe(1)
   })
 
-  test('readable by project guest', async () => {
+  test('results include project guest', async () => {
     const project = { id: 'p123p', createdById: seedValues.otherUserId, name: 'Other Project' }
     const stream = { id: 'x456y', createdById: seedValues.otherUserId, project_id: project.id, name: 'Jaguar Station' }
     await models.Project.create(project)
     await models.Stream.create(stream)
     await models.UserProjectRole.create({ user_id: seedValues.primaryUserId, project_id: project.id, role_id: seedValues.roleGuest })
 
-    const response = await request(app).get(`/${stream.id}`)
+    const response = await request(app).get('/')
 
     expect(response.statusCode).toBe(200)
+    expect(response.body.length).toBe(1)
   })
 
-  test('readable by organization guest', async () => {
+  test('results include organization guest', async () => {
     const organization = { id: 'o789o', createdById: seedValues.otherUserId, name: 'Other Org' }
     const project = { id: 'p123p', createdById: seedValues.otherUserId, organization_id: organization.id, name: 'Other Project' }
     const stream = { id: 'x456y', createdById: seedValues.otherUserId, project_id: project.id, name: 'Jaguar Station' }
@@ -99,8 +132,9 @@ describe('GET /', () => {
     await models.Stream.create(stream)
     await models.UserOrganizationRole.create({ user_id: seedValues.primaryUserId, organization_id: organization.id, role_id: seedValues.roleGuest })
 
-    const response = await request(app).get(`/${stream.id}`)
+    const response = await request(app).get('/')
 
     expect(response.statusCode).toBe(200)
+    expect(response.body.length).toBe(1)
   })
 })
