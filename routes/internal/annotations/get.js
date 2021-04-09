cfbb102fc8e41789e4de2daf3ad5256b4431bb2c
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-04-10 00:11:27 +0700

    CE-432 Add internal annotation query

--- routes/internal/annotations/get.js
+++ routes/internal/annotations/get.js
@@ -6,35 +6,71 @@ const detectionsService = require('../../../services/detections')
  * @swagger
  * /internal/annotations
  *   get:
- *     summary: Get list of detections integrate with annotation
+ *     summary: Get list of detections integrate with annotations
  *     description: -
  *     tags:
  *       - internal
+ *     parameters:
+ *       - name: start
+ *         description: Limit to a start date on or after (iso8601 or epoch)
+ *         in: query
+ *         required: true
+ *         type: string
+ *         example: 2020-01-01T00:00:00.000Z
+ *       - name: end
+ *         description: Limit to a start date before (iso8601 or epoch)
+ *         in: query
+ *         required: true
+ *         type: string
+ *         example: 2020-12-31T00:00:00.000Z
+ *       - name: streams
+ *         description: List of stream ids to limit results
+ *         in: query
+ *         type: array|string
+ *       - name: classifications
+ *         description: List of clasification values
+ *         in: query
+ *         type: array|string
+ *       - name: min_confidence
+ *         description: Return results above a minimum confidence
+ *         in: query
+ *         type: float
+ *         example: 0.95
+ *       - name: limit
+ *         description: Maximum number of results to return
+ *         in: query
+ *         type: int
+ *         default: 100
+ *       - name: offset
+ *         description: Number of results to skip
+ *         in: query
+ *         type: int
+ *         default: 0
  *     responses:
  *       200:
  *         description: Success
  */
 module.exports = (req, res) => {
+  const user = req.rfcx.auth_token_info
   const converter = new Converter(req.query, {}, true)
   converter.convert('start').toMomentUtc()
   converter.convert('end').toMomentUtc()
-  converter.convert('streams').optional().toArray()
-  converter.convert('projects').optional().toArray()
+  converter.convert('stream_ids').optional().toArray()
+  converter.convert('project_ids').optional().toArray()
+  converter.convert('classifier_ids').optional().toArray()
   converter.convert('classifications').optional().toArray()
   converter.convert('min_confidence').optional().toFloat()
   converter.convert('is_reviewed').optional().toBoolean()
   converter.convert('is_positive').optional().toBoolean()
-  converter.convert('limit').optional().toInt().maximum(1000)
-  converter.convert('offset').optional().toInt()
+  converter.convert('limit').default(100).toInt().maximum(1000)
+  converter.convert('offset').default(0).toInt()
 
   converter.validate()
     .then(async (params) => {
-      // const { start, end, streams, projects, classifications, minConfidence, isReviewed, isPositive, limit, offset } = params
-      const result = await detectionsService.reviewQuery(params)
-      return res.json(result)
+      const results = await detectionsService.reviewQuery({ ...params }, user)
+      return res.json(results)
     })
     .catch((e) => {
-      console.log(e)
       httpErrorHandler(req, res, 'Failed getting annotations')(e)
     })
 }
--- services/detections/index.js
+++ services/detections/index.js
@@ -115,25 +115,87 @@ function create (detections) {
  * Get a list of detections
  * @param {string} start
  * @param {string} end
- * @param {string | string[]} streamIdOrIds Stream id or list of stream ids
- * @param {string | string[]} projetcs Project id or list of project ids
+ * @param {string | string[]} streamIds Stream id or list of stream ids
+ * @param {string | string[]} projectIds Project id or list of project ids
+ * @param {string | string[]} classifierIds Classifier id or list of classifier ids
  * @param {string | string[]} classifications Classification or list of classifications
  * @param {number} minConfidence Minimum confidence to query detections
  * @param {boolean} isReviewed
  * @param {boolean} isPositive
  * @param {number} limit Maximum number to get detections
  * @param {number} offset Number of resuls to skip
+ * @param {object} user
  * @returns {Detection[]} Detections
  */
-async function reviewQuery (start, end, streamIdOrIds, projetcs, classifications, minConfidence, isReviewed, isPositive, limit, offset) {
+async function reviewQuery (opts, user) {
+  const { start, end, streamIds, projectIds, classifierIds, classifications, minConfidence, isReviewed, isPositive, limit, offset } = opts
+  let conditions = 'WHERE d.start > $start AND d.end < $end'
+  const userId = user.id
+
+  if (streamIds) {
+    conditions = conditions + ' AND d.stream_id = ANY($streamIds)'
+  }
+
+  if (projectIds) {
+    conditions = conditions + ' AND s.project_id = ANY($projectIds)'
+  }
+
+  if (classifierIds) {
+    conditions = conditions + ' AND s.project_id = ANY($classifierIds)'
+  }
+
+  if (classifications) {
+    conditions = conditions + '  AND c.value = ANY($classifications)'
+  }
+
+  if (minConfidence) {
+    conditions = conditions + ' AND d.confidence >= $minConfidence'
+  }
+
+  if (isPositive) {
+    conditions = conditions + ' AND a.is_positive'
+  }
+
+  if (isPositive === false) {
+    conditions = conditions + ' AND NOT a.is_positive'
+  }
+
+  // TODO: Implement unreviewed conditions
+
   const sql =
-    `SELECT * FROM detections
-      LEFT JOIN annotations ON detections.start = annotations.start
-      AND detections.end = annotations.end
-      AND detections.stream_id = annotations.stream_id
-      AND detections.classification_id = annotations.classification_id
     `
-  return models.sequelize.query(sql, { type: models.sequelize.QueryTypes.SELECT })
+      SELECT d.start,d.end, d.classification_id, d.classifier_id, d.stream_id, d.confidence,
+      SUM(CASE WHEN a.is_positive THEN 1 ELSE 0 END) number_of_positive,
+      SUM(CASE WHEN NOT a.is_positive THEN 1 ELSE 0 END) number_of_negative, count(1) total,
+      SUM(CASE WHEN a.created_by_id = $userId AND a.is_positive then 1 ELSE 0 END) me_positive,
+      SUM(CASE WHEN a.created_by_id = $userId AND NOT a.is_positive THEN 1 ELSE 0 END) me_negative
+      FROM detections d
+      JOIN streams s ON d.stream_id = s.id
+      JOIN classifications c ON d.classification_id = c.id
+      LEFT JOIN annotations a ON d.stream_id = a.stream_id AND d.classification_id = a.classification_id AND d.start >= a.start AND d.end <= a.end
+      ${conditions}
+      GROUP BY d.start, d.end, d.classification_id, d.classifier_id, d.stream_id, d.confidence
+      LIMIT $limit
+      OFFSET $offset
+    `
+  const results = await models.sequelize.query(sql, { bind: { start, end, streamIds, projectIds, classifierIds, classifications, minConfidence, limit, offset, userId }, type: models.sequelize.QueryTypes.SELECT })
+
+  return results.map(review => {
+    return {
+      start: review.start,
+      end: review.end,
+      stream_id: review.stream_id,
+      classifier_id: review.classifier_id,
+      classification_id: review.classification_id,
+      confidence: review.confidence,
+      number_of_reviewed: Number(review.total),
+      number_of_positive: Number(review.number_of_positive),
+      number_of_negative: Number(review.number_of_negative),
+      me_reviewed: review.me_positive === 0 || review.me_negative === 0,
+      me_positive: review.me_positive !== 0 && review.me_negative === 0,
+      me_negative: review.me_negative !== 0 && review.me_positive === 0
+    }
+  })
 }
 
 const DEFAULT_IGNORE_THRESHOLD = 0.5
