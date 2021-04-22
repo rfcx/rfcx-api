43edf2cf409152139ea9576b5cc7eadfaba47004
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-04-22 17:45:36 +0700

    CE-432 Move GET /internal/ai-hub/detections params hint to requestBodies.json

--- routes/internal/ai-hub/get.js
+++ routes/internal/ai-hub/get.js
@@ -10,42 +10,6 @@ const reviewsService = require('../../../services/detections/reviews')
  *     description: -
  *     tags:
  *       - internal
- *     parameters:
- *       - name: start
- *         description: Limit to a start date on or after (iso8601 or epoch)
- *         in: query
- *         required: true
- *         type: string
- *         example: 2020-01-01T00:00:00.000Z
- *       - name: end
- *         description: Limit to a start date before (iso8601 or epoch)
- *         in: query
- *         required: true
- *         type: string
- *         example: 2020-12-31T00:00:00.000Z
- *       - name: streams
- *         description: List of stream ids to limit results
- *         in: query
- *         type: array|string
- *       - name: classifications
- *         description: List of clasification values
- *         in: query
- *         type: array|string
- *       - name: min_confidence
- *         description: Return results above a minimum confidence
- *         in: query
- *         type: float
- *         example: 0.95
- *       - name: limit
- *         description: Maximum number of results to return
- *         in: query
- *         type: int
- *         default: 100
- *       - name: offset
- *         description: Number of results to skip
- *         in: query
- *         type: int
- *         default: 0
  *     responses:
  *       200:
  *         description: Success
