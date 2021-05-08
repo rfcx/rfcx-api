b698b58c0c6deaeeb356cd9ede1f787d14f864fb
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-05-08 19:51:55 +0700

    CE-597 Fix classifications query by value instead of id and update document

--- routes/internal/ai-hub/get.js
+++ routes/internal/ai-hub/get.js
@@ -5,10 +5,10 @@ const ForbiddenError = require('../../../utils/converter/forbidden-error')
 
 /**
  * @swagger
- * /internal/ai-hub/detections
+ *
+ * /internal/ai-hub/detections:
  *   get:
  *     summary: Get list of detections integrate with annotations
- *     description: -
  *     tags:
  *       - internal
  *     parameters:
@@ -35,7 +35,7 @@ const ForbiddenError = require('../../../utils/converter/forbidden-error')
  *       - name: classifiers
  *         description: List of classifiers ids
  *         in: query
- *         type: array|string
+ *         type: array|number
  *       - name: classifications
  *         description: List of clasification values
  *         in: query
@@ -57,6 +57,7 @@ const ForbiddenError = require('../../../utils/converter/forbidden-error')
  *         default: 0
  *     responses:
  *       200:
+ *         description: List of detections integrate with annotations object
  *         content:
  *           application/json:
  *             schema:
