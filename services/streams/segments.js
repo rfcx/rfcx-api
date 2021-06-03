b54333ae67531eea837e8a3b6bc8b6c3f959bcd7
Antony Harfield <antonyharfield@gmail.com>
2021-06-03 18:38:59 +0700

    CE-747 Modify segment created message payload to use camelCase

--- services/streams/segments.js
+++ services/streams/segments.js
@@ -89,7 +89,7 @@ function create (segment, options = {}) {
   return StreamSegment.create(segment, { transaction })
     .then(() => {
       if (MessageQueue.isEnabled()) {
-        const message = { id: segment.id, start: segment.start, stream_id: segment.stream_id }
+        const message = { id: segment.id, start: segment.start, streamId: segment.stream_id }
         return MessageQueue.default().enqueue(SEGMENT_CREATED, message).catch((e) => {
           console.error('Stream segment service -> create -> enqueue failed', e.message || e)
         })
