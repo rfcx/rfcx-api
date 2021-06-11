066fd068a8d057cc62934f0cd3c8dee464d093e2
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2021-06-11 16:46:12 +0300

    fix datetime util structure

--- utils/misc/datetime.js
+++ utils/misc/datetime.js
@@ -1,45 +1,47 @@
 const moment = require('moment')
 
-const datetime = {
-  /**
-   * We round the time so that all times only have 0 or 500 miliseconds - our quantum of time
-   * @returns {moment} quantified time
-   */
-  quantify: function (mom) {
-    const miliseconds = mom.milliseconds()
+/**
+ * We round the time so that all times only have 0 or 500 miliseconds - our quantum of time
+ * @returns {moment} quantified time
+ */
+function quantify (mom) {
+  const miliseconds = mom.milliseconds()
 
-    // first trident (like quadrant but for three segments? )
-    if (miliseconds >= 0 && miliseconds < 250) {
-      mom.milliseconds(0)
-    }
-
-    // second trident
-    if (miliseconds >= 250 && miliseconds < 750) {
-      mom.milliseconds(500)
-    }
-
-    // third trident
-    if (miliseconds >= 750) {
-      // setting miliseconds above 999 will add the appropriate secs to the date
-      mom.milliseconds(1000)
-      mom.milliseconds(0)
-    }
-
-    return mom
-  },
-
-  momentToMysqlString: function (mom) {
-    return mom.tz('UTC').format('YYYY-MM-DD HH:mm:ss.SSS')
-  },
-
-  gluedDateStrToMoment: function (dateStr) {
-    return moment(dateStr, 'YYYYMMDDTHHmmssSSSZ').utc()
-  },
-
-  gluedDateStrToISO: function (dateStr) {
-    return this.gluedDateStrToMoment(dateStr).toISOString()
+  // first trident (like quadrant but for three segments? )
+  if (miliseconds >= 0 && miliseconds < 250) {
+    mom.milliseconds(0)
   }
 
+  // second trident
+  if (miliseconds >= 250 && miliseconds < 750) {
+    mom.milliseconds(500)
+  }
+
+  // third trident
+  if (miliseconds >= 750) {
+    // setting miliseconds above 999 will add the appropriate secs to the date
+    mom.milliseconds(1000)
+    mom.milliseconds(0)
+  }
+
+  return mom
 }
 
-module.exports = datetime
+function momentToMysqlString (mom) {
+  return mom.tz('UTC').format('YYYY-MM-DD HH:mm:ss.SSS')
+}
+
+function gluedDateStrToMoment (dateStr) {
+  return moment(dateStr, 'YYYYMMDDTHHmmssSSSZ').utc()
+}
+
+function gluedDateStrToISO (dateStr) {
+  return gluedDateStrToMoment(dateStr).toISOString()
+}
+
+module.exports = {
+  quantify,
+  momentToMysqlString,
+  gluedDateStrToMoment,
+  gluedDateStrToISO
+}
