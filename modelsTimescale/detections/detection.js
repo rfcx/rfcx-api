e5595d5b92cc52b70700734e8a27360c8c4b9531
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2021-08-27 12:31:30 +0300

    CE-1138 update sequelize models according to new db stucture

--- modelsTimescale/detections/detection.js
+++ modelsTimescale/detections/detection.js
@@ -1,5 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const Detection = sequelize.define('Detection', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
     start: {
       // Hypertable key
       type: DataTypes.DATE(3)
@@ -18,19 +24,23 @@ module.exports = (sequelize, DataTypes) => {
     },
     confidence: {
       type: DataTypes.FLOAT
+    },
+    review_status: {
+      type: DataTypes.INTEGER,
+      default: 0
     }
   }, {
     timestamps: false
   })
-  Detection.removeAttribute('id') // https://github.com/sequelize/sequelize/issues/1026#issuecomment-54877327
   Detection.associate = function (models) {
     Detection.belongsTo(models.Stream, { as: 'stream', foreignKey: 'stream_id' })
     Detection.belongsTo(models.Classification, { as: 'classification', foreignKey: 'classification_id' })
     Detection.belongsTo(models.Classifier, { as: 'classifier', foreignKey: 'classifier_id' })
+    Detection.hasMany(models.DetectionReview, { as: 'reviews', foreignKey: 'detection_id' })
   }
   Detection.attributes = {
     lite: ['stream_id', 'start', 'end', 'confidence'],
-    full: ['stream_id', 'start', 'end', 'confidence']
+    full: ['id', 'stream_id', 'start', 'end', 'confidence', 'review_status']
   }
   return Detection
 }
