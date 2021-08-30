67182171dde0fd0e3bb51df4b8153e517922af64
wasuphon <wasuphonn59@email.nu.ac.th>
2021-08-30 22:02:34 +0700

    Backfill old guardian data

--- migrations/20210830000001-add-stream-id-to-guardian.js
+++ migrations/20210830000001-add-stream-id-to-guardian.js
@@ -2,14 +2,13 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.addColumn(
-      'Guardians',
-      'stream_id',
-      {
-        type: Sequelize.STRING(12),
-        allowNull: true
+    return queryInterface.sequelize.transaction(async t => {
+      await queryInterface.addColumn('Guardians', 'stream_id', { type: Sequelize.STRING(12), allowNull: true }, { transaction: t })
+      const guardians = await queryInterface.sequelize.query('SELECT guid FROM Guardians', { type: Sequelize.QueryTypes.SELECT, transaction: t })
+      for (const guardian of guardians) {
+        await queryInterface.sequelize.query(`UPDATE Guardians SET stream_id='${guardian.guid}' WHERE guid='${guardian.guid}';`, { transaction: t })
       }
-    )
+    })
   },
 
   down: function (queryInterface, Sequelize) {
