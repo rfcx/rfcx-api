3f27bd3b2e5696157fb0dcc546b8a7acc40e85da
wasuphon <wasuphonn59@email.nu.ac.th>
2021-09-13 17:57:16 +0700

    Refactor segment save db function

---
+++ utils/rfcx-guardian/guardian-swm.js
@@ -0,0 +1,15 @@
+
+exports.swarm = {
+  validateIncomingMessage: function (req) {
+    let isValid = false
+    if ((req.headers['x-forwarded-for'] != null) && ((req.headers['x-forwarded-for'] === '212.71.235.32') || (req.headers['x-forwarded-for'] === '109.74.196.135'))) {
+      isValid = true/* twilioClient.validateRequest(
+                  process.env.TWILIO_AUTH_TOKEN,
+                  req.headers['x-twilio-signature'],
+                  req.headers['x-forwarded-proto'] + '://' + req.headers.host + req.originalUrl,
+                  req.body
+                ); */
+    }
+    return isValid
+  }
+}
