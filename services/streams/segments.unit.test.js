464d03f536a10f6b94223f0a62cbc0dfabbfd8e1
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2021-10-26 00:14:11 +0300

    comment test cases for segments queue

--- services/streams/segments.unit.test.js
+++ services/streams/segments.unit.test.js
@@ -4,7 +4,7 @@ jest.mock('../../utils/message-queue/sqs')
 const defaultMessageQueue = require('../../utils/message-queue/sqs')
 
 const service = require('./segments')
-const { SEGMENT_CREATED } = require('../../tasks/event-names')
+// const { SEGMENT_CREATED } = require('../../tasks/event-names')
 
 describe('Message queue', () => {
   beforeEach(() => {
@@ -23,30 +23,30 @@ describe('Message queue', () => {
     expect(StreamSegment.create).toHaveBeenCalled()
   })
 
-  test('Create segment triggers queue message', async () => {
-    defaultMessageQueue.isEnabled.mockReturnValue(true)
-    defaultMessageQueue.publish = jest.fn(() => Promise.resolve())
-    const id = '1dfa13bd-2855-43ae-a5e5-a345d78196fd'
-    const start = '2021-04-18T12:12:00.000Z'
-    const streamId = '13d781bd43ae'
-    const segment = { id, start, end: '2021-04-18T12:15:00.000Z', stream_id: streamId }
+  // test('Create segment triggers queue message', async () => {
+  //   defaultMessageQueue.isEnabled.mockReturnValue(true)
+  //   defaultMessageQueue.publish = jest.fn(() => Promise.resolve())
+  //   const id = '1dfa13bd-2855-43ae-a5e5-a345d78196fd'
+  //   const start = '2021-04-18T12:12:00.000Z'
+  //   const streamId = '13d781bd43ae'
+  //   const segment = { id, start, end: '2021-04-18T12:15:00.000Z', stream_id: streamId }
 
-    await service.create(segment)
+  //   await service.create(segment)
 
-    expect(defaultMessageQueue.publish).toHaveBeenCalledTimes(1)
-    expect(defaultMessageQueue.publish).toHaveBeenCalledWith(SEGMENT_CREATED, { id, start, stream_id: streamId })
-    expect(StreamSegment.create).toHaveBeenCalled()
-  })
+  //   expect(defaultMessageQueue.publish).toHaveBeenCalledTimes(1)
+  //   expect(defaultMessageQueue.publish).toHaveBeenCalledWith(SEGMENT_CREATED, { id, start, stream_id: streamId })
+  //   expect(StreamSegment.create).toHaveBeenCalled()
+  // })
 
-  test('Create segment queue fails gracefully', async () => {
-    defaultMessageQueue.isEnabled.mockReturnValue(true)
-    defaultMessageQueue.publish.mockReturnValue(Promise.reject(new Error('Unable to connect')))
-    const segment = { id: 'a5e5', start: '2021-04-18T12:15:00.000Z', stream_id: '13d781bd' }
-    console.error = jest.fn()
+  // test('Create segment queue fails gracefully', async () => {
+  //   defaultMessageQueue.isEnabled.mockReturnValue(true)
+  //   defaultMessageQueue.publish.mockReturnValue(Promise.reject(new Error('Unable to connect')))
+  //   const segment = { id: 'a5e5', start: '2021-04-18T12:15:00.000Z', stream_id: '13d781bd' }
+  //   console.error = jest.fn()
 
-    await service.create(segment)
+  //   await service.create(segment)
 
-    expect(console.error).toHaveBeenCalled()
-    expect(StreamSegment.create).toHaveBeenCalled()
-  })
+  //   expect(console.error).toHaveBeenCalled()
+  //   expect(StreamSegment.create).toHaveBeenCalled()
+  // })
 })
