821d4e42622abf5d486409bec879a7fc71b1567b
Antony Harfield <antonyharfield@hotmail.com>
2021-12-20 15:55:11 +0700

    Make tests not depend on string parsing

--- utils/rfcx-mqtt/mqtt-save-meta-datatransfer.unit.test.js
+++ utils/rfcx-mqtt/mqtt-save-meta-datatransfer.unit.test.js
@@ -1,6 +1,5 @@
 const moment = require('moment')
 const { saveMeta: { DataTransfer } } = require('./mqtt-save-meta')
-const { checkInDatabase: { strArrToJSArr } } = require('./mqtt-database.js')
 const models = require('../../models')
 
 jest.mock('../../models', () => {
@@ -26,43 +25,53 @@ beforeEach(async () => {
 })
 
 test('normal data transfer', async () => {
-  const ogDataTransfer = '1639662436136*1639662614136*0*0*490602*9344564|1639662257911*1639662436136*0*0*490602*9344564|1639662079919*1639662257911*0*0*490602*9344564|1639661901896*1639662079919*0*0*490602*9344564|1639661723676*1639661901896*0*0*490602*9344564|1639661545671*1639661723676*0*0*490602*9344564|1639661367657*1639661545671*0*0*490602*9344564|1639661189492*1639661367657*0*0*490602*9344564'
+  const payloadAsArray = [
+    ['1639662436136', '1639662614136', '0', '0', '490602', '9344564'],
+    ['1639662257911', '1639662436136', '0', '0', '490602', '9344564'],
+    ['1639662079919', '1639662257911', '0', '0', '490602', '9344564'],
+    ['1639661901896', '1639662079919', '0', '0', '490602', '9344564'],
+    ['1639661723676', '1639661901896', '0', '0', '490602', '9344564'],
+    ['1639661545671', '1639661723676', '0', '0', '490602', '9344564'],
+    ['1639661367657', '1639661545671', '0', '0', '490602', '9344564'],
+    ['1639661189492', '1639661367657', '0', '0', '490602', '9344564']]
 
-  const jsArr = strArrToJSArr(ogDataTransfer, '|', '*')
-  await DataTransfer(jsArr, 'xyz', '123')
+  await DataTransfer(payloadAsArray, 'xyz', '123')
 
   const results = await models.GuardianMetaDataTransfer.findAll()
   expect(results.length).toBe(8)
 
-  let index = 0
-  jsArr.forEach(it => {
+  payloadAsArray.forEach((it, index) => {
     expect(results[index].started_at).toEqual(moment(parseInt(it[0])).toDate())
     expect(results[index].ended_at).toEqual(moment(parseInt(it[1])).toDate())
     expect(results[index].mobile_bytes_received).toBe(parseInt(it[2]))
     expect(results[index].mobile_bytes_sent).toBe(parseInt(it[3]))
     expect(results[index].mobile_total_bytes_received).toBe(parseInt(it[4]))
     expect(results[index].mobile_total_bytes_sent).toBe(parseInt(it[5]))
-    index++
   })
 })
 
 test('shorten data transfer', async () => {
-  const ogDataTransfer = '1639662436136*1639662614136*0*0*490602*9344564|1639662257911*1639662436136*0*0*490602*9344564|1639662079919*1639662257911*0*0*490602*9344564|1639661901896*1639662079919*0*0*490602*9344564|1639661723676*1639661901896*0*0*490602*9344564|1639661545671*1639661723676*0*0*490602*9344564|1639661367657*1639661545671*0*0*490602*9344564|1639661189492*1639661367657*0*0*490602*9344564'
+  const payloadAsArray = [
+    ['1639662436136', '1639662614136', '0', '0', '490602', '9344564'],
+    ['1639662257911', '1639662436136', '0', '0', '490602', '9344564'],
+    ['1639662079919', '1639662257911', '0', '0', '490602', '9344564'],
+    ['1639661901896', '1639662079919', '0', '0', '490602', '9344564'],
+    ['1639661723676', '1639661901896', '0', '0', '490602', '9344564'],
+    ['1639661545671', '1639661723676', '0', '0', '490602', '9344564'],
+    ['1639661367657', '1639661545671', '0', '0', '490602', '9344564'],
+    ['1639661189492', '1639661367657', '0', '0', '490602', '9344564']]
 
-  const jsArr = strArrToJSArr(ogDataTransfer, '|', '*')
-  await DataTransfer(jsArr, 'xyz', '123')
+  await DataTransfer(payloadAsArray, 'xyz', '123')
 
   const results = await models.GuardianMetaDataTransfer.findAll()
   expect(results.length).toBe(8)
 
-  let index = 0
-  jsArr.forEach(it => {
+  payloadAsArray.forEach((it, index) => {
     expect(results[index].started_at).toEqual(moment(parseInt(it[0])).toDate())
     expect(results[index].ended_at).toEqual(moment(parseInt(it[1])).toDate())
     expect(results[index].mobile_bytes_received).toBe(parseInt(it[2]))
     expect(results[index].mobile_bytes_sent).toBe(parseInt(it[3]))
     expect(results[index].mobile_total_bytes_received).toBe(parseInt(it[4]))
     expect(results[index].mobile_total_bytes_sent).toBe(parseInt(it[5]))
-    index++
   })
 })
