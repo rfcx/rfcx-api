b8cea6e9aca23fd7de2223185297ca2977527c7e
wasuphon <wasuphonn59@email.nu.ac.th>
2021-12-20 16:13:33 +0700

    Add CPU meta unit test

---
+++ utils/rfcx-mqtt/mqtt-save-meta-cpu.unit.test.js
@@ -0,0 +1,61 @@
+const moment = require('moment')
+const { saveMeta: { CPU } } = require('./mqtt-save-meta')
+const models = require('../../models')
+
+jest.mock('../../models', () => {
+  const cpu = []
+  return {
+    GuardianMetaCPU: {
+      findAll: () => Promise.resolve(cpu),
+      bulkCreate: (objects) => {
+        cpu.push(...objects)
+        return Promise.resolve()
+      }
+    },
+    reset: () => {
+      while (cpu.length) {
+        cpu.pop()
+      }
+    }
+  }
+})
+
+beforeEach(async () => {
+  await models.reset()
+})
+
+test('normal cpu', async () => {
+  const payloadAsArray = [
+    ['1639990677555', '24', '424', '200'],
+    ['1639990558292', '24', '354', '200'],
+    ['1639990618092', '18', '251', '200']]
+
+  await CPU(payloadAsArray, 'xyz', '123')
+
+  const results = await models.GuardianMetaCPU.findAll()
+  expect(results.length).toBe(3)
+
+  payloadAsArray.forEach((it, index) => {
+    expect(results[index].measured_at).toEqual(moment(parseInt(it[0])).toDate())
+    expect(results[index].cpu_percent).toBe(parseInt(it[1]))
+    expect(results[index].cpu_clock).toBe(parseInt(it[2]))
+  })
+})
+
+test('shorten cpu', async () => {
+  const payloadAsArray = [
+    ['1639990677555', '24', '424', '200'],
+    ['1639990558292', '24', '354', '200'],
+    ['1639990618092', '18', '251', '200']]
+
+  await CPU(payloadAsArray, 'xyz', '123')
+
+  const results = await models.GuardianMetaCPU.findAll()
+  expect(results.length).toBe(3)
+
+  payloadAsArray.forEach((it, index) => {
+    expect(results[index].measured_at).toEqual(moment(parseInt(it[0])).toDate())
+    expect(results[index].cpu_percent).toBe(parseInt(it[1]))
+    expect(results[index].cpu_clock).toBe(parseInt(it[2]))
+  })
+})
