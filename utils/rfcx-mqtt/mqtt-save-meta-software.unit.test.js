1df909f1f1fa8aa7e7427a058b14c11e6d10e7a7
Antony Harfield <antonyharfield@hotmail.com>
2021-12-21 11:25:32 +0700

    Mock models manually for meta-software

--- utils/rfcx-mqtt/mqtt-save-meta-software.unit.test.js
+++ utils/rfcx-mqtt/mqtt-save-meta-software.unit.test.js
@@ -2,36 +2,56 @@ const { saveMeta: { SoftwareRoleVersion } } = require('./mqtt-save-meta')
 const models = require('../../models')
 
 jest.mock('../../models', () => {
+  const software = [
+    { id: 4, role: 'guardian' },
+    { id: 5, role: 'admin' },
+    { id: 6, role: 'classify' }
+  ]
   const softwareVersions = []
-  const saveMock = jest.fn()
-  const findOrCreateMock = jest.fn()
+  const metaSoftwareVersions = []
   return {
-    GuardianSoftwareVersion: {
-      findAll: () => Promise.resolve(softwareVersions),
-      findOrCreate: () => findOrCreateMock.mockImplementation(() => Promise.resolve([
-        {
-          save: () => saveMock
-        }
-      ]))
-    },
     GuardianSoftware: {
-      findOne: () => {
-        return Promise.resolve(softwareVersions)
+      findOne: (options) => {
+        return Promise.resolve(software.find(s => s.role === options.where.role))
+      }
+    },
+    GuardianSoftwareVersion: {
+      findAll: (options) => {
+        const filteredSoftwareVersions = options && options.where ? softwareVersions.filter(sv => sv.software_role_id === options.where.software_role_id && sv.version === options.where.version) : softwareVersions
+        return Promise.resolve(filteredSoftwareVersions)
+      },
+      findOrCreate: (options) => {
+        let softwareVersion = softwareVersions.find(sv => sv.software_role_id === options.where.software_role_id && sv.version === options.where.version)
+        let wasInserted = false
+        if (softwareVersion === undefined) {
+          softwareVersion = options.where
+          softwareVersions.push(softwareVersion)
+          wasInserted = true
+        }
+        return Promise.resolve([{ ...softwareVersion, save: () => Promise.resolve() }, wasInserted])
       }
     },
     GuardianMetaSoftwareVersion: {
-      findOrCreate: () => findOrCreateMock.mockImplementation(() => Promise.resolve([
-        {
-          save: () => saveMock
+      findAll: () => Promise.resolve(metaSoftwareVersions),
+      findOrCreate: (options) => {
+        let metaSoftwareVersion = metaSoftwareVersions.find(sv => sv.guardian_id === options.where.guardian_id && sv.software_id === options.where.software_id && sv.version_id === options.where.version_id)
+        let wasInserted = false
+        if (metaSoftwareVersion === undefined) {
+          metaSoftwareVersion = options.where
+          metaSoftwareVersions.push(metaSoftwareVersion)
+          wasInserted = true
         }
-      ]))
+        return Promise.resolve([{ ...metaSoftwareVersion, save: () => Promise.resolve() }, wasInserted])
+      }
     },
     reset: () => {
       while (softwareVersions.length) {
         softwareVersions.pop()
       }
-    },
-    save: saveMock
+      while (metaSoftwareVersions.length) {
+        metaSoftwareVersions.pop()
+      }
+    }
   }
 })
 
@@ -43,10 +63,12 @@ test('normal software', async () => {
   const payloadAsArray = [
     ['guardian', '0.8.9'],
     ['admin', '0.8.8'],
-    ['classify', '0.8.3'],
-    ['updater', '0.8.1']]
+    ['classify', '0.8.3']]
 
   await SoftwareRoleVersion(payloadAsArray, 'xyz', '123')
 
-  expect(models.save).toHaveBeenCalled()
+  const softwareVersions = await models.GuardianSoftwareVersion.findAll()
+  const metaSoftwareVersions = await models.GuardianMetaSoftwareVersion.findAll()
+  expect(softwareVersions.length).toBe(3)
+  expect(metaSoftwareVersions.length).toBe(0)
 })
