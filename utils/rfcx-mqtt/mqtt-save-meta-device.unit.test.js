3d88aff5c2e82f82a08d3a48f9a7fe475d26fb15
Antony Harfield <antonyharfield@hotmail.com>
2021-12-23 10:49:12 +0700

    Better mocking of devices and check device properties

--- utils/rfcx-mqtt/mqtt-save-meta-device.unit.test.js
+++ utils/rfcx-mqtt/mqtt-save-meta-device.unit.test.js
@@ -2,34 +2,24 @@ const { saveMeta: { Device } } = require('./mqtt-save-meta')
 const models = require('../../models')
 
 jest.mock('../../models', () => {
-  const devicePayload = {
-    android: {
-      product: 'Guardian',
-      brand: 'RFCx',
-      model: '3G_IOT',
-      build: '0.2.3',
-      android: '4.4.2',
-      manufacturer: 'OrangePi'
-    },
-    phone: {
-      sim: '8901260295780250462f',
-      number: '14156300572',
-      imei: '358877840010416',
-      imsi: '310260298025046'
-    }
-  }
-  const device = []
+  const devices = []
   return {
     GuardianMetaHardware: {
-      findAll: () => Promise.resolve(device),
-      findOrCreate: () => {
-        device.push(devicePayload)
-        return Promise.resolve([{ ...devicePayload, save: () => Promise.resolve() }, true])
+      findAll: () => Promise.resolve(devices),
+      findOrCreate: (options) => {
+        let device = devices.find(d => d.guardian_id === options.where.guardian_id)
+        const hasInserted = device === undefined
+        if (hasInserted) {
+          device = options.where
+          device.save = () => Promise.resolve()
+          devices.push(device)
+        }
+        return Promise.resolve([device, hasInserted])
       }
     },
     reset: () => {
-      while (device.length) {
-        device.pop()
+      while (devices.length) {
+        devices.pop()
       }
     }
   }
@@ -39,7 +29,7 @@ beforeEach(async () => {
   await models.reset()
 })
 
-test('normal device', async () => {
+test('new device meta is saved', async () => {
   const payload = {
     android: {
       product: 'Guardian',
@@ -50,6 +40,7 @@ test('normal device', async () => {
       manufacturer: 'OrangePi'
     },
     phone: {
+      carrier: 'AIS',
       sim: '8901260295780250462f',
       number: '14156300572',
       imei: '358877840010416',
@@ -61,9 +52,66 @@ test('normal device', async () => {
 
   const results = await models.GuardianMetaHardware.findAll()
   expect(results.length).toBe(1)
+  expect(results[0].manufacturer).toEqual(payload.android.manufacturer)
+  expect(results[0].product).toEqual(payload.android.product)
+  expect(results[0].brand).toEqual(payload.android.brand)
+  expect(results[0].model).toEqual(payload.android.model)
+  expect(results[0].android_build).toEqual(payload.android.build)
+  expect(results[0].android_version).toEqual(payload.android.android)
+  expect(results[0].phone_sim_carrier).toEqual(payload.phone.carrier)
+  expect(results[0].phone_sim_serial).toEqual(payload.phone.sim)
+  expect(results[0].phone_sim_number).toEqual(payload.phone.number)
+  expect(results[0].phone_imei).toEqual(payload.phone.imei)
+  expect(results[0].phone_imsi).toEqual(payload.phone.imsi)
 })
 
-test('shorten device', async () => {
+test('phone meta is updated', async () => {
+  const existingDevice = {
+    android: {
+      product: 'Guardian',
+      brand: 'RFCx',
+      model: '3G_IOT',
+      build: '0.2.3',
+      android: '4.4.2',
+      manufacturer: 'OrangePi'
+    },
+    phone: {
+      carrier: 'AIS',
+      sim: '8901260295780250462f',
+      number: '14156300572',
+      imei: '358877840010416',
+      imsi: '310260298025046'
+    }
+  }
+  await Device(existingDevice, 'xyz')
+  const payload = {
+    phone: {
+      carrier: 'True',
+      sim: '55555502957802555555',
+      number: '12345678901',
+      imei: '358999999999999',
+      imsi: '310888888888888'
+    }
+  }
+
+  await Device(payload, 'xyz')
+
+  const results = await models.GuardianMetaHardware.findAll()
+  expect(results.length).toBe(1)
+  expect(results[0].manufacturer).toEqual(existingDevice.android.manufacturer)
+  expect(results[0].product).toEqual(existingDevice.android.product)
+  expect(results[0].brand).toEqual(existingDevice.android.brand)
+  expect(results[0].model).toEqual(existingDevice.android.model)
+  expect(results[0].android_build).toEqual(existingDevice.android.build)
+  expect(results[0].android_version).toEqual(existingDevice.android.android)
+  expect(results[0].phone_sim_carrier).toEqual(payload.phone.carrier)
+  expect(results[0].phone_sim_serial).toEqual(payload.phone.sim)
+  expect(results[0].phone_sim_number).toEqual(payload.phone.number)
+  expect(results[0].phone_imei).toEqual(payload.phone.imei)
+  expect(results[0].phone_imsi).toEqual(payload.phone.imsi)
+})
+
+test('new device meta (compact format) is saved', async () => {
   const payload = {
     a: {
       p: 'Guardian',
@@ -110,6 +158,5 @@ test('shorten device invalid', async () => {
 
   const results = await models.GuardianMetaHardware.findAll()
 
-  console.log(results)
   expect(results.length).toBe(1)
 })
