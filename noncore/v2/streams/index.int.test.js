2d0b5bcceb6430721a1d60256e4d07248afb2424
Antony Harfield <antonyharfield@hotmail.com>
2022-02-18 19:09:58 +0700

    Tests for deployment date

--- noncore/v2/streams/index.int.test.js
+++ noncore/v2/streams/index.int.test.js
@@ -12,29 +12,29 @@ const streamIdCell = 'abc123'
 const streamIdSatellite = 'def456'
 const streamIdUnknown = 'ghi789'
 const data = {
-  [streamIdCell]: { guardianId: 23, prefValue: 'mqtt,rest' },
-  [streamIdSatellite]: { guardianId: 56, prefValue: 'sat' },
-  [streamIdUnknown]: { guardianId: 89 }
+  [streamIdCell]: { guardianId: 23, deployedAt: '2022-02-16T10:30:05.000Z', prefValue: 'mqtt,rest' },
+  [streamIdSatellite]: { guardianId: 56, deployedAt: '2022-02-17T11:45:35.000Z', prefValue: 'sat' },
+  [streamIdUnknown]: { guardianId: 89, deployedAt: null }
 }
 
 beforeEach(() => {
   model.Guardian.findOne = jest.fn().mockImplementation(({ where }) => {
     const item = data[where.stream_id]
     if (!item) { return Promise.reject(new EmptyResultError()) }
-    return Promise.resolve({ id: item.guardianId })
+    return Promise.resolve({ id: item.guardianId, deployed_at: item.deployedAt })
   })
   model.GuardianSoftwarePrefs.findOne = jest.fn().mockImplementation(({ where }) => {
     const item = Object.values(data).find(row => row.guardianId === where.guardian_id)
     if (!item || !item.prefValue || where.pref_key !== 'api_protocol_escalation_order') {
       return Promise.resolve(null)
     }
-    return { pref_key: '', pref_value: item.prefValue }
+    return { pref_key: 'api_protocol_escalation_order', pref_value: item.prefValue }
   })
 })
 
 test('guardian not found', async () => {
   muteConsole('warn')
-  const response = await request(app).get('/n0tF0unD')
+  const response = await request(app).get('/n0t4Gu4r6')
 
   expect(response.statusCode).toBe(404)
 })
@@ -58,4 +58,18 @@ test('unknown guardian type returned', async () => {
 
   expect(response.statusCode).toBe(200)
   expect(response.body.type).toBe('unknown')
+})
+
+test('date returned for deployed guardian', async () => {
+  const response = await request(app).get(`/${streamIdSatellite}`)
+
+  expect(response.statusCode).toBe(200)
+  expect(response.body.deployed_at).toEqual(data[streamIdSatellite].deployedAt)
+})
+
+test('not yet deployed guardian', async () => {
+  const response = await request(app).get(`/${streamIdUnknown}`)
+
+  expect(response.statusCode).toBe(200)
+  expect(response.body.deployed_at).toBeNull()
 })
