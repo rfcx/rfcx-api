a6e74596e9768f0ad528026f752ace87c5713a41
Antony Harfield <antonyharfield@hotmail.com>
2022-02-26 14:27:09 +0700

    Use core tasks for queuing prediction

--- noncore/_utils/rfcx-analysis/queue-for-prediction.js
+++ noncore/_utils/rfcx-analysis/queue-for-prediction.js
@@ -1,5 +1,4 @@
 const legacyQueueToNeo4jAndSqs = require('../rfcx-checkin/checkin-audio').audio.queueForTaggingByActiveV3Models
-const aws = require('../../_utils/external/aws').aws()
 
 function queueForPrediction (audioInfo, guardian) {
   console.info(`queueForPrediction: guardian ${guardian.guid} stream ${guardian.stream_id}`)
@@ -9,30 +8,6 @@ function queueForPrediction (audioInfo, guardian) {
   if (process.env.NEO4J_ENABLED === 'true') {
     return legacyQueueToNeo4jAndSqs(audioInfo, guardian)
   }
-  const streamId = guardian.stream_id
-  const timestamp = audioInfo.dbAudioObj.measured_at
-  return getClassifiers(streamId)
-    .then(classifiers => Promise.all(
-      classifiers.map(c => publish(c, streamId, timestamp))))
-}
-
-function getClassifiers (streamId) {
-  console.warn('NOT IMPLEMENTED: using chainsaw and vehicle only')
-  return Promise.resolve([ // TODO: should get list of active classifiers a stream
-    { platform: 'aws', name: 'chainsaw', version: 5 }
-  ])
-}
-
-function publish (classifier, streamId, timestamp) {
-  let topic = `classifier-${classifier.platform}-${classifier.name}-v${classifier.version}`
-  if (process.env.NODE_ENV !== 'production') {
-    topic += `-${process.env.NODE_ENV}`
-  }
-  const message = {
-    stream_id: streamId,
-    start: timestamp
-  }
-  return aws.publish(topic, message)
 }
 
 module.exports = queueForPrediction
