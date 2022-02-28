efbc8702ff9217ac81463706b27760ae8f475fe6
Antony Harfield <antonyharfield@hotmail.com>
2022-02-28 13:20:40 +0700

    Remove xml from swarm webhook

--- noncore/v2/guardians/guardians-segments.js
+++ noncore/v2/guardians/guardians-segments.js
@@ -71,59 +71,35 @@ router.route('/segments/sbd')
 
 router.route('/segments/swm')
   .post(passport.authenticate('token', { session: false }), function (req, res) {
-    console.info(`swarm segment: ${JSON.stringify(req.body)}`)
-
     const deviceId = req.body.deviceId
     const packetId = req.body.packetId
+
+    console.info(`swarm segment: ${deviceId} ${packetId} ${JSON.stringify(req.body)}`)
 
     // Swarm test message
     if (deviceId === 0 && packetId === 0) {
       console.info('swarm segment: test detected')
-      res.writeHead(200, { 'Content-Type': 'text/xml' }).end()
+      res.sendStatus(200)
       return
     }
 
     if (!swarmMsg.validateIncomingMessage(req)) {
-      console.error(`swarm segment: invalid message: ${deviceId} ${packetId}`)
+      console.error(`swarm segment: ${deviceId} ${packetId} invalid message: ${deviceId} ${packetId}`)
+      res.sendStatus(400)
+      return
     }
 
-    console.info(`swarm segment: validated ${deviceId} ${packetId}`)
+    console.info(`swarm segment: ${deviceId} ${packetId} validated`)
 
     const segObj = msgSegUtils.parseMsgSegment(Buffer.from(req.body.data, 'base64'), 'swm', deviceId)
     segmentUtils.saveSegmentToDb(segObj)
       .then(() => {
-        res.writeHead(200, { 'Content-Type': 'text/xml' }).end()
-      })
-      .catch(ValidationError, e => {
-        let message = 'Validation error'
-        try {
-          message = e.errors && e.errors.length ? e.errors.map((er) => er.message).join('; ') : e.message
-        } catch (_err) { }
-        console.error(`swarm segment: validation: ${message}`)
-        res.writeHead(400, { 'Content-Type': 'text/xml' }).send(`<ValidationError>${message}</ValidationError>`)
+        console.info(`swarm segment: ${deviceId} ${packetId} processed`)
+        res.sendStatus(200)
       })
       .catch(err => {
-        console.error(`swarm segment: ${JSON.stringify(err)}`)
-        res.writeHead(500, { 'Content-Type': 'text/xml' }).send(`<Error>${err.message}</Error>`)
-      })
-  })
-
-// For debugging purposes
-router.route('/segments/:groupId')
-  .get(passport.authenticate(['token', 'jwt', 'jwt-custom'], { session: false }), function (req, res) {
-    const groupId = req.params.groupId
-    segmentUtils.getSegmentsFromGroupId(groupId)
-      .then(segments => {
-        if (segments.length === 0) {
-          httpErrorResponse(req, res, 400, null, 'group id is not existing')
-        }
-        msgSegUtils.decodeSegmentsToJSON(segments)
-          .then(result => {
-            res.json(result)
-          })
-          .catch(err => {
-            res.status(500).json({ message: err.message, error: { status: 500 } })
-          })
+        console.error(`swarm segment: ${deviceId} ${packetId} failed ${JSON.stringify(err)}`)
+        res.sendStatus(200)
       })
   })
 
