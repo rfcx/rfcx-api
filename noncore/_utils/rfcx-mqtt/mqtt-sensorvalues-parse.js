097845318db5b366cfaad0cd6491d993ae593452
Antony Harfield <antonyharfield@hotmail.com>
2022-05-20 12:11:43 +0700

    #361 Sensor values pass parsing test cases

--- noncore/_utils/rfcx-mqtt/mqtt-sensorvalues-parse.js
+++ noncore/_utils/rfcx-mqtt/mqtt-sensorvalues-parse.js
@@ -2,13 +2,16 @@ const moment = require('moment')
 const { ValidationError } = require('../../../common/error-handling/errors')
 
 function parse (input) {
-  const parseRegex = /(?<sensorname>[a-zA-Z_]+)\*(?<timestamp>[0-9]+)\*(?<values>([0-9]*\.[0-9]+)?((,|n[0-9]+)([0-9]*\.[0-9]+)?)+)$/g
+  const parseRegex = /(?<sensor>[a-zA-Z_]+)\*(?<epoch>[0-9]+)\*(?<rawvalues>([0-9]*\.[0-9]+)(\*[0-9]*\.[0-9]+)+)$/g
   const result = parseRegex.exec(input)
   if (result === null) {
     throw new ValidationError('Format not recognized')
   }
 
-  return []
+  const { sensor, epoch, rawvalues } = result.groups
+  const timestamp = moment(parseInt(epoch))
+  const values = rawvalues.split('*').map(x => parseFloat(x))
+  return { sensor, timestamp, values }
 }
 
 module.exports = { parse }
