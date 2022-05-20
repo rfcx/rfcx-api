afb409998f71eea792ff73f583c3260a9bb32fea
Antony Harfield <antonyharfield@hotmail.com>
2022-05-20 11:55:29 +0700

    #361 Sensor values pass validation test cases

--- noncore/_utils/rfcx-mqtt/mqtt-sensorvalues-parse.js
+++ noncore/_utils/rfcx-mqtt/mqtt-sensorvalues-parse.js
@@ -2,6 +2,12 @@ const moment = require('moment')
 const { ValidationError } = require('../../../common/error-handling/errors')
 
 function parse (input) {
+  const parseRegex = /(?<sensorname>[a-zA-Z_]+)\*(?<timestamp>[0-9]+)\*(?<values>([0-9]*\.[0-9]+)?((,|n[0-9]+)([0-9]*\.[0-9]+)?)+)$/g
+  const result = parseRegex.exec(input)
+  if (result === null) {
+    throw new ValidationError('Format not recognized')
+  }
+
   return []
 }
 
