4a471abed947169482ea829ea207a26cceb54585
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2022-05-24 00:23:48 +0300

    restore v1/guardians endpoint for streaming app

--- noncore/v1/guardians/guardians.js
+++ noncore/v1/guardians/guardians.js
@@ -1,6 +1,59 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
+const hasRole = require('../../../common/middleware/authorization/authorization').hasRole
+const models = require('../../_models')
 
-router.route('/').get((req, res) => { res.status(200).json([]) })
+router.route('/')
+  .get(passport.authenticate(['token', 'jwt'], { session: false }), hasRole(['rfcxUser']), function (req, res) {
+    const userGuid = req.rfcx.auth_token_info.guid
+    if (userGuid !== '7f17c350-8ab7-b667-cfe2-9d69facda55b') { // return only for anonymous-assistant@rfcx.org which is used in Streaming app
+      res.json([])
+    } else {
+      models.GuardianSite.findAll({
+        attributes: ['id'],
+        include: [{
+          attributes: [],
+          model: models.User,
+          where: { guid: userGuid }
+        }]
+      }).then(async (sites) => {
+        const siteIds = sites.map(r => r.id)
+        let guardians = await models.Guardian.findAll({
+          attributes: ['guid', 'is_visible', 'last_check_in', 'shortname', 'latitude', 'longitude'],
+          include: [{
+            model: models.GuardianSite,
+            as: 'Site',
+            where: { id: siteIds },
+            attributes: ['guid', 'name']
+          }],
+          order: [['last_check_in', 'DESC']],
+          limit: req.query.limit ? parseInt(req.query.limit) : req.rfcx.limit,
+          offset: req.query.offset ? parseInt(req.query.offset) : req.rfcx.offset
+        })
+        guardians = guardians.map((g) => {
+          return {
+            guid: g.guid,
+            shortname: g.shortname,
+            is_visible: g.is_visible,
+            location: {
+              latitude: g.latitude,
+              longitude: g.longitude
+            },
+            checkins: {
+              guardian: {
+                last_checkin_at: g.last_check_in
+              }
+            },
+            site: g.Site
+          }
+        })
+        res.json(guardians)
+      }).catch(function (err) {
+        console.log(err)
+        if (err) { res.status(500).json({ msg: 'failed to return guardians' }) }
+      })
+    }
+  })
 
 module.exports = router
