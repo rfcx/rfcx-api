eef130be082f7c90e4abe50a0b5c05f5e4b72d07
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2022-06-10 23:20:51 +0300

    extend migration with last_audio_sync fill

--- noncore/_cli/migrations/20220610000001-add-battery-and-last-audio-to-guardians.js
+++ noncore/_cli/migrations/20220610000001-add-battery-and-last-audio-to-guardians.js
@@ -1,4 +1,5 @@
 'use strict'
+const moment = require('moment-timezone')
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
@@ -6,6 +7,13 @@ module.exports = {
       const lastAudioSyncExists = await queryInterface.sequelize.query('SELECT 1 FROM information_schema.COLUMNS WHERE table_name = \'Guardians\' AND column_name = \'last_audio_sync\' AND table_schema = \'rfcx_api\'', { plain: true, transaction })
       if (!lastAudioSyncExists) {
         await queryInterface.addColumn('Guardians', 'last_audio_sync', { type: Sequelize.DATE(3), allowNull: true, defaultValue: null }, { transaction })
+      }
+      const guardians = await queryInterface.sequelize.query('SELECT id FROM Guardians', { type: Sequelize.QueryTypes.SELECT, transaction })
+      for (const guardian of guardians) {
+        const lastAudio = (await queryInterface.sequelize.query(`SELECT measured_at FROM GuardianAudio WHERE guardian_id = ${guardian.id} ORDER BY measured_at DESC LIMIT 1`, { type: Sequelize.QueryTypes.SELECT, transaction }))[0]
+        if (lastAudio) {
+          await queryInterface.sequelize.query(`UPDATE Guardians SET last_audio_sync = '${moment.utc(lastAudio.measured_at).format('YYYY-MM-DD HH:mm:ss.SSS')}' WHERE id='${guardian.id}';`, { transaction })
+        }
       }
 
       const lastBatteryMainExists = await queryInterface.sequelize.query('SELECT 1 FROM information_schema.COLUMNS WHERE table_name = \'Guardians\' AND column_name = \'last_battery_main\' AND table_schema = \'rfcx_api\'', { plain: true, transaction })
