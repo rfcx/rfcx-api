3e8643f2ff0717e21ddc8fc0f06c8cf05bfaca44
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2022-06-15 14:15:54 +0300

    fix typo in migration

--- noncore/_cli/migrations/20220615000001-change-battery-columns-type.js
+++ noncore/_cli/migrations/20220615000001-change-battery-columns-type.js
@@ -4,14 +4,14 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.sequelize.transaction(async transaction => {
       await queryInterface.changeColumn('Guardians', 'last_battery_main', { type: Sequelize.FLOAT, allowNull: true, defaultValue: null }, { transaction })
-      await queryInterface.addColumn('Guardians', 'last_battery_internal', { type: Sequelize.FLOAT, allowNull: true, defaultValue: null }, { transaction })
+      await queryInterface.changeColumn('Guardians', 'last_battery_internal', { type: Sequelize.FLOAT, allowNull: true, defaultValue: null }, { transaction })
     })
   },
 
   down: function (queryInterface, Sequelize) {
     return queryInterface.sequelize.transaction(async transaction => {
       await queryInterface.changeColumn('Guardians', 'last_battery_main', { type: Sequelize.INTEGER, allowNull: true, defaultValue: null }, { transaction })
-      await queryInterface.addColumn('Guardians', 'last_battery_internal', { type: Sequelize.INTEGER, allowNull: true, defaultValue: null }, { transaction })
+      await queryInterface.changeColumn('Guardians', 'last_battery_internal', { type: Sequelize.INTEGER, allowNull: true, defaultValue: null }, { transaction })
     })
   }
 }
