8802c995784642cff3ffbff608c850539da03195
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-06-29 15:15:16 +0700

    - Add more test cases

--- core/classifiers/create.int.test.js
+++ core/classifiers/create.int.test.js
@@ -7,6 +7,13 @@ const app = expressApp({ has_system_role: true })
 
 app.use('/', routes)
 
+const CLASSIFICATION_1 = { id: 1, value: 'chainsaw', title: 'chainsaw', type_id: 1, source_id: null, parent_id: null, source_external_id: null, created_at: '2022-06-29 11:22:37.094935', update_at: '2022-06-29 11:22:37.094935' }
+const CLASSIFICATION_2 = { id: 2, value: 'vehicle', title: 'vehicle', type_id: 1, source_id: null, parent_id: null, source_external_id: null, created_at: '2022-06-29 11:22:37.094935', update_at: '2022-06-29 11:22:37.094935' }
+
+async function commonSetup () {
+  await models.Classification.bulkCreate([CLASSIFICATION_1, CLASSIFICATION_2])
+}
+
 beforeAll(async () => {
   await migrate(models.sequelize, models.Sequelize)
   await seed(models)
@@ -16,42 +23,110 @@ beforeEach(async () => {
   await commonSetup()
 })
 
-async function commonSetup () {
-  const CLASSIFICATION_1 = { id: 1, value: 'chainsaw', title: 'chainsaw', type_id: 1, source_id: null, parent_id: null, source_external_id: null, created_at: '2022-06-29 11:22:37.094935', update_at: '2022-06-29 11:22:37.094935' }
-  await models.Classification.bulkCreate([CLASSIFICATION_1])
-}
-
 describe('POST /classifiers/:id', () => {
-  test('normal user is forbidden', async () => {
-    const regularUserApp = expressApp({ is_super: false })
-    regularUserApp.use('/', routes)
-    console.warn = jest.fn()
-    const requestBody = {
-      name: 'chainsaw',
-      version: 1,
-      classification_values: 'chainsaw'
-    }
+  const superUserApp = expressApp({ is_super: true })
+  superUserApp.use('/', routes)
 
-    const response = await request(regularUserApp).post('/').send(requestBody)
+  const requestBody = {
+    name: 'chainsaw',
+    version: 1,
+    classification_values: 'chainsaw'
+  }
 
-    expect(response.statusCode).toBe(403)
-    expect(console.warn).toHaveBeenCalled()
+  describe('valid usage', () => {
+    test('normal user is forbidden', async () => {
+      const regularUserApp = expressApp({ is_super: false })
+      regularUserApp.use('/', routes)
+      console.warn = jest.fn()
+
+      const response = await request(regularUserApp).post('/').send(requestBody)
+
+      expect(response.statusCode).toBe(403)
+      expect(console.warn).toHaveBeenCalled()
+    })
+    test('super user can create classifier', async () => {
+      // Act
+      const response = await request(superUserApp).post('/').send(requestBody)
+      const classifiers = await models.Classifier.findAll()
+      const classifierOutputs = await models.ClassifierOutput.findAll()
+
+      // Assert
+      expect(response.statusCode).toBe(201)
+      expect(response.headers.location).toContain('/1')
+      expect(classifiers.length).toBe(1)
+      expect(classifiers[0].name).toBe(requestBody.name)
+      expect(classifiers[0].version).toBe(requestBody.version)
+      expect(classifierOutputs.length).toBe(1)
+      expect(classifierOutputs[0].classifierId).toBe(classifiers[0].id)
+      expect(classifierOutputs[0].classificationId).toBe(CLASSIFICATION_1.id)
+      expect(classifierOutputs[0].outputClassName).toBe(CLASSIFICATION_1.value)
+    })
+
+    test('map correct classification value', async () => {
+      // Arrange
+      const classifierLabel = 'chain'
+      const dbLabel = CLASSIFICATION_1.value
+      const requestBody = {
+        name: 'chainsaw',
+        version: 1,
+        classification_values: `${classifierLabel}:${dbLabel}`
+      }
+
+      // Act
+      const response = await request(superUserApp).post('/').send(requestBody)
+      const classifierOutputs = await models.ClassifierOutput.findAll()
+
+      // Assert
+      expect(response.statusCode).toBe(201)
+      expect(classifierOutputs.length).toBe(1)
+      expect(classifierOutputs[0].classificationId).toBe(CLASSIFICATION_1.id)
+      expect(classifierOutputs[0].outputClassName).toBe(classifierLabel)
+    })
+
+    test('multiple classification values', async () => {
+      // Arrange
+      const classifierLabel1 = 'chain'
+      const classifierLabel2 = 'veh'
+      const dbLabel1 = CLASSIFICATION_1.value
+      const dbLabel2 = CLASSIFICATION_2.value
+      const requestBody = {
+        name: 'chainsaw_and_vehicle',
+        version: 1,
+        classification_values: [`${classifierLabel1}:${dbLabel1}`, `${classifierLabel2}:${dbLabel2}`]
+      }
+
+      // Act
+      const response = await request(superUserApp).post('/').send(requestBody)
+      const classifierOutputs = await models.ClassifierOutput.findAll()
+
+      // Assert
+      expect(response.statusCode).toBe(201)
+      expect(classifierOutputs.length).toBe(2)
+      expect(classifierOutputs[0].classificationId).toBe(CLASSIFICATION_1.id)
+      expect(classifierOutputs[0].outputClassName).toBe(classifierLabel1)
+      expect(classifierOutputs[1].classificationId).toBe(CLASSIFICATION_2.id)
+      expect(classifierOutputs[1].outputClassName).toBe(classifierLabel2)
+    })
   })
-  test('super user can create classifier', async () => {
-    // Arrange
-    const superUserApp = expressApp({ is_super: true })
-    superUserApp.use('/', routes)
-    const requestBody = {
-      name: 'chainsaw',
-      version: 1,
-      classification_values: 'chainsaw'
-    }
 
-    // Act
-    const response = await request(superUserApp).post('/')
-      .send(requestBody)
+  describe('invalid usage', () => {
+    test('400 if not classification not exist in db', async () => {
+      // Arrange
+      const requestBody = {
+        name: 'abc',
+        version: 1,
+        classification_values: 'abc'
+      }
 
-    expect(response.statusCode).toBe(201)
-    expect(response.headers.location).toContain('/1')
+      // Act
+      const response = await request(superUserApp).post('/').send(requestBody)
+      const classifiers = await models.Classifier.findAll()
+      const classifierOutputs = await models.ClassifierOutput.findAll()
+
+      // Assert
+      expect(response.statusCode).toBe(400)
+      expect(classifiers.length).toBe(0)
+      expect(classifierOutputs.length).toBe(0)
+    })
   })
 })
