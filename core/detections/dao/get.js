2ab0ba551c19e8cda30d1b6f0e3ea5efd1918061
Stanislav Rassokhin <sr.rassokhin@gmail.com>
2023-03-23 00:20:36 +0400

    delete comments

--- core/detections/dao/get.js
+++ core/detections/dao/get.js
@@ -8,11 +8,6 @@ const moment = require('moment-timezone')
 async function get (filters, options = {}) {
   const attributes = options.fields && options.fields.length > 0 ? Detection.attributes.full.filter(a => options.fields.includes(a)) : Detection.attributes.full
   const include = options.fields && options.fields.length > 0 ? availableIncludes.filter(i => options.fields.includes(i.as)) : availableIncludes
-  // const fieldsIncludeStreamId = options.fields && options.fields.includes('stream_id')
-
-  // if (!fieldsIncludeStreamId) {
-  //   attributes.push('stream_id') // it is required for 'hasPermission' function call later
-  // }
 
   const detection = await Detection.findOne({
     where: {
@@ -27,13 +22,6 @@ async function get (filters, options = {}) {
     throw new EmptyResultError('Detection with given parameters not found')
   }
 
-  // if (options.readableBy && !(await hasPermission(READ, options.readableBy, detection.stream_id, STREAM))) {
-  //   throw new ForbiddenError()
-  // }
-
-  // if (!fieldsIncludeStreamId) {
-  //   delete detection.stream_id
-  // }
   return detection
 }
 
