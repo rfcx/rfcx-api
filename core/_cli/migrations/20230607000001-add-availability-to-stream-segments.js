6e7f00092ca3002e2bc09f4ae9c0d166599ab800
TooSeriuz <frongs@rfcx.org>
2023-06-07 19:00:09 +0700

    Change to ALTER TABLE instead of UPDATE

--- core/_cli/migrations/20230607000001-add-availability-to-stream-segments.js
+++ core/_cli/migrations/20230607000001-add-availability-to-stream-segments.js
@@ -2,16 +2,16 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async transaction => {
-      await queryInterface.addColumn('stream_segments', 'availability', { 
+      await queryInterface.addColumn('stream_segments', 'availability', {
         type: Sequelize.INTEGER,
         defaultValue: 0,
         allowNull: false,
         validate: {
-            min: 0,
-            max: 2
-        } },{ transaction })
-
-    await queryInterface.sequelize.query('UPDATE stream_segments SET availability = 1;', { transaction })
+          min: 0,
+          max: 2
+        }
+      }, { transaction })
+      await queryInterface.sequelize.query('ALTER TABLE stream_segments ALTER COLUMN availability SET DEFAULT 1', { transaction })
     })
   },
   down: async (queryInterface, Sequelize) => {
