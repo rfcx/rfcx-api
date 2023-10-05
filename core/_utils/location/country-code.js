253f749faac45ea48cefc766516ef345de31fc37
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-10-05 16:14:09 +0300

    Pass points array to the country_code lib

--- core/_utils/location/country-code.js
+++ core/_utils/location/country-code.js
@@ -3,7 +3,7 @@ const { ValidationError } = require('../../../common/error-handling/errors')
 
 function getCountryCodeByLatLng (latitude, longitude) {
   try {
-    return wc(latitude, longitude)
+    return wc([latitude, longitude])
   } catch (err) {
     throw new ValidationError(err.message)
   }
