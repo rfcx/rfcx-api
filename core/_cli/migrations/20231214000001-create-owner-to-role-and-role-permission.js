f681131756767c0c9c283e63941ef18a9745e536
TooSeriuz <frongs@rfcx.org>
2023-12-15 15:45:42 +0700

    Fix add wrong permission to each roles, Fix migration trying to create user_project_roles on the same values

--- core/_cli/migrations/20231214000001-create-owner-to-role-and-role-permission.js
+++ core/_cli/migrations/20231214000001-create-owner-to-role-and-role-permission.js
@@ -1,7 +1,7 @@
 'use strict'
 const roleIds = [1, 2, 3, 4]
 const roleList = ["Admin", "Member", "Guest", "Owner"]
-const permissionList = ["C", "R", "U", "D"]
+const permissionList = ["1,C", "1,R", "1,U", "1,D", "2,C", "2,R", "2,U", "3,R", "4,C", "4,R", "4,U", "4,D"]
 const insertedRoles = [] // [1, 2 ,3 ,4]
 const insertedRolePermissions = [] // [{ roleId:1, permission:"C"}, { roleId:1, permission:"R"}]
 module.exports = {
@@ -16,16 +16,16 @@ module.exports = {
                 }
             }
             // Find and Create role permissions (1, C), (1, R), (1, U), (1, D), (2, C), (2, R), (2, U), (3, R), (4, C), (4, R), (4, U), (4, D)
-            for (const id of insertedRoles) {
-                for (const permission of permissionList) {
-                    const [[permissionData]] = await queryInterface.sequelize.query(`select (role_id) from role_permissions where role_id = ${id} and permission = '${permission}'`, { transaction })
-                    if (!permissionData) {
-                        await queryInterface.sequelize.query(`insert into role_permissions (role_id, permission) values (${id}, '${permission}')`, { transaction })
-                        insertedRolePermissions.push({
-                            roleId: id,
-                            permission: permission
-                        })
-                    }
+            for (const permission of permissionList) {
+                const roleId = permission.split(',')[0]
+                const permissionValue = permission.split(',')[1]
+                const [[permissionData]] = await queryInterface.sequelize.query(`select (role_id) from role_permissions where role_id = ${roleId} and permission = '${permissionValue}'`, { transaction })
+                if (!permissionData) {
+                    await queryInterface.sequelize.query(`insert into role_permissions (role_id, permission) values (${roleId}, '${permissionValue}')`, { transaction })
+                    insertedRolePermissions.push({
+                        roleId: roleId,
+                        permission: permissionValue
+                    })
                 }
             }
         })
--- core/_cli/migrations/20231215000001-create-owner-in-user-project-roles.js
+++ core/_cli/migrations/20231215000001-create-owner-in-user-project-roles.js
@@ -5,11 +5,14 @@ module.exports = {
         return queryInterface.sequelize.transaction(async transaction => {
             const [projectsCreatedBy] = await queryInterface.sequelize.query(`select id, created_by_id from projects`, { transaction })
             for (const projectAndOwner of projectsCreatedBy) {
-                await queryInterface.sequelize.query(`insert into user_project_roles (user_id, project_id, role_id, created_at, updated_at) values (${projectAndOwner.created_by_id}, '${projectAndOwner.id}', 4, now(), now())`, { transaction })
-                insertedUserAndProject.push({
-                    userId: projectAndOwner.created_by_id,
-                    projectId: projectAndOwner.id
-                })
+                const [[userProjectRoleData]] = await queryInterface.sequelize.query(`select (role_id) from user_project_roles where user_id = ${projectAndOwner.created_by_id} and project_id = '${projectAndOwner.id}' and role_id = 4`, { transaction })
+                if (!userProjectRoleData) {
+                    await queryInterface.sequelize.query(`insert into user_project_roles (user_id, project_id, role_id, created_at, updated_at) values (${projectAndOwner.created_by_id}, '${projectAndOwner.id}', 4, now(), now())`, { transaction })
+                    insertedUserAndProject.push({
+                        userId: projectAndOwner.created_by_id,
+                        projectId: projectAndOwner.id
+                    })
+                }
             }
         })
     },
