67e23cbde506a1e03f85252f86f2bba0d3eee913
TooSeriuz <frongs@rfcx.org>
2024-02-28 18:41:45 +0700

    fix lint

--- core/_cli/migrations/20240228000001-latitude-longitude-0-to-null.js
+++ core/_cli/migrations/20240228000001-latitude-longitude-0-to-null.js
@@ -1,9 +1,9 @@
 'use strict'
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.query(`update streams set latitude = null, longitude = null where latitude = 0 and longitude = 0;`)
+    return queryInterface.sequelize.query('update streams set latitude = null, longitude = null where latitude = 0 and longitude = 0;')
   },
   down: (queryInterface) => {
-    return queryInterface.sequelize.query(`update streams set latitude = 0, longitude = 0 where latitude is null and longitude is null;`)
+    return queryInterface.sequelize.query('update streams set latitude = 0, longitude = 0 where latitude is null and longitude is null;')
   }
 }
