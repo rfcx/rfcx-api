778da7856106a2ba0f0290a6a13f355085060dc7
Bhattarapong Somewong <numbbutt34685@gmail.com>
2024-03-08 14:31:09 +0700

    #561 add jobs with various statuses

---
+++ core/_cli/seeds/72-classifier-jobs-streams.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.classifier_job_streams (classifier_job_id, stream_id) VALUES (226, 'MoLQA8aNulGb');
+INSERT INTO public.classifier_job_streams (classifier_job_id, stream_id) VALUES (227, 'MoLQA8aNulGb');
+INSERT INTO public.classifier_job_streams (classifier_job_id, stream_id) VALUES (228, 'MoLQA8aNulGb');
+INSERT INTO public.classifier_job_streams (classifier_job_id, stream_id) VALUES (229, 'MoLQA8aNulGb');
+INSERT INTO public.classifier_job_streams (classifier_job_id, stream_id) VALUES (230, 'MoLQA8aNulGb');
