cc22755bb7a0739105ec52df764e904cd2ee1206
Bhattarapong Somewong <numbbutt34685@gmail.com>
2024-03-08 17:36:16 +0700

    #561 add Tree inside the puerto rico seeds

--- core/_cli/seeds/07-user-organization-roles.sql
+++ core/_cli/seeds/07-user-organization-roles.sql
@@ -4,4 +4,5 @@ INSERT INTO public.user_organization_roles(user_id, organization_id, role_id, cr
 INSERT INTO public.user_organization_roles(user_id, organization_id, role_id, created_at, updated_at) VALUES (8, 'aaaaaaaaaaa2', 2, '2020-01-01T00:00:00.000Z', '2020-01-01T00:00:00.000Z');
 INSERT INTO public.user_organization_roles(user_id, organization_id, role_id, created_at, updated_at) VALUES (13, 'aaaaaaaaaaa4', 3, '2020-01-01T00:00:00.000Z', '2020-01-01T00:00:00.000Z');
 INSERT INTO public.user_organization_roles(user_id, organization_id, role_id, created_at, updated_at) VALUES (8, 'aaaaaaaaaaa4', 3, '2020-01-01T00:00:00.000Z', '2020-01-01T00:00:00.000Z');
-INSERT INTO public.user_organization_roles(user_id, organization_id, role_id, created_at, updated_at) VALUES (11412, 'zy5jbxx4cs9f', 1, '2020-01-01T00:00:00.000Z', '2020-01-01T00:00:00.000Z');
+INSERT INTO public.user_organization_roles(user_id, organization_id, role_id, created_at, updated_at) VALUES (11412, 'aaaaaaaaaaa6', 1, '2020-01-01T00:00:00.000Z', '2020-01-01T00:00:00.000Z');
+INSERT INTO public.user_organization_roles(user_id, organization_id, role_id, created_at, updated_at) VALUEs (6036, 'aaaaaaaaaaa6', 1, '2020-01-01T00:00:00.000Z', '2020-01-01T00:00:00.000Z');
--- core/_cli/seeds/08-user-project-roles.sql
+++ core/_cli/seeds/08-user-project-roles.sql
@@ -2,3 +2,4 @@ INSERT INTO public.user_project_roles(user_id, project_id, role_id, created_at, 
 INSERT INTO public.user_project_roles(user_id, project_id, role_id, created_at, updated_at) VALUES (176, 'bbbbbbbbbbb5', 3, '2020-01-01T00:00:00.000Z', '2020-01-01T00:00:00.000Z');
 INSERT INTO public.user_project_roles(user_id, project_id, role_id, created_at, updated_at) VALUES (176, 'bbbbbbbbbbb6', 3, '2020-01-01T00:00:00.000Z', '2020-01-01T00:00:00.000Z');
 INSERT INTO public.user_project_roles(user_id, project_id, role_id, created_at, updated_at) VALUES (11412, 'zy5jbxx4cs9f', 1, '2023-05-03 09:29:08.052 +0700', '2023-05-03 09:29:08.052 +0700');
+INSERT INTO public.user_project_roles (user_id, project_id, role_id, created_at, updated_at) VALUES (6036, 'zy5jbxx4cs9f', 1, '2023-05-03 09:29:08.052 +0700', '2023-05-03 09:29:08.052 +0700');
