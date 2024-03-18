87ad98aa8d859295308c6280c94c99493e7807e5
TooSeriuz <frongs@rfcx.org>
2024-03-18 17:39:50 +0700

    fix docs not show for `GET /classifier/:id/file`

--- core/classifiers/download.js
+++ core/classifiers/download.js
@@ -3,27 +3,29 @@ const { getSignedUrl } = require('./dao/download')
 const { get } = require('./dao')
 
 /**
- * /classifiers/{id}/file
+ * @swagger
+ * 
+ * /classifiers/{id}/file:
  *  get:
- *    summary: Downlaod a classifier file
+ *    summary: Download a classifier file
  *    tags:
- *      - classifiers
- *     parameters:
- *       - name: id
- *         description: Classifier identifier
- *         in: path
- *         type: string
- *         example: 1
- *     responses:
- *       200:
- *         description: A classifier file
- *         content:
- *           application/zip:
- *             schema:
- *               type: string
- *               format: binary
- *       404:
- *         description: Not found
+ *     - classifiers
+ *    parameters:
+ *     - name: id
+ *       description: Classifier identifier
+ *       in: path
+ *       type: string
+ *       example: 1
+ *    responses:
+ *      200:
+ *        description: A classifier file
+ *        content:
+ *          application/zip:
+ *            schema:
+ *              type: string
+ *              format: binary
+ *      404:
+ *        description: Not found
  */
 module.exports = (req, res) => {
   const user = req.rfcx.auth_token_info
