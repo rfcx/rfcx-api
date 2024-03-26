2e0511ca546dd2d1b6c813b542dfc13c0d3d89da
TooSeriuz <frongs@rfcx.org>
2024-03-26 20:03:06 +0700

    #577 Add more tests

--- core/internal/classifier-jobs/awaiting-cancellation.int.test.js
+++ core/internal/classifier-jobs/awaiting-cancellation.int.test.js
@@ -73,5 +73,35 @@ describe('GET /internal/classifier-jobs/awaiting-cancellation', () => {
     const response = await request(app).get('/awaiting-cancellation').query({ limit: '2' })
 
     expect(response.body).toHaveLength(2)
+    expect(response.headers['total-items']).toBe('3')
+  })
+
+  test('respects offset', async () => {
+    const { classifierId, projectId } = await commonSetup()
+    // Waiting cancel jobs (3)
+    await models.ClassifierJob.create({ status: AWAITING_CANCELLATION, classifierId, projectId, createdById: seedValues.otherUserId })
+    await models.ClassifierJob.create({ status: AWAITING_CANCELLATION, classifierId, projectId, createdById: seedValues.otherUserId })
+    await models.ClassifierJob.create({ status: AWAITING_CANCELLATION, classifierId, projectId, createdById: seedValues.otherUserId })
+
+    const response = await request(app).get('/awaiting-cancellation').query({ offset: '2' })
+
+    expect(response.body).toHaveLength(1)
+    expect(response.headers['total-items']).toBe('3')
+  })
+
+  test('respects sort', async () => {
+    const { classifierId, projectId } = await commonSetup()
+    // Waiting cancel jobs (3)
+    const job1 = await models.ClassifierJob.create({ status: AWAITING_CANCELLATION, classifierId, projectId, createdById: seedValues.otherUserId })
+    const job2 = await models.ClassifierJob.create({ status: AWAITING_CANCELLATION, classifierId, projectId, createdById: seedValues.otherUserId })
+    const job3 = await models.ClassifierJob.create({ status: AWAITING_CANCELLATION, classifierId, projectId, createdById: seedValues.otherUserId })
+
+    const response = await request(app).get('/awaiting-cancellation').query({ sort: '-created_at' })
+
+    expect(response.body).toHaveLength(3)
+    expect(response.headers['total-items']).toBe('3')
+    expect(response.body[0].id).toBe(job3.id)
+    expect(response.body[1].id).toBe(job2.id)
+    expect(response.body[2].id).toBe(job1.id)
   })
 })
