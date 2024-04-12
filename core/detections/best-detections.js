92b5809f6ff1397199cca3f0957c18f457395aa2
veckatimest <necgregory@gmail.com>
2024-04-12 21:50:57 +0400

    router added added

---
+++ core/detections/best-detections.js
@@ -0,0 +1,89 @@
+const router = require('express').Router()
+const { httpErrorHandler } = require('../../common/error-handling/http')
+const dao = require('./dao')
+const Converter = require('../../common/converter')
+
+/**
+ * @swagger
+ *
+ * /{jobId}/best-detections:
+ *   get:
+ *     summary: Get list of detections
+ *     description:
+ *     tags:
+ *       - detections
+ *     parameters:
+ *       - name: jobId
+ *         description: Classifier Job identifier
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: streams
+ *         description: List of stream ids to limit results
+ *         in: query
+ *         type: array|string
+ *       - name: by_date
+ *         description: Find best detections per each date (instead of whole period)
+ *         in: query
+ *         type: boolean
+ *         default: false
+ *         example: true
+ *       - name: start
+ *         description: Limit a start date on or after (iso8601 or epoch)
+ *         in: query
+ *         required: false
+ *         type: string
+ *         example: 2020-01-01T00:00:00.000Z
+ *       - name: end
+ *         description: Limit a start date before (iso8601 or epoch)
+ *         in: query
+ *         required: false
+ *         type: string
+ *         example: 2020-12-31T00:00:00.000Z
+ *       - name: review_statuses
+ *         description: Return rejected/uncertain/confirmed/unreviewed detections
+ *         in: query
+ *         type: string[]
+ *         example: ['rejected', 'uncertain', 'confirmed', 'unreviewed']
+ *       - name: n_per_stream
+ *         description: Maximum number of results per stream (and per day if by_date is set)
+ *         in: query
+ *         type: int
+ *         default: 100
+ *     responses:
+ *       200:
+ *         description: List of detection (lite) objects
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Detection'
+ *       400:
+ *         description: Invalid query parameters
+ */
+router.get('/:jobId/best-detections', (req, res) => {
+  const user = req.rfcx.auth_token_info
+
+  const { jobId } = req.params
+  const converter = new Converter(req.query, {}, true)
+  converter.convert('streams').optional().toArray()
+  converter.convert('by_date').default(false).toBoolean()
+  converter.convert('start').optional().toMomentUtc()
+  converter.convert('end').optional().toMomentUtc()
+  converter.convert('review_statuses').optional().toArray()
+  converter.convert('n_per_stream').default(1).toInt().maximum(10)
+
+  return converter.validate()
+    .then(async (filters) => {
+      filters.classifierJobId = jobId
+      const options = {
+        user
+      }
+      const result = await dao.queryBestDetections(filters, options)
+      return res.json(result)
+    })
+    .catch(httpErrorHandler(req, res, 'Failed getting detections'))
+})
+
+module.exports = router
