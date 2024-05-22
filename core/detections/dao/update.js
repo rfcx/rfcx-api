02fd17e5325aeff4089f717a99d687faf3b35315
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-05-22 21:07:17 +0700

    #602 use moment-timezone to serialize date before database interaction

--- core/detections/dao/update.js
+++ core/detections/dao/update.js
@@ -1,8 +1,9 @@
+const moment = require('moment-timezone')
 const { Detection } = require('../../_models')
 
 /**
- * Update detection
- * @param {{ id: string, start: string, streamId: string, classificationId?: number, classifierId?: number, classifierJobId?: number }} where where options
+ * Update detection, `id`, `start`, and `streamId` are required.
+ * @param {{ id: string, start: string, streamId: string, classificationId?: number, classifierId?: number, classifierJobId?: number }} filters where options
  * @param {Detection} detection
  * @param {string} detection.reviewStatus
  * @param {*} options
@@ -10,7 +11,7 @@ const { Detection } = require('../../_models')
  * @returns {Promise<void>} nothing to return
  * @throws EmptyResultError when detection not found
  */
-async function update (where, detection, options = {}) {
+async function update (filters, detection, options = {}) {
   const allowedDetection = {}
   const allowedUpdates = ['reviewStatus']
   allowedUpdates.forEach(k => {
@@ -18,6 +19,24 @@ async function update (where, detection, options = {}) {
       allowedDetection[k] = detection[k]
     }
   })
+
+  const where = {
+    id: filters.id,
+    start: moment.utc(filters.start).valueOf(),
+    streamId: filters.streamId
+  }
+
+  if (filters?.classificationId) {
+    where.classificationId = filters.classificationId
+  }
+
+  if (filters?.classifierId) {
+    where.classifierId = filters.classifierId
+  }
+
+  if (filters?.classifierJobId) {
+    where.classifierJobId = filters.classifierJobId
+  }
 
   const transaction = options.transaction || null
   return await Detection.update(allowedDetection, {
