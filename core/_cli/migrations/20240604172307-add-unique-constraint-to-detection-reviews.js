aae814bf2cbe894228f234f1c68748d498b55d57
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-06-07 16:49:21 +0700

    Add code part to delete duplicates out of the db before migration

--- core/_cli/migrations/20240604172307-add-unique-constraint-to-detection-reviews.js
+++ core/_cli/migrations/20240604172307-add-unique-constraint-to-detection-reviews.js
@@ -1,7 +1,38 @@
 'use strict'
 
+const { QueryTypes } = require('sequelize')
+
 module.exports = {
   up: async (queryInterface) => {
+    // Remove duplicates off of the `detection_reviews` table before creating the constraint.
+    const recordsWithDuplicates = await queryInterface.sequelize.query(`
+      SELECT
+        MIN(id) as first_id,
+        detection_id,
+        user_id,
+        count(*)
+        from detection_reviews
+      GROUP BY detection_id, user_id
+      HAVING COUNT(*) > 1;
+    `, { type: QueryTypes.RAW })
+
+    for (const record of recordsWithDuplicates) {
+      await queryInterface.sequelize.query(`
+        DELETE FROM
+          detection_reviews
+        WHERE
+          detection_id = :detectionId AND
+          user_id = :userId AND
+          id != :firstId
+      `, {
+        replacements: {
+          detectionId: record.detection_id,
+          userId: record.user_id,
+          firstId: record.first_id
+        }
+      })
+    }
+
     await queryInterface.sequelize.query(`
       ALTER TABLE
         "public"."detection_reviews"
