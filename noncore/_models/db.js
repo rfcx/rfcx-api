680f48827d960bef39f2c1c010671f2e7243ad55
TooSeriuz <frongs@rfcx.org>
2024-08-21 17:12:34 +0700

    fix lint

--- noncore/_models/db.js
+++ noncore/_models/db.js
@@ -1,4 +1,4 @@
-const { sequelize: sequelize, Sequelize, options } = require('../../common/db')('noncore')
+const { sequelize, Sequelize, options } = require('../../common/db')('noncore')
 const coreDb = require('../../common/db')('core')
 
 module.exports = { sequelize, Sequelize, options, coreSequelize: coreDb.sequelize, coreOptions: coreDb.options }
